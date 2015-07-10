3314c4c4022780fd1f167fda172c4ede591aa1ba
Corey Vixie <corey.vixie@gmail.com>
2015-07-09 23:18:22 -0700

    Built in watch task for images.

--- gulp_tasks/images.js
+++ gulp_tasks/images.js
@@ -4,15 +4,19 @@
 
 "use strict";
 
-var gulp       = require( "gulp" );
-var plumber    = require( "gulp-plumber" );
-var watch      = require( "gulp-watch" );
+var gulp = require( "gulp" );
+
+var chalk = require( "chalk" );
 
 gulp.task( "images"
          , [ "clean" ]
          , function () {
   return gulp.src( "app/src/images/**/*" )
-             .pipe( watch( "app/src/images/**/*" ) )
              // TODO: Process images appropriately
              .pipe( gulp.dest( "app/build/img" ) );
+});
+
+var watcher = gulp.watch( "app/src/images/**/*", [ "images" ] );
+watcher.on( "change", function ( event ) {
+  console.log( chalk.green( "Images: " ) + event.path + " was " + event.type );
 });
