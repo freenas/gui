20799e7bc43e7c44ef9b573bd514f1a0d7221af8
Corey Vixie <corey.vixie@gmail.com>
2015-07-10 16:43:07 -0700

    Added task to concat shared libs.

---
+++ gulp_tasks/libs.js
@@ -0,0 +1,21 @@
+// LIBS
+// ====
+// Simple concat task for our libs, using the already minified source provided
+// by the Bower components.
+
+"use strict";
+
+var gulp   = require( "gulp" );
+var concat = require( "gulp-concat" );
+
+gulp.task( "libs", function () {
+  return gulp.src( [ "bower_components/velocity/velocity.min.js"
+                   , "bower_components/velocity/velocity.ui.min.js"
+                   , "bower_components/d3/d3.min.js"
+                   , "node_modules/babel/browser-pollyfill.min.js"
+                   , "app/src/lib-ext/nv.d3.js"
+                   ]
+                 )
+              .pipe( concat( "libs.js" ) )
+              .pipe( gulp.dest( "app/build" ) );
+});
