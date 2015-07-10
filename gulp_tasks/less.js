520826e2f58dcb93debee94992ca4fb04b4999f6
Corey Vixie <corey.vixie@gmail.com>
2015-07-09 23:13:49 -0700

    Use built-in watch task for LESS.

--- gulp_tasks/less.js
+++ gulp_tasks/less.js
@@ -9,19 +9,23 @@ var less       = require( "gulp-less" );
 var minify     = require( "gulp-minify-css" );
 var plumber    = require( "gulp-plumber" );
 var sourcemaps = require( "gulp-sourcemaps" );
-var watch      = require( "gulp-watch" );
 var rename     = require( "gulp-rename" );
 
+var chalk = require( "chalk" );
+
 gulp.task( "less"
-         , [ "clean", "check-environment", "install-packages" ]
+         , [ "install-packages" ]
          , function () {
   return gulp.src( "app/src/styles/core.less" )
-             .pipe( watch( "app/src/styles/**/*" ) )
-             .pipe( plumber() )
              .pipe( sourcemaps.init() )
              .pipe( less() )
              .pipe( minify() )
              .pipe( sourcemaps.write() )
              .pipe( rename( "main.css" ) )
              .pipe( gulp.dest( "app/build/css" ) );
+});
+
+var watcher = gulp.watch( "app/src/styles/**/*", [ "less" ] );
+watcher.on( "change", function ( event ) {
+  console.log( chalk.cyan( "LESS: " ) + event.path + " was " + event.type );
 });
