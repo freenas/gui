c01d326aeda73e75063ad1384bc826ce13c9166d
Corey Vixie <corey.vixie@gmail.com>
2015-07-12 18:33:51 -0700

    WOW. I don't know what happened, but install-packages wasn't sync.

--- gulp_tasks/install-packages.js
+++ gulp_tasks/install-packages.js
@@ -9,8 +9,7 @@ var install = require( "gulp-install" );
 
 // TODO: Install packages whenever bower.json or package.json change (from git
 // pull or other non-user initiated action)
-gulp.task( "install-packages", function ( callback ) {
-  gulp.src( [ "./bower.json", "./package.json" ] )
-      .pipe( install() );
-  callback();
+gulp.task( "install-packages", function () {
+  return gulp.src( [ "./bower.json", "./package.json" ] )
+             .pipe( install() );
 });
