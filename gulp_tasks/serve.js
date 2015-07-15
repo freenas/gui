fc73a12f5f06db38609cca06dc7e17e4faddd244
Hans van Mil <hans.vanmil72@gmail.com>
2015-07-15 14:41:44 +0200

    Fix the gulp-watch issue on Windows platform.

--- gulp_tasks/serve.js
+++ gulp_tasks/serve.js
@@ -23,16 +23,16 @@ var buildChangeHandler = function ( vinyl ) {
   }
 };
 
-watch( [ "app/build/babel/**/*"
-       , "app/build/app.js"
-       , "app/build/libs.js"
-       , "app/server.js"
-       ]
-     , _.debounce( buildChangeHandler, 3000 ) );
-
 gulp.task( "serve", function () {
   var mode;
   var host;
+
+  watch( [ "app/build/babel/**/*"
+         , "app/build/app.js"
+         , "app/build/libs.js"
+         , "app/server.js"
+         ]
+       , _.debounce( buildChangeHandler, 3000 ) );
 
   if ( argv["connect"] ) {
     mode = "connect";
