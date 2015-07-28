28d492768dba79abc26ee533ceb924cab29ab47e
Ben Milman <bpmilman@gmail.com>
2015-07-27 18:07:51 -0700

    Add a response to the 'volumes.get_available_disks' query.

--- simulator/simulator.js
+++ simulator/simulator.js
@@ -3,6 +3,7 @@
 
 "use strict";
 
+var _ = require( "lodash" );
 var chalk = require( "chalk" );
 var WebSocketServer = require( "ws" ).Server;
 
@@ -21,6 +22,60 @@ var diskTypes = "both";
 var virtualSystem = systemGenerator( diskCount, diskTypes );
 
 var authTokens = {};
+
+function getUsedDiskPaths ( vdev ) {
+  var usedDiskPaths = [];
+
+  var i;
+
+  if ( vdev[ "type" ] === "disk" ) {
+    usedDiskPaths.push( vdev[ "path" ] );
+  } else {
+    for ( i = 0; i < vdev[ "children" ].length; i++ ) {
+      usedDiskPaths.push( vdev[ "children" ][ i ][ "path" ] );
+    }
+  }
+
+  return usedDiskPaths;
+
+}
+
+function getAvailableDisks ( system ) {
+  var availableDisks = [];
+  var volumes = system[ "volumes" ];
+
+  var i;
+  var j;
+
+  // Make a list of the paths to all disks
+  var availableDisks =
+    _.map( system[ "disks" ]
+         , function mapAvailableDiskPaths ( disk ) {
+           return disk[ "name" ];
+         }
+         );
+  // Iterate over volumes
+  for ( i = 0; i < volumes.length; i++ ) {
+    // Iterate over disk, spare, cache, and log in each volume
+    _.forEach( volumes[i].topology
+             , function mapUsedDisksOverTopology ( vdevType ) {
+               // Iterate over all the vdevs of a particular type
+               _.forEach( vdevType
+                        , function removeUsedDisksByVdev ( vdev ) {
+                          // Remove all the disks in a vdev from the list of
+                          // available disks.
+                          _.pull( availableDisks
+                                , getUsedDiskPaths( vdev )
+                                );
+                        }
+                        );
+             }
+             );
+  }
+
+  return availableDisks;
+
+}
 
 function handleCall ( data ) {
 
@@ -54,6 +109,10 @@ function handleCall ( data ) {
 
     case "disks.query":
       responseArgs = virtualSystem[ "disks" ];
+      break;
+
+    case "volumes.get_available_disks":
+      responseArgs = getAvailableDisks( virtualSystem );
       break;
 
     case "volumes.query":
