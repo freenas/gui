e6240967a9e8f6296cb03d74643e7b6404bfda04
Ben Milman <bpmilman@gmail.com>
2015-08-15 22:16:51 -0700

    Use a normal for loop instead of a fancy one. Who needs scope issues?

--- simulator/templates/VolumeCommon.js
+++ simulator/templates/VolumeCommon.js
@@ -25,46 +25,45 @@ class VolumeCommon {
 
     var volumeSize = 0;
 
-    _.forEach( dataVdevs
-             , function calculateVdevSize ( vdev ) {
-               let vdevSize = 0;
-               let smallestDiskSize = Infinity;
-               // Disk vdevs have only one disk and no children to iterate over.
-               if ( vdev.type === "disk" ) {
-                 vdevSize = VolumeCommon.getDiskSize( disks, vdev[ "path" ] );
-               } else {
-                 // Search for the smallest disk
-                 for ( let i = 0; i < vdev[ "children" ].length; i++ ) {
-                   if ( VolumeCommon.getDiskSize( disks
-                                   , vdev[ "children" ][ i ][ "path" ]
-                                   )
-                      < smallestDiskSize
-                      ) {
-                     smallestDiskSize =
-                       VolumeCommon.getDiskSize( disks
-                                  , vdev[ "children" ][ i ][ "path" ]
-                                  );
-                   }
-                 }
-                 // The size of a mirror vdev is always the size of its smallest
-                 // component disk.
-                 if ( vdev[ "type" ] === "mirror" ) {
-                   vdevSize = smallestDiskSize;
-                 } else {
-                   // Add the smallest disk size to the vdev size for each disk
-                   // over the vdev redundancy level.
-                   for ( let j = 0
-                       ; j < vdev[ "children" ].length
-                           - vdevRedundancy[ vdev[ "type" ] ]
-                       ; j ++
-                       ) {
-                     vdevSize += smallestDiskSize;
-                   }
-                 }
-                 volumeSize += vdevSize;
-               }
-             }
-             );
+    for ( let i = 0; i < dataVdevs.length; i++ ) {
+      let vdev = dataVdevs[i];
+      let vdevSize = 0;
+      let smallestDiskSize = Infinity;
+      // Disk vdevs have only one disk and no children to iterate over.
+      if ( vdev.type === "disk" ) {
+        vdevSize = VolumeCommon.getDiskSize( disks, vdev[ "path" ] );
+       } else {
+        // Search for the smallest disk
+        for ( let j = 0; j < vdev[ "children" ].length; j++ ) {
+          if ( VolumeCommon.getDiskSize( disks
+                          , vdev[ "children" ][ j ][ "path" ]
+                          )
+             < smallestDiskSize
+             ) {
+            smallestDiskSize =
+              VolumeCommon.getDiskSize( disks
+                         , vdev[ "children" ][ j ][ "path" ]
+                         );
+          }
+        }
+        // The size of a mirror vdev is always the size of its smallest
+        // component disk.
+        if ( vdev[ "type" ] === "mirror" ) {
+          vdevSize = smallestDiskSize;
+        } else {
+          // Add the smallest disk size to the vdev size for each disk
+          // over the vdev redundancy level.
+          for ( let j = 0
+              ; j < vdev[ "children" ].length
+                  - vdevRedundancy[ vdev[ "type" ] ]
+              ; j ++
+              ) {
+            vdevSize += smallestDiskSize;
+          }
+        }
+        volumeSize += vdevSize;
+      }
+    }
 
     return volumeSize;
 
