41ff7404c1269821dd2ba4352c926857fb2ba816
Ben Milman <bpmilman@gmail.com>
2015-08-16 19:37:41 -0700

    Update InterfacesStore to use the ES2015 class pattern

--- app/src/scripts/flux/stores/InterfacesStore.js
+++ app/src/scripts/flux/stores/InterfacesStore.js
@@ -4,12 +4,10 @@
 "use strict";
 
 import _ from "lodash";
-import EventEmitter from "events";
 
 import FreeNASDispatcher from "../dispatcher/FreeNASDispatcher";
 import { ActionTypes } from "../constants/FreeNASConstants";
-
-import InterfacesMiddleware from "..//middleware/InterfacesMiddleware";
+import FluxBase from "./FLUX_STORE_BASE_CLASS";
 
 const CHANGE_EVENT = "change";
 const UPDATE_MASK  = "network.interface.changed";
@@ -70,25 +68,25 @@ const INTERFACE_LABELS =
     , mtu                 : "MTU"
   };
 
-var InterfacesStore = _.assign( {}, EventEmitter.prototype, {
+class InterfacesStore extends FluxBase {
 
-  emitChange: function () {
-    this.emit( CHANGE_EVENT );
+  constructor () {
+    super()
+
+    this.dispatchToken = FreeNASDispatcher.register(
+      handlePayload.bind( this )
+    );
+
+    this.KEY_UNIQUE = "name";
+    this.ITEM_SCHEMA = INTERFACE_SCHEMA;
+    this.ITEM_LABELS = INTERFACE_LABELS;
   }
 
-  , addChangeListener: function ( callback ) {
-    this.on( CHANGE_EVENT, callback );
-  }
-
-  , removeChangeListener: function ( callback ) {
-    this.removeListener( CHANGE_EVENT, callback );
-  }
-
-  , getUpdateMask: function () {
+  get updateMask () {
     return UPDATE_MASK;
   }
 
-  , getPendingUpdateNames: function () {
+  get pendingUpdateNames () {
     return _updatedOnServer;
   }
 
@@ -98,97 +96,66 @@ var InterfacesStore = _.assign( {}, EventEmitter.prototype, {
    * @param  {String} name The interface name.
    * @return {Boolean}
    */
-  , isLocalTaskPending: function ( name ) {
-      return _.values( _localUpdatePending ).indexof( name ) > -1;
-    }
+  isLocalTaskPending ( name ) {
+    return _.values( _localUpdatePending ).indexof( name ) > -1;
+  }
 
   /**
    * Check if the selected interface is in the list of updated interfaces.
    * @param  {String} name The interface name.
    * @return {Boolean}
    */
-  , isInterfaceUpdatePending: function ( name ) {
+  isInterfaceUpdatePending ( name ) {
       return _updatedOnServer.indexof( name ) > -1;
-    }
+  }
 
-  , findInterfaceByKeyValue: function ( key, value ) {
+  findInterfaceByKeyValue ( key, value ) {
     var predicate = {};
-    predicate[key] = value;
+    predicate[ key ] = value;
     return _.find( _interfaces, predicate );
   }
 
-  , getInterfaceSchema: function () {
-      return INTERFACE_SCHEMA;
-    }
-
-  , getInterfaceLabels: function () {
-      return INTERFACE_LABELS;
-    }
-
-  , getInterface: function ( name ) {
+  getInterface ( name ) {
     return _interfaces[ name ];
   }
 
-  , getAllInterfaces: function () {
+  get interfaces () {
     return _.values( _interfaces );
   }
 
-});
+}
 
-InterfacesStore.dispatchToken = FreeNASDispatcher.register(
-  function ( payload ) {
-    var action = payload.action;
+function handlePayload ( payload ) {
+  var action = payload.action;
 
-    switch ( action.type ) {
+  switch ( action.type ) {
 
-      case ActionTypes.RECEIVE_INTERFACES_LIST:
-        var updatedInterfaceNames = _.pluck( action.rawInterfacesList, "name" );
+    case ActionTypes.RECEIVE_INTERFACES_LIST:
+      var updatedInterfaceNames = _.pluck( action.rawInterfacesList, "name" );
 
-        if ( _updatedOnServer.length ) {
-          _updatedOnServer = _.difference(
-            _updatedOnServer
-            , updatedInterfaceNames
-          );
-        }
+      if ( _updatedOnServer.length ) {
+        _updatedOnServer = _.difference(
+          _updatedOnServer
+          , updatedInterfaceNames
+        );
+      }
 
-        action.rawInterfacesList.map( function ( _interface ) {
-          _interfaces[ _interface.name ] = _interface;
-        });
+      action.rawInterfacesList.map( function ( _interface ) {
+        _interfaces[ _interface.name ] = _interface;
+      });
+      this.emitChange();
+      break;
 
-        InterfacesStore.emitChange();
-        break;
+    case ActionTypes.RECEIVE_UP_INTERFACE_TASK:
+    case ActionTypes.RECEIVE_DOWN_INTERFACE_TASK:
+    case ActionTypes.RECEIVE_INTERFACE_CONFIGURE_TASK:
+      _localUpdatePending[ action.taskID ] = action.interfaceName;
+      this.emitChange();
+      break;
 
-      case ActionTypes.MIDDLEWARE_EVENT:
-        var args = action.eventData.args;
-        var updateData = args["args"];
-
-        /*
-        if ( args["name"] === UPDATE_MASK ) {
-          let updateData = args["args"];
-
-          if ( updateData ["operation"] === "update" ) {
-
-            // Not reall sure this is doing something useful.
-            Array.prototype.push.apply( _updatedOnServer, updateData["ids"] );
-            InterfacesMiddleware.requestInterfacesList( );
-          }
-        }
-        */
-
-        // InterfacesStore.emitChange();
-        break;
-
-      case ActionTypes.RECEIVE_UP_INTERFACE_TASK:
-      case ActionTypes.RECEIVE_DOWN_INTERFACE_TASK:
-      case ActionTypes.RECEIVE_INTERFACE_CONFIGURE_TASK:
-        _localUpdatePending[ action.taskID ] = action.interfaceName;
-        InterfacesStore.emitChange();
-        break;
-
-      default:
-      // Do nothing
-    }
+    default:
+    // Do nothing
   }
-);
+}
 
-module.exports = InterfacesStore;
+export default new InterfacesStore;
