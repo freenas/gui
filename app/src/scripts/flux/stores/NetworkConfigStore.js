50ed5c18ede17446f1df0f2b33784bb57a48a7f5
Ben Milman <bpmilman@gmail.com>
2015-08-16 19:35:02 -0700

    Update NetworkConfigStore to use the ES2015 class pattern

--- app/src/scripts/flux/stores/NetworkConfigStore.js
+++ app/src/scripts/flux/stores/NetworkConfigStore.js
@@ -4,78 +4,65 @@
 "use strict";
 
 import _ from "lodash";
-import { EventEmitter } from "events";
 
 import FreeNASDispatcher from "../dispatcher/FreeNASDispatcher";
 import { ActionTypes } from "../constants/FreeNASConstants";
-
-import NetworkConfigMiddleware from "..//middleware/GroupsMiddleware";
+import FluxBase from "./FLUX_STORE_BASE_CLASS";
 
 const CHANGE_EVENT = "change";
-const UPDATE_MASK = "network.changed";
+const UPDATE_MASK = "network.config.changed";
 
 var _localUpdatePending = [];
 var _networkConfig = {};
 
+class NetworkConfigStore extends FluxBase {
 
-var NetworkConfigStore = _.assign( {}, EventEmitter.prototype, {
+  constructor () {
+    super();
 
-  emitChange: function () {
-      this.emit( CHANGE_EVENT );
-    }
+    this.dispatchToken = FreeNASDispatcher.register(
+      handlePayload.bind( this )
+    );
+  }
 
-  , addChangeListener: function ( callback ) {
-      this.on( CHANGE_EVENT, callback );
-    }
-
-  , removeChangeListener: function ( callback ) {
-      this.removeListener( CHANGE_EVENT, callback );
-    }
-
-  , getUpdateMask: function () {
+  get updateMask () {
       return UPDATE_MASK;
     }
 
-  /**
-   * Check if there are any pending update tasks.
-   * @return {Boolean}
-   */
-  , isUpdating: function () {
-      return _localUpdatePending.length > 0;
-    }
-
-  , getNetworkConfig: function () {
-      return _networkConfig;
-    }
-
-});
-
-NetworkConfigStore.dispatchToken = FreeNASDispatcher.register(
-  function ( payload ) {
-    var action = payload.action;
-
-    switch ( action.type ) {
-      case ActionTypes.RECEIVE_NETWORK_CONFIG:
-        _networkConfig = action.networkConfig;
-        NetworkConfigStore.emitChange();
-        break;
-
-      case ActionTypes.MIDDLEWARE_EVENT:
-        let args = action.eventData.args;
-        if ( args.name === "task.updated"
-            && args.args.name === "network.configure"
-            && args.args.state === "FINISHED" ) {
-          _localUpdatePending = _.without( _localUpdatePending, args.args.id );
-          NetworkConfigStore.emitChange();
-        }
-        break;
-
-      case ActionTypes.RECEIVE_NETWORK_CONFIG_UPDATE:
-        _localUpdatePending.push( action.taskID );
-        NetworkConfigStore.emitChange();
-        break;
-    }
+  isUpdating () {
+    return _localUpdatePending.length > 0;
   }
-);
 
-module.exports = NetworkConfigStore;
+  get networkConfig () {
+    return _networkConfig;
+  }
+
+}
+
+function handlePayload ( payload ) {
+  var action = payload.action;
+
+  switch ( action.type ) {
+    case ActionTypes.RECEIVE_NETWORK_CONFIG:
+      _networkConfig = action.networkConfig;
+      this.emitChange();
+      break;
+
+    case ActionTypes.MIDDLEWARE_EVENT:
+      let args = action.eventData.args;
+      if ( args.name === "task.updated"
+          && args.args.name === "network.configure"
+          && args.args.state === "FINISHED" ) {
+        _localUpdatePending = _.without( _localUpdatePending, args.args.id );
+        this.emitChange();
+      }
+      break;
+
+    case ActionTypes.RECEIVE_NETWORK_CONFIG_UPDATE:
+      _localUpdatePending.push( action.taskID );
+      this.emitChange();
+      break;
+  }
+}
+
+export default new NetworkConfigStore;
