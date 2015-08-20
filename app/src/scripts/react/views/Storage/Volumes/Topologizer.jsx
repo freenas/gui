5a687b46b39bdd25a474acdb205d6937a1ec531a
Corey Vixie <corey.vixie@gmail.com>
2015-08-20 01:47:10 -0700

    Thing can be dragged now (does nothing)

--- app/src/scripts/react/views/Storage/Volumes/Topologizer.jsx
+++ app/src/scripts/react/views/Storage/Volumes/Topologizer.jsx
@@ -7,10 +7,68 @@
 
 import React from "react";
 
+import Coords from "../../../../utility/Coords";
+
 const Topologizer = React.createClass(
-  { render () {
+  { getInitialState () {
+    return { topoPrefs: [ 0.33, 0.34, 0.33 ]
+           , active: false
+           , bounding: null
+           , cursorPos: [ 0, 0 ]
+           , trianglePoints: []
+           };
+  }
+
+  , componentDidMount () {
+      let rect = React.findDOMNode( this.refs.bounding )
+                      .getBoundingClientRect();
+
+      let A = [ 0, rect.height ];
+      let B = [ rect.width / 2, 0 ];
+      let C = [ rect.width, rect.height ];
+
+      this.setState(
+        { bounding: rect
+        , trianglePoints: [ A, B, C ]
+        }
+      );
+    }
+
+  , handleCursorActive ( event ) {
+      this.setState(
+        { active: true
+        }
+      );
+    }
+
+  , handleCursorMove ( event ) {
+      if ( this.state.active ) {
+        let cursorPos = [ event.clientX - this.state.bounding.left
+                        , event.clientY - this.state.bounding.top ];
+
+        let [ A, B, C ] = this.state.trianglePoints;
+
+        this.setState(
+          { cursorPos: cursorPos
+          }
+        );
+      }
+
+    }
+
+  , handleEndActive ( event ) {
+      this.setState(
+        { active: false
+        }
+      );
+    }
+
+  , render () {
       return (
-        <div className = "topologizer">
+        <div
+          className = "topologizer"
+          onMouseUp = { this.handleEndActive }
+        >
           <span className = "topologizer-label-health">
             {"Safety"}
           </span>
@@ -20,8 +78,16 @@ const Topologizer = React.createClass(
           <span className = "topologizer-label-size">
             {"Storage"}
           </span>
-          <div className = "topologizer-bounding">
-            <div className = "topologizer-handle" />
+          <div
+            ref = "bounding"
+            className = "topologizer-bounding"
+            onMouseMove = { this.handleCursorMove }
+          >
+            <div
+              className = "topologizer-handle"
+              style = {{ top: this.state.cursorPos[1], left: this.state.cursorPos[0] }}
+              onMouseDown = { this.handleCursorActive }
+            />
           </div>
           <div className = "toppologizer-wrap">
             <span className = "toppologizer-triangle-wrap">
