33eb0de7496b9a81c2d561706180e50fccbde5bb
Ben Milman <bpmilman@gmail.com>
2015-08-19 18:15:50 -0700

    Initial commit of "real" interface content. Just name, status, and interface type for now.

--- app/src/scripts/react/views/Network/Interfaces/InterfaceItem.jsx
+++ app/src/scripts/react/views/Network/Interfaces/InterfaceItem.jsx
@@ -24,12 +24,68 @@ const InterfaceItem = React.createClass(
     );
   }
 
+  , componentDidMount: function () {
+    IS.addChangeListener( this.handleInterfaceChange );
+  }
+
+  , componentWillUnmount: function () {
+    IS.removeChangeListener( this.handleInterfaceChange );
+  }
+
+  , handleInterfaceChange: function () {
+    this.setState( { targetInterface: this.getInterface() } );
+  }
+
   , getInterface: function () {
-    return { targetInterface: IS.getInterface( this.getDynamicRoute ) };
+    return IS.findInterfaceByKeyValue( "name", this.getDynamicRoute() );
   }
 
   , render: function () {
-    return null;
+
+    var statusClass = "";
+    var interfaceName = null;
+    var interfaceType = "";
+    var enabled = this.state.targetInterface
+                ? this.state.targetInterface[ "enabled" ]
+                : false;
+
+    // This all breaks if the interface isn't yet loaded.
+    if ( this.state.targetInterface ) {
+      if ( this.state.targetInterface[ "status" ][ "link-state" ]
+       === "LINK_STATE_UP" ) {
+        statusClass = "interface-up";
+      } else if ( this.state.targetInterface[ "status" ][ "link-state" ]
+       === "LINK_STATE_UNKNOWN" ) {
+        statusClass = "interface-unknown";
+      } else if ( this.state.targetInterface[ "status" ][ "link-state" ]
+       === "LINK_STATE_DOWN" ) {
+        statusClass = "interface-down";
+      }
+
+      interfaceName = (
+        <h2 className = { "interface-name " + statusClass } >
+          { this.state.targetInterface[ "name" ] }
+        </h2>
+      );
+
+      // So fake. Just a token use of the middleware.
+      interfaceType = this.state.targetInterface[ "type" ] === "ETHER"
+                    ? "10/100/1000 Ethernet Adapter"
+                    : "";
+
+      // This could be much more robust.
+    }
+
+    return (
+      <TWBS.Grid className = "viewer-item-info interface-item">
+        <TWBS.Row className = "interface-header" >
+          <TWBS.Col xs = {12} >
+            { interfaceName }
+            <h4>{ interfaceType }</h4>
+          </TWBS.Col>
+        </TWBS.Row>
+      </TWBS.Grid>
+    );
   }
 });
 
