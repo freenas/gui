688cbcd48ba2ee57ca473d6bb5492c1879126a72
Ben Milman <bpmilman@gmail.com>
2015-08-21 19:20:47 -0700

    Empty view for settings, to allow me to send changes selectively from now one.

---
+++ app/src/scripts/react/views/Network/Settings.jsx
@@ -0,0 +1,18 @@
+ // Settings
+// ========
+// Display and modify FreeNAS OS and GUI settings.
+
+"use strict";
+
+import React from "react";
+import _ from "lodash";
+import TWBS from "react-bootstrap";
+
+const Settings = React.createClass(
+  { render () {
+      return null;
+    }
+  }
+);
+
+export default Settings;
