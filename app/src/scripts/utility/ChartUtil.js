7485d5e144cbd25a54fefdecc1b521716f39af75
Corey Vixie <corey.vixie@gmail.com>
2015-08-24 13:21:19 -0700

    No longer needed!

--- app/src/scripts/utility/ChartUtil.js
+++ app/src/scripts/utility/ChartUtil.js
@@ -3,32 +3,6 @@
 
 "use strict";
 
-import _ from "lodash";
-import GLOBAL_STYLES from "../static/ChartjsDefaults";
-
-const FREENAS_STYLES =
-  { responsive          : true
-  , maintainAspectRatio : false
-  };
-
-const COMMON_STYLES =
-  { pointStrokeColor: "#fff"
-  , pointHighlightFill: "#fff"
-  };
-
-const SERIES_STYLES =
-  [ { fillColor            : "rgba( 1, 150, 216, 0.2 )"
-    , strokeColor          : "rgba( 1, 150, 216, 0.5 )"
-    , pointColor           : "rgba( 1, 150, 216, 1 )"
-    , pointHighlightStroke : "rgba( 1, 150, 216, 1 )"
-    }
-  , { fillColor: "rgba(151,187,205,0.2)"
-    , strokeColor: "rgba(151,187,205,1)"
-    , pointColor: "rgba(151,187,205,1)"
-    , pointHighlightStroke: "rgba(151,187,205,1)"
-    }
-  ];
-
 class WidgetUtil {
   static rand ( min, max, num ) {
     let rtn = [];
@@ -36,24 +10,6 @@ class WidgetUtil {
       rtn.push( ( Math.random() * ( max - min ) ) + min );
     }
     return rtn;
-  }
-
-  static styleDatasets ( datasets, override = 0 ) {
-    return datasets.map( ( dataset, index ) => {
-      return _.assign( {}
-                     , COMMON_STYLES
-                     , SERIES_STYLES[ index + override ]
-                     , dataset
-                     );
-    });
-  }
-
-  static getChartStyles ( customStyles ) {
-    return _.assign( {}
-                   , GLOBAL_STYLES
-                   , FREENAS_STYLES
-                   , customStyles || {}
-                   );
   }
 }
 
