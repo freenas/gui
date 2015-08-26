85b07b45aafa49793a651babbfa4b1519dc82e19
Ben Milman <bpmilman@gmail.com>
2015-08-25 18:31:38 -0700

    Add the initial tabs for Settings

--- app/src/scripts/react/views/Settings.jsx
+++ app/src/scripts/react/views/Settings.jsx
@@ -1,18 +1,48 @@
- // Settings
+// Settings
 // ========
 // Display and modify FreeNAS OS and GUI settings.
 
 "use strict";
 
 import React from "react";
-import _ from "lodash";
-import TWBS from "react-bootstrap";
+import { RouteHandler } from "react-router";
+
+import routerShim from "../mixins/routerShim";
+import SectionNav from "../components/SectionNav";
+
+const sections = [ { route: "system"
+                   , display: "System Settings"
+                   }
+                 , { route: "update"
+                   , display: "Updates and Boot Environments"
+                   }
+                 , { route: "security"
+                   , display: "Security Certificates"
+                   }
+                 , { route: "support"
+                   , display: "Support"
+                   }
+                 ];
 
 const Settings = React.createClass(
-  { render () {
-      return null;
-    }
+  { mixins: [ routerShim ]
+
+  , componentDidMount () {
+    this.calculateDefaultRoute( "settings", "system", "endsWith" );
   }
-);
+
+  , componentWillUpdate ( prevProps, prevState ) {
+    this.calculateDefaultRoute( "settings", "system", "endsWith" );
+  }
+
+  , render () {
+    return (
+      <main>
+        <SectionNav views = { sections } />
+        <RouteHandler />
+      </main>
+    );
+  }
+});
 
 export default Settings;
