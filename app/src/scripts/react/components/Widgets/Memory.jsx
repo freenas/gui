5256de849a04546b5acd08a3476c5668bf34c87d
Corey Vixie <corey.vixie@gmail.com>
2015-08-27 00:04:52 -0700

    Fix values and display of Memory Allocation chart

--- app/src/scripts/react/components/Widgets/Memory.jsx
+++ app/src/scripts/react/components/Widgets/Memory.jsx
@@ -6,6 +6,7 @@ import React from "react";
 
 import Widget from "../Widget";
 import ChartUtil from "../../../utility/ChartUtil";
+import ByteCalc from "../../../utility/ByteCalc";
 import c3Defaults from "../../../constants/c3Defaults";
 
 var c3;
@@ -20,10 +21,10 @@ if ( typeof window !== "undefined" ) {
 
 const Memory = React.createClass(
   { componentDidMount () {
-      let active = [ "Active" ].concat( ChartUtil.rand( 0.4, 0.5, 31 ) );
-      let cache = [ "Cache" ].concat( ChartUtil.rand( 0.002, 0.003, 31 ) );
-      let wired = [ "Wired" ].concat( ChartUtil.rand( 1.1, 1.3, 31 ) );
-      let inactive = [ "Inactive" ].concat( ChartUtil.rand( 0.2, 0.25, 31 ) );
+      let active = [ "Active" ].concat( ChartUtil.rand( 400000000, 500000000, 31 ) );
+      let cache = [ "Cache" ].concat( ChartUtil.rand( 2000000, 3000000, 31 ) );
+      let wired = [ "Wired" ].concat( ChartUtil.rand( 1100000000, 1300000000, 31 ) );
+      let inactive = [ "Inactive" ].concat( ChartUtil.rand( 200000000, 250000000, 31 ) );
       let free = this.calcFree( 31, active, cache, wired, inactive );
 
       this.chart = c3.generate(
@@ -41,6 +42,20 @@ const Memory = React.createClass(
                   , axis:
                     { x:
                       { show: false
+                      }
+                    , y:
+                      { tick:
+                        { format: function ( y ) {
+                            return ByteCalc.humanize( y, { roundMode: "whole" } );
+                          }
+                        }
+                      }
+                    }
+                  , tooltip:
+                    { format:
+                      { value: function ( value, ratio, id ) {
+                          return ByteCalc.humanize( value );
+                        }
                       }
                     }
                   }
@@ -60,7 +75,7 @@ const Memory = React.createClass(
 
       // Start at 1 because the first value will be the category
       for ( let i = 1; i < length + 1; i++ ) {
-        free.push( 30 - active[i] - cache[i] - wired[i] - inactive[i] );
+        free.push( 30000000000 - active[i] - cache[i] - wired[i] - inactive[i] );
       }
 
       return free;
@@ -68,12 +83,13 @@ const Memory = React.createClass(
 
   , tick () {
       if ( this.chart ) {
-        let active = [ "Active" ].concat( ChartUtil.rand( 0.4, 0.5, 1 ) );
-        let cache = [ "Cache" ].concat( ChartUtil.rand( 0.002, 0.003, 1 ) );
-        let wired = [ "Wired" ].concat( ChartUtil.rand( 1.1, 1.3, 1 ) );
-        let inactive = [ "Inactive" ].concat( ChartUtil.rand( 0.2, 0.25, 1 ) );
+        let active = [ "Active" ].concat( ChartUtil.rand( 400000000, 500000000, 31 ) );
+        let cache = [ "Cache" ].concat( ChartUtil.rand( 2000000, 3000000, 31 ) );
+        let wired = [ "Wired" ].concat( ChartUtil.rand( 1100000000, 1300000000, 31 ) );
+        let inactive = [ "Inactive" ].concat( ChartUtil.rand( 200000000, 250000000, 31 ) );
         let free = this.calcFree( 1, active, cache, wired, inactive );
 
+        console.log( free );
         this.chart.flow(
           { columns: [ free, active, cache, wired, inactive ]
           }
