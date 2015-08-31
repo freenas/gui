be9e04151e0a94c91bf45f17542d03e08aea288e
Ben Milman <bpmilman@gmail.com>
2015-08-31 15:52:44 -0700

    Initial layout for System Settings page

--- app/src/scripts/react/views/Settings/System.jsx
+++ app/src/scripts/react/views/Settings/System.jsx
@@ -5,10 +5,83 @@
 "use strict";
 
 import React from "react";
+import TWBS from "react-bootstrap";
+
+const HardwareSettings = React.createClass(
+  { render () {
+    return (
+      <TWBS.Panel>
+        <h4>Hardware</h4>
+      </TWBS.Panel>
+    );
+  }
+});
+
+const LocalizationSettings = React.createClass(
+  { render () {
+    return (
+      <TWBS.Panel>
+        <h4>Localization</h4>
+      </TWBS.Panel>
+    );
+  }
+});
+
+const OSSettings = React.createClass(
+  { render () {
+    return (
+      <TWBS.Panel>
+        <h4>Operating System </h4>
+      </TWBS.Panel>
+    );
+  }
+});
+
+const Tuneables = React.createClass(
+  { render () {
+    return (
+      <TWBS.Panel>
+        <h4>Tuneables</h4>
+      </TWBS.Panel>
+    );
+  }
+});
+
+const UISettings = React.createClass(
+  { render () {
+    return (
+      <TWBS.Panel>
+        <h4>Webapp</h4>
+      </TWBS.Panel>
+    );
+  }
+});
 
 const System = React.createClass(
   { render () {
-    return <h2>System Settings</h2>;
+    return (
+      <TWBS.Grid>
+        <TWBS.Row>
+          <TWBS.Col xs = {4}>
+            <UISettings/>
+          </TWBS.Col>
+          <TWBS.Col xs = {4}>
+            <OSSettings/>
+          </TWBS.Col>
+          <TWBS.Col xs = {4}>
+            <LocalizationSettings/>
+          </TWBS.Col>
+        </TWBS.Row>
+        <TWBS.Row>
+          <TWBS.Col xs = {4}>
+            <HardwareSettings/>
+          </TWBS.Col>
+          <TWBS.Col xs = {8}>
+            <Tuneables/>
+          </TWBS.Col>
+        </TWBS.Row>
+      </TWBS.Grid>
+    );
   }
 });
 
