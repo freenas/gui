249b68a0dddc6dc99863725ced188d92244a2262
Ben Milman <bpmilman@gmail.com>
2015-09-02 22:26:55 -0700

    Add functions for obtaining and populating data to System settings

--- app/src/scripts/react/views/Settings/System.jsx
+++ app/src/scripts/react/views/Settings/System.jsx
@@ -8,6 +8,8 @@ import React from "react";
 import TWBS from "react-bootstrap";
 import _ from "lodash";
 
+import IM from "../../../flux/middleware/InterfacesMiddleware";
+import IS from "../../../flux/stores/InterfacesStore"
 import SM from "../../../flux/middleware/SystemMiddleware";
 import SS from "../../../flux/stores/SystemStore";
 
@@ -21,8 +23,76 @@ function getSystemUIConfig () {
   return SS.systemUIConfig;
 }
 
+function getSystemGeneralConfig () {
+  return SS.systemGeneralConfig;
+}
+
+function getSystemAdvancedConfig () {
+  return SS.systemAdvancedConfig
+}
+
+function getInterfaces () {
+  return IS.interfaces;
+}
+
+function getTimezones () {
+  return SS.timezones;
+}
+
+function getKeymaps () {
+  return SS.keymaps;
+}
+
 const System = React.createClass(
-  { render () {
+  { getInitialState () {
+    return { general: {}
+           , ui: {}
+           , advanced: {}
+           , interfaces: []
+           , keymapList: []
+           , timezoneList: []
+           };
+  }
+
+  , componentDidMount () {
+    IS.addChangeListener( this.handleInterfacesChange );
+    IM.requestInterfacesList();
+    IM.subscribe( this.constructor.displayName );
+
+    SS.addChangeListener( this.handleSystemChange );
+    SM.requestSystemGeneralConfig();
+    SM.requestSystemUIConfig();
+    SM.requestSystemAdvancedConfig();
+    SM.requestKeymaps();
+    SM.requestTimezones();
+    SM.subscribe( this.constructor.displayName );
+  }
+
+  , handleSystemChange ( eventMask ) {
+    switch ( eventMask ) {
+      case "general":
+        this.setState( { general: getSystemGeneralConfig() } );
+        break;
+      case "ui":
+        this.setState( { ui: getSystemUIConfig() } );
+        break;
+      case "advanced":
+        this.setState( { advanced: getSystemAdvancedConfig() } );
+        break;
+      case "keymaps":
+        this.setState( { keymapList: getKeymaps() } );
+        break;
+      case "timezones":
+        this.setState( { timezoneList: getTimezones() } );
+        break;
+    }
+  }
+
+  , handleInterfacesChange () {
+    this.setState( { interfaces: getInterfaces() } );
+  }
+
+  , render () {
     return (
       <TWBS.Grid>
         <TWBS.Row>
