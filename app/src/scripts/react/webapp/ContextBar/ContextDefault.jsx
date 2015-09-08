15b40f7f8da28e2f252bfbc06c39ff202d6ba558
Corey Vixie <corey.vixie@gmail.com>
2015-09-07 23:48:54 -0700

    Remove tabbed navigation from default context

--- app/src/scripts/react/webapp/ContextBar/ContextDefault.jsx
+++ app/src/scripts/react/webapp/ContextBar/ContextDefault.jsx
@@ -1,33 +1,15 @@
 // OVERVIEW BAR NAVIGATION
 // ========================
+// This is the default view shown in the FreeNAS Context Bar when no other
+// contexts are active.
 
 "use strict";
 
 import React from "react";
-import { Nav, NavItem } from "react-bootstrap";
-
-import Icon from "../../components/Icon";
 
 const ContextDefault = React.createClass(
   { render () {
-      return (
-        <Nav
-          bsStyle = "tabs"
-          activeKey = {1}
-          onSelect = { this.handleSelect }
-          justified
-        >
-          <NavItem eventKey={1}>
-            <Icon glyph="icon-datareport" />
-          </NavItem>
-          <NavItem eventKey={2}>
-            <Icon glyph="icon-comment-alt" />
-          </NavItem>
-          <NavItem eventKey={3}>
-            <Icon glyph="icon-clipboard" />
-          </NavItem>
-        </Nav>
-      );
+      return null;
     }
 
   }
