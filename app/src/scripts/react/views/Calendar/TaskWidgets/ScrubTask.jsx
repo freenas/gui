36b09d9cb3839d14523d16a2adda05f0fe802e7b
Ben Milman <bpmilman@gmail.com>
2015-09-10 19:12:25 -0700

    ScrubTask is just for display. All the logic about what it represents is relocating to ScrubModal.

--- app/src/scripts/react/views/Calendar/TaskWidgets/ScrubTask.jsx
+++ app/src/scripts/react/views/Calendar/TaskWidgets/ScrubTask.jsx
@@ -6,63 +6,15 @@
 "use strict";
 
 import React from "react";
-import { Input } from "react-bootstrap";
-
-// import TM from "../../../../flux/middleware/TaskMiddleware";
-// import TS from "../../../../flux/stores/TaskStore";
-
-import VS from "../../../../flux/stores/VolumeStore";
-import ZM from "../../../../flux/middleware/ZfsMiddleware";
-
 
 const ScrubTask = React.createClass(
-  { getInitialState () {
-    return { volumes: [] }
-  }
-
-  , componentDidMount () {
-    VS.addChangeListener( this.handleVolumes );
-
-    ZM.subscribe( this.constructor.displayName );
-    ZM.requestVolumes();
-  }
-
-  , componentWillUnmount () {
-    VS.removeChangeListener( this.handleVolumes );
-
-    ZM.unsubscribe( this.constructor.displayName );
-  }
-
-  , handleVolumes () {
-    this.setState( { volumes: VS.listVolumes( "name" ) } );
-  }
-
-  , createVolumeOptions () {
-    var options = [];
-
-    if ( _.has( this, [ "state", "volumes" ] ) ) {
-      options = _.map( this.state.volumes
-                     , function mapVolumeNameOptions ( volume ) {
-                       return (
-                         <option
-                           value = { volume.name }
-                           key = { volume.name }>
-                           { volume.name }
-                         </option>
-                       );
-                     }
-                     );
-    }
-    return options;
-  }
+  { getDefaultProps () { return { volumeName: null } }
 
   , render () {
     return (
       <div>
         <h4>ZFS Scrub</h4>
-        <Input type = "select">
-          { this.createVolumeOptions() }
-        </Input>
+        <h5>{ this.props.volumeName }</h5>
       </div>
     );
   }
