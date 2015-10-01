d5db515fe83ce5c2eb994a8c96e219aec393b5b9
Ben Milman <bpmilman@gmail.com>
2015-09-30 23:18:32 -0700

    Only request interesting events

--- app/src/scripts/flux/middleware/EventsMiddleware.js
+++ app/src/scripts/flux/middleware/EventsMiddleware.js
@@ -12,7 +12,31 @@ import EAC from "../actions/EventsActionCreators";
 class EventsMiddleware extends MiddlewareBase {
   static requestEvents () {
     MC.request( "event.query"
-              , [ []
+              , [ [ "and"
+                  , [ "name"
+                    , "in"
+                    , [ "alerts.filters.changed"
+                      , "calendar_tasks.changed"
+                      , "disks.changed"
+                      , "groups.changed"
+                      , "network.changed"
+                      , "ntpservers.changed"
+                      , "shares.changed"
+                      , "tunables.changed"
+                      , "update.changed"
+                      , "users.changed"
+                      , "volumes.changed"
+                      , "fs.zfs.scrub.finish"
+                      , "fs.zfs.scrub.start"
+                      , "server.client_logged"
+                   // , "server.client_loggedout"
+                      , "server.shutdown"
+                      , "system.network.interface.link_down"
+                      , "system.network.interface.link_up"
+                      , "update.changed"
+                      ]
+                    ]
+                  ]
                 , { limit: EVENTS_LIMIT
                   , sort: "-id"
                   , dir: "desc"
