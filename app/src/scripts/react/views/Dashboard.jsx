89fb1d8131bc42cd34056d562679e27607d35cd6
Ben Milman <bpmilman@gmail.com>
2015-10-04 15:53:16 -0700

    Add function to subscribe to a group of datasources to Dashboard

--- app/src/scripts/react/views/Dashboard.jsx
+++ app/src/scripts/react/views/Dashboard.jsx
@@ -35,6 +35,26 @@ export default class Dashboard extends React.Component {
     }
   }
 
+  subscribeToDataSources ( dataSourceGroup, frequency ) {
+    var newDataSourceGroups = this.state.dataSourceGroups.slice();
+    newDataSourceGroups.push( dataSourceGroup );
+    this.setState( { dataSourceGroups: newDataSourceGroups } );
+    SM.subscribeToPulse( this.displayName, dataSourceGroup );
+
+    const now = moment().format();
+    // Hardcoded to the past 60 seconds of data.
+    const startTime = moment( now ).subtract( frequency * 60, "seconds" ).format();
+
+    dataSourceGroup.forEach( function requestInitialData( dataSource ) {
+                             SM.requestWidgetData( dataSource
+                                                 , startTime
+                                                 , now
+                                                 , frequency + "S"
+                                                 );
+                            }
+                         );
+  }
+
   render () {
     return (
       <main className="full dashboard">
