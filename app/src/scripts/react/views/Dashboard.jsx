e1cd12a13b56744d0d078e4ae1525d3080fbcf43
Ben Milman <bpmilman@gmail.com>
2015-10-05 13:56:30 -0700

    Add new data acquistion to Dashboard for SystemInfo

--- app/src/scripts/react/views/Dashboard.jsx
+++ app/src/scripts/react/views/Dashboard.jsx
@@ -8,6 +8,9 @@
 import React from "react";
 import moment from "moment";
 
+import DM from "../../flux/middleware/DisksMiddleware";
+import DS from "../../flux/stores/DisksStore";
+
 import SM from "../../flux/middleware/StatdMiddleware";
 import SS from "../../flux/stores/StatdStore";
 
@@ -19,6 +22,19 @@ import CPU from "../components/Widgets/CPU";
 import Network from "../components/Widgets/Network";
 import Memory from "../components/Widgets/Memory";
 import DashboardContext from "./Dashboard/DashboardContext";
+
+function getSystemInformation () {
+  return _.merge( SystemStore.getSystemInfo( "hardware" )
+                , SystemStore.systemGeneralConfig
+                , { version: SystemStore.version }
+                );
+}
+
+function getPredominantDisks () {
+  let predominantDisks = DS.predominantDisks;
+
+  return { predominantDisks: predominantDisks };
+}
 
 export default class Dashboard extends React.Component {
 
@@ -32,11 +48,19 @@ export default class Dashboard extends React.Component {
     document.addEventListener( "visibilitychange"
                              , this.handleVisibilityChange.bind( this )
                              );
+
+    DS.addChangeListener( this.handleDisksChange.bind( this ) );
+    DM.requestDisksOverview();
+    DM.subscribe( this.displayName );
+
     SS.addChangeListener( this.handleDataUpdate.bind( this ) );
 
     SystemMiddleware.subscribe( this.displayName );
-    SystemStore.addChangeListener( this.handleStaticData.bind( this ) );
+    SystemStore.addChangeListener( this.handleHardwareChange.bind( this ) );
     SystemMiddleware.requestSystemInfo( "hardware" );
+    SystemMiddleware.requestSystemGeneralConfig();
+    SystemMiddleware.requestVersion();
+
   }
 
   componentWillUnmount () {
@@ -49,10 +73,21 @@ export default class Dashboard extends React.Component {
       SM.unsubscribeFromPulse( this.displayName, dataSourceGroup );
     }, this );
 
+    DS.removeChangeListener( this.handleDisksChange.bind( this ) );
+    DM.unsubscribe( this.displayName );
+
     SS.removeChangeListener( this.handleDataUpdate.bind( this ) );
 
+    SystemStore.removeChangeListener( this.handleHardwareChange.bind( this ) );
     SystemMiddleware.unsubscribe( this.displayName );
-    SystemStore.removeChangeListener( this.handleStaticData.bind( this ) );
+  }
+
+  handleDisksChange () {
+    this.setState( getPredominantDisks() );
+  }
+
+  handleHardwareChange () {
+    this.setState( getSystemInformation() );
   }
 
   handleVisibilityChange ( event ) {
