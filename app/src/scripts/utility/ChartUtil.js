0825428fac8ace0a50a07b2d819edd92b59faed1
Ben Milman <bpmilman@gmail.com>
2015-10-05 18:05:57 -0700

    Add function to check statd data consistency to utility class

--- app/src/scripts/utility/ChartUtil.js
+++ app/src/scripts/utility/ChartUtil.js
@@ -11,6 +11,31 @@ class WidgetUtil {
     }
     return rtn;
   }
+
+  static validateCompleteStats ( stats , dataSources , frequency ) {
+    // make sure all the dataSources are populated
+    var allPopulated = _.all( dataSources
+                            , function ( dataSource ) {
+                                return stats[ dataSource ];
+                              }
+                            );
+
+    var timestamps = dataSources.map( function ( dataSource ) {
+                                        // timestamps come as unix time in
+                                        // milliseconds, but we test against
+                                        // time in seconds
+                                        return stats[ dataSource ][0] / 1000;
+                                      }
+                                    );
+
+    // If all the timestamps are within half the period of each other, the
+    // stats arrived close enough together to be valid.
+    var timestampsInRange = ( _.max( timestamps ) - _.min( timestamps ) )
+                            < frequency / 2;
+
+    return allPopulated && timestampsInRange;
+  }
+
 }
 
 export default WidgetUtil;
