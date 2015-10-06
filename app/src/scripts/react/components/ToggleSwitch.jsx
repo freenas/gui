427377cadc8f1c6e61503571f464d8de44a8b498
Corey Vixie <corey.vixie@gmail.com>
2015-10-05 23:29:14 -0700

    Fix ToggleSwitch

--- app/src/scripts/react/components/ToggleSwitch.jsx
+++ app/src/scripts/react/components/ToggleSwitch.jsx
@@ -7,59 +7,58 @@
 
 import React from "react";
 
-const ToggleSwitch = React.createClass(
-  { propTypes: { toggled  : React.PropTypes.bool
-               , disabled : React.PropTypes.bool
-               , onChange : React.PropTypes.func
-               , small    : React.PropTypes.string
-               , sm       : React.PropTypes.string
-    }
+export default class ToggleSwitch extends React.Component {
 
-  , getDefaultProps () {
-      return { toggled: false
-             , disabled: false
-             , onChange: function ( toggleState, reactID ) {
-                 console.warn( "No onChange handler was provided for"
-                               + " ToggleSwitch"
-                             , reactID );
-               }
-             };
-    }
+  handleToggleClick ( event, reactID ) {
+    event.stopPropagation();
+    event.preventDefault();
 
-  , handleToggleClick: function ( event, reactID ) {
-      event.stopPropagation();
-      event.preventDefault();
-
-      this.props.onChange( !this.props.toggled, reactID );
-    }
-
-  , render () {
-      var toggleClasses = [ "toggle-switch" ];
-
-      if ( this.props.toggled ) {
-        toggleClasses.push( "on" );
-      }
-
-      if ( this.props.disabled ) {
-        toggleClasses.push( "disabled" );
-      }
-
-      if ( this.props.sm || this.props.small ) {
-        toggleClasses.push( "toggle-switch-sm" );
-      }
-
-      return (
-        <div
-          className = { toggleClasses.join( " " ) }
-          onClick   = { this.props.disabled
-                      ? null
-                      : this.handleToggleClick
-                      }
-        />
-      );
-    }
-
+    this.props.onChange( !this.props.toggled, reactID );
   }
-);
 
-export default ToggleSwitch;
+  render () {
+    let toggleClasses = [ "toggle-switch" ];
+
+    if ( this.props.toggled ) {
+      toggleClasses.push( "on" );
+    }
+
+    if ( this.props.disabled ) {
+      toggleClasses.push( "disabled" );
+    }
+
+    if ( this.props.sm || this.props.small ) {
+      toggleClasses.push( "toggle-switch-sm" );
+    }
+
+    return (
+      <div
+        className = { toggleClasses.join( " " ) }
+        onClick   = { this.props.disabled
+                    ? null
+                    : this.handleToggleClick
+                    }
+      />
+    );
+  }
+
+}
+
+ToggleSwitch.propTypes =
+  { toggled  : React.PropTypes.bool
+  , disabled : React.PropTypes.bool
+  , onChange : React.PropTypes.func
+  , small    : React.PropTypes.bool
+  , sm       : React.PropTypes.bool
+  };
+
+ToggleSwitch.defaultProps =
+  { toggled: false
+  , disabled: false
+  , onChange: function ( toggleState, reactID ) {
+      console.warn( "No onChange handler was provided for"
+                  + " ToggleSwitch"
+                  , reactID
+                  );
+    }
+  };
