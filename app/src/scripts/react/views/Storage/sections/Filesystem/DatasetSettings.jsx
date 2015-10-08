e874ceae130c405745030fe692db98916e84c4b8
Corey Vixie <corey.vixie@gmail.com>
2015-10-07 21:17:09 -0700

    Dummy items in form

--- app/src/scripts/react/views/Storage/sections/Filesystem/DatasetSettings.jsx
+++ app/src/scripts/react/views/Storage/sections/Filesystem/DatasetSettings.jsx
@@ -5,6 +5,7 @@
 "use strict";
 
 import React from "react";
+import { Button, ButtonToolbar, Input } from "react-bootstrap";
 
 // TODO: Find a better way to source this
 const CONTAINER_PADDING = 15;
@@ -20,7 +21,46 @@ export default class DatasetSettings extends React.Component {
           className = "pointer"
           style     = {{ left: this.props.shiftLeft }}
         />
-        { "Settings" }
+
+
+        <section className="form-section">
+          <form className="form-horizontal">
+            <Input
+              type = "text"
+              label = "Share Name"
+              labelClassName = "col-xs-2"
+              wrapperClassName = "col-xs-10"
+            />
+            <Input
+              type = "text"
+              label = "Export Share As"
+              labelClassName = "col-xs-2"
+              wrapperClassName = "col-xs-10"
+            />
+            <Input
+              type = "checkbox"
+              label = "Allow Guest Access"
+              wrapperClassName = "col-xs-offset-2 col-xs-10"
+            />
+          </form>
+        </section>
+
+        <div className="form-handlers">
+          <ButtonToolbar>
+            <Button
+              bsStyle = "default"
+              bsSize  = "small"
+            >
+              { "Revert" }
+            </Button>
+            <Button
+              bsStyle = "primary"
+              bsSize  = "small"
+            >
+              { "Submit" }
+            </Button>
+          </ButtonToolbar>
+        </div>
       </div>
     );
   }
