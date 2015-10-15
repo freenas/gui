fc27f75542aa6b27f2f2802afd336c3225a8c71d
Corey Vixie <corey.vixie@gmail.com>
2015-10-15 15:43:34 -0700

    Add `react-motion` and use it to animate in the SID

--- app/scripts/views/App/SessionInterruptDialog.jsx
+++ app/scripts/views/App/SessionInterruptDialog.jsx
@@ -9,9 +9,10 @@
 
 import React from "react";
 import { connect } from "react-redux";
+import { Motion, spring } from "react-motion";
 
 import * as actions from "../../actions/auth";
-import Spinner from "./SessionInterruptDialog/Spinner";
+import Throbber from "../../components/Throbber";
 import Login from "./SessionInterruptDialog/Login";
 
 // STYLESHEET
@@ -21,27 +22,44 @@ if ( process.env.BROWSER ) require( "./SessionInterruptDialog.less" );
 // REACT
 class SessionInterruptDialog extends React.Component {
   render () {
-    const { auth } = this.props;
-
     return (
-      <div className="overlay-dark sid" >
-        <Spinner message = "Waiting for GUI to not suck..." />
-        <Login
-          username = { auth.username }
-          password = { auth.password }
-          onUsernameChange = { this.props.onUsernameChange }
-          onPasswordChange = { this.props.onPasswordChange }
-        />
-      </div>
+      <Motion
+        defaultStyle = {{ mainOpacity: 0 }}
+        style = {{ mainOpacity: spring( this.props.showSID ? 1 : 0 ) }}
+      >
+        { ({ mainOpacity }) =>
+          <div
+            className = "overlay-dark sid"
+            style = {
+              { opacity: mainOpacity
+              , display: mainOpacity === 0
+                       ? "none"
+                       : ""
+              }
+            }
+          >
+            <h3>{ "Foo" }</h3>
+            <Throbber />
+            <Login
+              username = { this.props.username }
+              password = { this.props.password }
+              onUsernameChange = { this.props.onUsernameChange }
+              onPasswordChange = { this.props.onPasswordChange }
+            />
+          </div>
+        }
+      </Motion>
     );
   }
 }
 
 
 // REDUX
+
 function mapStateToProps ( state ) {
-  console.log( state );
-  return { auth: state.auth };
+  return (
+    { ...state.auth }
+  );
 }
 
 function mapDispatchToProps ( dispatch ) {
