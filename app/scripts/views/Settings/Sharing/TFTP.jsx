3d51c7d5c9202f977393a6b6373dc90bff7dd788
Ben Milman <bpmilman@gmail.com>
2015-10-16 16:53:34 -0700

    Do-nothing form for TFTP

--- app/scripts/views/Settings/Sharing/TFTP.jsx
+++ app/scripts/views/Settings/Sharing/TFTP.jsx
@@ -14,10 +14,92 @@ export default class TFTP extends React.Component {
   }
 
   render () {
+
+    const toggleService = (
+      <div className = "pull-right" >
+        <ToggleSwitch
+          toggled = { true }
+        />
+      </div>
+    );
+
+    // This should be a filesystem selection component
+    /*const path = (
+      <Input
+        type =
+        label = Directory
+        value = { this.props.path }
+      />
+    );*/
+
+    const username = (
+      <Input
+        type = "select"
+        label = "username"
+        value = { this.props.username }
+      />
+    );
+
+    const allow_new_files = (
+      <Input
+        type = "checkbox"
+        label = "Allow New Files"
+        checked = { this.props.allow_new_files }
+      />
+    );
+
+    // This should be a permissions widget
+    /* const umask = (
+      <Input
+        type = "text"
+        label = "Default File Permissions"
+        value = { this.props.umask }
+      />
+    );*/
+
+    const port = (
+      <Input
+        type = "text"
+        label = "Port"
+        value = { this.props.port }
+      />
+    );
+
+    const formControlButtons = (
+      <ButtonToolbar className = "pull-right">
+        <Button
+          bsStyle = "default"
+        >
+          { "Reset" }
+        </Button>
+        <Button
+          bsStyle = "primary"
+        >
+          { "Apply" }
+        </Button>
+      </ButtonToolbar>
+    );
+
     return (
       <Panel>
         <h4>TFTP</h4>
+        { toggleService }
+        <form className = "settings-config-form">
+          { /*path*/ }
+          { username }
+          { allow_new_files }
+          { /*umask*/ }
+          { port }
+          { formControlButtons }
+        </form>
       </Panel>
     );
   }
 };
+
+TFTP.propTypes = { username: React.PropTypes.string
+                 , allow_new_files: React.PropTypes.bool
+                 , umask: React.PropTypes.string
+                 , path: React.PropTypes.string
+                 , port: React.PropTypes.number
+                 };
