3edb232ab5d9003743a195abb40073cf2d7cc659
Ben Milman <bpmilman@gmail.com>
2015-10-15 17:19:58 -0700

    Initial Sharing settings component

---
+++ app/scripts/views/Settings/Sharing.jsx
@@ -0,0 +1,51 @@
+// Sharing Service Settings
+// ========================
+// Settings for services directly related to shares: CIFS, NFS, AFP, WebDav, FTP, TFTP
+
+"use strict";
+
+import React from "react";
+import { Col } from "react-bootstrap";
+
+import CIFS from "./Sharing/CIFS";
+import NFS from "./Sharing/NFS";
+import AFP from "./Sharing/AFP";
+import WebDav from "./Sharing/WebDav";
+import FTP from "./Sharing/FTP";
+import TFTP from "./Sharing/TFTP";
+
+export default class Sharing extends React.Component {
+  constructor ( props ) {
+    super( props );
+  }
+
+  render () {
+    return (
+      <div className="view-content">
+        <section>
+          <Col xs = {4}>
+            <CIFS />
+          </Col>
+          <Col xs = {4}>
+            <NFS />
+          </Col>
+          <Col xs = {4}>
+            <AFP />
+          </Col>
+        </section>
+        <section>
+          <Col xs = {4}>
+            <WebDav />
+          </Col>
+          <Col xs = {4}>
+            <FTP />
+          </Col>
+          <Col xs = {4}>
+            <TFTP />
+          </Col>
+        </section>
+      </div>
+    );
+  }
+
+};
