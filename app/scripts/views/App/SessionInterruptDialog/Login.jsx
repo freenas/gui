14ee8af27d12a05fef163274ee8055bd9d2da0ff
Corey Vixie <corey.vixie@gmail.com>
2015-10-19 13:22:07 -0700

    Allow enter to submit login form

--- app/scripts/views/App/SessionInterruptDialog/Login.jsx
+++ app/scripts/views/App/SessionInterruptDialog/Login.jsx
@@ -10,48 +10,57 @@ import { Button, Input } from "react-bootstrap";
 
 import { ghost } from "../../../utility/motions";
 
-const Login = ( props ) => (
-  <Motion
-    defaultStyle = { props.visible ? ghost.defaultIn : ghost.defaultOut }
-    style = { props.visible ? ghost.in : ghost.out }
-  >
-    { ({ y, opacity }) =>
-      <form
-        style = {
-          { transform: `translateY( ${ y }px )`
-          , opacity
-          , display: y === -100 ? "none" : ""
+const Login = ( props ) => {
+
+  function handleInputKeyDown ( key ) {
+    if ( key === "Enter" ) props.onLoginSubmit();
+  }
+
+  return (
+    <Motion
+      defaultStyle = { props.visible ? ghost.defaultIn : ghost.defaultOut }
+      style = { props.visible ? ghost.in : ghost.out }
+    >
+      { ({ y, opacity }) =>
+        <form
+          style = {
+            { transform: `translateY( ${ y }px )`
+            , opacity
+            , display: y === -100 ? "none" : ""
+            }
           }
-        }
-      >
-        <Input
-          type        = "text"
-          placeholder = "Username"
-          className   = "form-overlay"
-          value       = { props.username }
-          onChange    = { (e) => props.onUsernameChange( e.target.value ) }
-        />
-
-        <Input
-          type        = "password"
-          placeholder = "Password"
-          className   = "form-overlay"
-          value       = { props.password }
-          onChange    = { (e) => props.onPasswordChange( e.target.value ) }
-        />
-
-        <Button
-          block
-          bsStyle  = "primary"
-          disabled = { props.username.length < 1 || !props.visible }
-          onClick  = { (e) => props.onLoginSubmit() }
         >
-          {"Sign In"}
-        </Button>
-      </form>
-    }
-  </Motion>
-);
+          <Input
+            type        = "text"
+            placeholder = "Username"
+            className   = "form-overlay"
+            value       = { props.username }
+            onKeyDown   = { (e) => handleInputKeyDown( e.key ) }
+            onChange    = { (e) => props.onUsernameChange( e.target.value ) }
+          />
+
+          <Input
+            type        = "password"
+            placeholder = "Password"
+            className   = "form-overlay"
+            value       = { props.password }
+            onKeyDown   = { (e) => handleInputKeyDown( e.key ) }
+            onChange    = { (e) => props.onPasswordChange( e.target.value ) }
+          />
+
+          <Button
+            block
+            bsStyle  = "primary"
+            disabled = { props.username.length < 1 || !props.visible }
+            onClick  = { (e) => props.onLoginSubmit() }
+          >
+            {"Sign In"}
+          </Button>
+        </form>
+      }
+    </Motion>
+  );
+};
 
 Login.propTypes =
   { visible: React.PropTypes.bool.isRequired
