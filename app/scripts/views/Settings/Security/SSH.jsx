f873f571c2bb18d99da354d9b292be1eeecebcaa
Ben Milman <bpmilman@gmail.com>
2015-10-22 23:44:43 -0700

    SSH and Security update for Redux

--- app/scripts/views/Settings/Security/SSH.jsx
+++ app/scripts/views/Settings/Security/SSH.jsx
@@ -47,120 +47,191 @@ function createDropdownOptions ( optionArray ) {
   return options;
 }
 
-export default class SSH extends React.Component {
-  constructor ( props ) {
-    super( props );
-  }
+const SSH = ( props ) => {
 
-  render() {
+  const sftpLogFacilityValue = typeof props.sshForm.sftp_log_facility
+                           !== "undefined"
+                             ? props.sshForm.sftp_log_facility
+                             : props.sshServerState.sftp_log_facility;
+  const permitRootLoginValue = typeof props.sshForm.permit_root_login
+                           !== "undefined"
+                             ? props.sshForm.permit_root_login
+                             : props.sshServerState.permit_root_login;
+  const compressionValue = typeof props.sshForm.compression
+                       !== "undefined"
+                         ? props.sshForm.compression
+                         : props.sshServerState.compression;
+  const sftpLogLevelValue = typeof props.sshForm.sftp_log_level
+                        !== "undefined"
+                          ? props.sshForm.sftp_log_level
+                          : props.sshServerState.sftp_log_level;
+  const allowPasswordAuthValue = typeof props.sshForm.allow_password_auth
+                             !== "undefined"
+                               ? props.sshForm.allow_password_auth
+                               : props.sshServerState.allow_password_auth;
+  const allowPortForwardingValue = typeof props.sshForm.allow_port_forwarding
+                               !== "undefined"
+                                 ? props.sshForm.allow_port_forwarding
+                                 : props.sshServerState.allow_port_forwarding;
+  const portValue = typeof props.sshForm.port
+                !== "undefined"
+                  ? props.sshForm.port
+                  : props.sshServerState.port;
 
-    const toggleService = (
-      <div className = "pull-right" >
-        <ToggleSwitch
-          toggled = { true }
-        />
-      </div>
-    );
+  const toggleService = (
+    <div className = "pull-right" >
+      <ToggleSwitch
+        toggled = { true }
+      />
+    </div>
+  );
 
-    const tcpPort = (
-      <Input
-        type = "text"
-        label = "TCP Port"
-        value = { this.props.port }
+  const port = (
+    <Input
+      type = "text"
+      label = "TCP Port"
+      value = { portValue }
+      onChange = { ( e ) => props.updateSSHForm( "port", e.target.value ) }
       />
     );
 
-    const portForwarding = (
-      <Input
-        type = "checkbox"
-        label = "TCP Port Forwarding"
-        checked = { this.props.allow_port_forwarding }
-      />
-    );
+  const allowPortForwarding = (
+    <Input
+      type = "checkbox"
+      label = "TCP Port Forwarding"
+      checked = { allowPortForwardingValue }
+      onChange = { ( e ) => props.updateSSHForm( "allow_port_forwarding"
+                                               , e.target.checked
+                                               )
+                 }
+    />
+  );
 
-    const rootPasswordLogin = (
-      <Input
-        type = "checkbox"
-        label = "Login as root with password"
-        checked = { this.props.permit_root_login }
-      />
-    );
+  const permitRootLogin = (
+    <Input
+      type = "checkbox"
+      label = "Login as root with password"
+      checked = { permitRootLoginValue }
+      onChange = { ( e ) => props.updateSSHForm( "permit_root_login"
+                                               , e.target.checked
+                                               )
+                 }
+    />
+  );
 
-    const passwordAuthentication = (
-      <Input
-        type = "checkbox"
-        label = "Allow Password Authentication"
-        checked = { this.props.allow_password_auth }
-      />
-    );
+  const allowPasswordAuth = (
+    <Input
+      type = "checkbox"
+      label = "Allow Password Authentication"
+      checked = { allowPasswordAuthValue }
+      onChange = { ( e ) => props.updateSSHForm( "allow_password_auth"
+                                               , e.target.checked
+                                               )
+                 }
+    />
+  );
 
-    const compressConnection = (
-      <Input
-        type = "checkbox"
-        label = "Compress Connection"
-        checked = { this.props.compression }
-      />
-    );
+  const compression = (
+    <Input
+      type = "checkbox"
+      label = "Compress Connection"
+      checked = { compression }
+      onChange = { ( e ) => props.updateSSHForm( "compression"
+                                               , e.target.checked
+                                               )
+                 }
+    />
+  );
 
-    const stfpLogLevel = (
-      <Input
-        type = "select"
-        label = "SFTP Log Level"
-        value = { this.props.sftp_log_level }
+  const sftpLogLevel = (
+    <Input
+      type = "select"
+      label = "SFTP Log Level"
+      value = { sftpLogLevelValue }
+      onChange = { ( e ) => props.updateSSHForm( "sftp_log_level"
+                                               , e.target.value
+                                               )
+                 }
+    >
+      { createDropdownOptions( LOG_LEVELS ) }
+    </Input>
+  );
+
+  const sftpLogFacility = (
+    <Input
+      type = "select"
+      label = "SFTP Log Facility"
+      value = { sftpLogFacilityValue }
+      onChange = { ( e ) => props.updateSSHForm( "sftp_log_facility"
+                                               , e.target.value
+                                               )
+                 }
+    >
+      { createDropdownOptions( LOG_FACILITIES ) }
+    </Input>
+  );
+
+  const formControlButtons = (
+    <ButtonToolbar className = "pull-right">
+      <Button
+        bsStyle = "default"
+        onClick = { props.resetSSHForm }
+        disabled = { Object.keys( props.sshForm ).length === 0 }
       >
-        { createDropdownOptions( LOG_LEVELS ) }
-      </Input>
-    );
-
-    const stfpLogFacility = (
-      <Input
-        type = "select"
-        label = "SFTP Log Facility"
-        value = { this.props.sftp_log_facility }
+        { "Reset" }
+      </Button>
+      <Button
+        bsStyle = "primary"
+        onClick = { props.submitSSHForm }
+        disabled = { Object.keys( props.sshForm ).length === 0 }
       >
-        { createDropdownOptions( LOG_FACILITIES ) }
-      </Input>
-    );
+        { "Apply" }
+      </Button>
+    </ButtonToolbar>
+  );
 
-    const formControlButtons = (
-      <ButtonToolbar className = "pull-right">
-        <Button
-          bsStyle = "default"
-        >
-          { "Reset" }
-        </Button>
-        <Button
-          bsStyle = "primary"
-        >
-          { "Apply" }
-        </Button>
-      </ButtonToolbar>
-    );
-
-    return (
-      <Panel>
-        <h4>SSH</h4>
-        { toggleService }
-        <form className = "settings-config-form">
-          { tcpPort }
-          { portForwarding }
-          { rootPasswordLogin }
-          { passwordAuthentication }
-          { compressConnection }
-          { stfpLogLevel }
-          { stfpLogFacility }
-          { formControlButtons }
-        </form>
-      </Panel>
-    );
-  }
+  return (
+    <Panel>
+      <h4>SSH</h4>
+      { toggleService }
+      <form className = "settings-config-form">
+        { port }
+        { allowPortForwarding }
+        { permitRootLogin }
+        { allowPasswordAuth }
+        { compression }
+        { sftpLogLevel }
+        { sftpLogFacility }
+        { formControlButtons }
+      </form>
+    </Panel>
+  );
 };
 
-SSH.propTypes = { sftp_log_facility: React.PropTypes.oneOf( LOG_FACILITIES )
-                , permit_root_login: React.PropTypes.bool
-                , compression: React.PropTypes.bool
-                , sftp_log_level: React.PropTypes.oneOf( LOG_LEVELS )
-                , allow_password_auth: React.PropTypes.bool
-                , allow_port_forwarding: React.PropTypes.bool
-                , port: React.PropTypes.number
+SSH.propTypes = { sshServerState: React.PropTypes.shape(
+                  { sftp_log_facility: React.PropTypes.oneOf( LOG_FACILITIES )
+                  , permit_root_login: React.PropTypes.bool
+                  , compression: React.PropTypes.bool
+                  , sftp_log_level: React.PropTypes.oneOf( LOG_LEVELS )
+                  , allow_password_auth: React.PropTypes.bool
+                  , allow_port_forwarding: React.PropTypes.bool
+                  , port: React.PropTypes.number
+                  }
+                )
+                , sshForm: React.PropTypes.shape(
+                  { sftp_log_facility: React.PropTypes.oneOf( LOG_FACILITIES )
+                  , permit_root_login: React.PropTypes.bool
+                  , compression: React.PropTypes.bool
+                  , sftp_log_level: React.PropTypes.oneOf( LOG_LEVELS )
+                  , allow_password_auth: React.PropTypes.bool
+                  , allow_port_forwarding: React.PropTypes.bool
+                  , port: React.PropTypes.number
+                  }
+                )
+                , sshConfigRequests: React.PropTypes.instanceOf( Set )
+                , updateSSHForm: React.PropTypes.func.isRequired
+                , resetSSHForm: React.PropTypes.func
+                // , submitSSHForm: React.PropTypes.func
                 };
+
+export default SSH;
