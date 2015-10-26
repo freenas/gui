afc23a8e66d8f4dd20a9e1b72abb9ce4790a0ee8
Ben Milman <bpmilman@gmail.com>
2015-10-25 23:56:49 -0700

    Reduce style horrors: Users Edition

--- app/scripts/views/Accounts/Users/UserView.jsx
+++ app/scripts/views/Accounts/Users/UserView.jsx
@@ -26,7 +26,7 @@ const UserView = React.createClass(
       var group = GroupsStore.getGroup( groupID );
 
       if ( group ) {
-        return group[ "name" ];
+        return group.name;
       } else {
         // TODO: Have a policy to deal with a user assigned to a
         // nonexistant group.
@@ -37,30 +37,36 @@ const UserView = React.createClass(
   , createGroupDisplayList: function () {
       var listGroupItemArray = [] ;
 
-      listGroupItemArray = _.map( this.props.item[ "groups" ]
-                                 , function ( groupID ) {
-        var displayItem = null;
-        var group = GroupsStore.getGroup( groupID );
+      listGroupItemArray = this.props.item.groups.map(
+                             function ( groupID ) {
+                               var displayItem = null;
+                               var group = GroupsStore.getGroup( groupID );
 
-        if ( group ) {
-          displayItem = <ListGroupItem>{ group["name"] }
-            </ListGroupItem>;
-        }
+                               if ( group ) {
+                                 displayItem =
+                                   <ListGroupItem>
+                                     { group.name }
+                                   </ListGroupItem>;
+                               }
 
-        return displayItem;
-      }, this );
+                               return displayItem;
+                             }
+                           , this
+                           );
 
       return listGroupItemArray;
     }
 
   , render: function () {
       var builtInUserAlert = null;
-      var editButtons      = null;
+      var editButtons = null;
 
-      if ( this.props.item["builtin"] ) {
+      if ( this.props.item.builtin ) {
         builtInUserAlert = (
-          <Alert bsStyle   = "info"
-                      className = "text-center">
+          <Alert
+            bsStyle = "info"
+            className = "text-center"
+          >
             <b>{"This is a built-in FreeNAS user account."}</b>
           </Alert>
         );
@@ -68,13 +74,21 @@ const UserView = React.createClass(
 
       editButtons = (
         <ButtonToolbar>
-            <Button className = "pull-left"
-                         disabled  = { this.props.item["builtin"] }
-                         onClick   = { this.deleteUser }
-                         bsStyle   = "danger" >{"Delete User"}</Button>
-            <Button className = "pull-right"
-                         onClick   = { this.props.handleViewChange.bind( null, "edit" ) }
-                         bsStyle   = "info" >{"Edit User"}</Button>
+            <Button
+              className = "pull-left"
+              disabled = { this.props.item.builtin }
+              onClick = { this.deleteUser }
+              bsStyle = "danger"
+            >
+              {"Delete User"}
+            </Button>
+            <Button
+              className = "pull-right"
+              onClick = { this.props.handleViewChange.bind( null, "edit" ) }
+              bsStyle = "info"
+            >
+              {"Edit User"}
+            </Button>
         </ButtonToolbar>
       );
 
@@ -85,17 +99,25 @@ const UserView = React.createClass(
 
           {/* User icon and general information */}
           <Row>
-            <Col xs={3}
-                      className="text-center">
-              <viewerUtil.ItemIcon primaryString   = { this.props.item["full_name"] }
-                                   fallbackString  = { this.props.item["username"] }
-                                   iconImage       = { this.props.item["user_icon"] }
-                                   seedNumber      = { this.props.item["id"] } />
+            <Col
+              xs={3}
+              className="text-center"
+            >
+              <viewerUtil.ItemIcon
+                primaryString = { this.props.item.full_name }
+                fallbackString = { this.props.item.username }
+                iconImage = { this.props.item.user_icon }
+                seedNumber = { this.props.item.id }
+              />
             </Col>
             <Col xs={9}>
-              <h3>{ this.props.item["username"] }</h3>
-              <h4 className="text-muted">{ viewerUtil.writeString( this.props.item["full_name"], "\u200B" ) }</h4>
-              <h4 className="text-muted">{ viewerUtil.writeString( this.props.item["email"], "\u200B" ) }</h4>
+              <h3>{ this.props.item.username }</h3>
+              <h4 className="text-muted">
+                { viewerUtil.writeString( this.props.item.full_name, "\u200B" ) }
+              </h4>
+              <h4 className="text-muted">
+                { viewerUtil.writeString( this.props.item.email, "\u200B" ) }
+              </h4>
               <hr />
             </Col>
           </Row>
@@ -105,35 +127,53 @@ const UserView = React.createClass(
 
           {/* Primary user data overview */}
           <Row>
-            <viewerUtil.DataCell title  = { "User ID" }
-                                 colNum = { 3 }
-                                 entry  = { this.props.item["id"] } />
-            <viewerUtil.DataCell title  = { "Primary Group" }
-                                 colNum = { 3 }
-                                 entry  = { this.getGroupName( this.props.item["group"] ) } />
-            <viewerUtil.DataCell title  = { "Shell" }
-                                 colNum = { 3 }
-                                 entry  = { this.props.item["shell"] } />
-            <viewerUtil.DataCell title  = { "Locked Account" }
-                                 colNum = { 3 }
-                                 entry  = { this.props.item["locked"] ? this.props.item["locked"] : false } />
-            <viewerUtil.DataCell title  = { "Sudo Access" }
-                                 colNum = { 3 }
-                                 entry  = { this.props.item["sudo"] ? this.props.item["sudo"]: false } />
-            <viewerUtil.DataCell title  = { "Password Disabled" }
-                                 colNum = { 3 }
-                                 entry  = { this.props.item["password_disabled"] ? this.props.item["password_disabled"]: false } />
-            <viewerUtil.DataCell title  = { "Home Directory" }
-                                 colNum = { 3 }
-                                 entry  = { this.props.item["home"] } />
-            <viewerUtil.DataCell title  = { "email" }
-                                 colNum = { 3 }
-                                 entry  = { this.props.item["email"] ? this.props.item["email"]: "" } />
+            <viewerUtil.DataCell
+              title = { "User ID" }
+              colNum = { 3 }
+              entry = { this.props.item.id }
+            />
+            <viewerUtil.DataCell
+              title = { "Primary Group" }
+              colNum = { 3 }
+              entry = { this.getGroupName( this.props.item.group ) }
+            />
+            <viewerUtil.DataCell
+              title = { "Shell" }
+              colNum = { 3 }
+              entry = { this.props.item.shell }
+            />
+            <viewerUtil.DataCell
+              title = { "Locked Account" }
+              colNum = { 3 }
+              entry = { this.props.item.locked ? this.props.item.locked : false }
+            />
+            <viewerUtil.DataCell
+              title = { "Sudo Access" }
+              colNum = { 3 }
+              entry = { this.props.item.sudo ? this.props.item.sudo: false }
+            />
+            <viewerUtil.DataCell
+              title = { "Password Disabled" }
+              colNum = { 3 }
+              entry = { this.props.item.password_disabled ? this.props.item.password_disabled: false }
+            />
+            <viewerUtil.DataCell
+              title = { "Home Directory" }
+              colNum = { 3 }
+              entry = { this.props.item.home }
+            />
+            <viewerUtil.DataCell
+              title = { "email" }
+              colNum = { 3 }
+              entry = { this.props.item.email ? this.props.item.email: "" }
+            />
             <Col
               xs = {12}
               className ="text-muted"
             >
-              <h4 className = "text-muted" >{ "Other Groups" }</h4>
+              <h4 className = "text-muted" >
+                { "Other Groups" }
+              </h4>
               <ListGroup>
                 { this.createGroupDisplayList() }
               </ListGroup>
