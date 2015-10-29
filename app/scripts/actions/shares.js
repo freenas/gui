2030969109dc5d759160525ccf0937e4d5490757
Corey Vixie <corey.vixie@gmail.com>
2015-10-29 16:59:56 -0700

    Stub in Redux actions for Shares CRUD

--- app/scripts/actions/shares.js
+++ app/scripts/actions/shares.js
@@ -16,3 +16,30 @@ export function fetchShares () {
               );
   }
 }
+
+export function createShare ( share ) {
+  return ( dispatch, getState ) => {
+    MC.submitTask( [ "share.create", [ share ] ]
+                 , ( UUID ) =>
+                   dispatch( watchRequest( UUID, TYPES.CREATE_SHARE_TASK_SUBMIT_REQUEST ) )
+                 );
+  }
+}
+
+export function updateShare ( name ) {
+  return ( dispatch, getState ) => {
+    MC.submitTask( [ "share.update", [ name ] ]
+                 , ( UUID ) =>
+                   dispatch( watchRequest( UUID, TYPES.UPDATE_SHARE_TASK_SUBMIT_REQUEST ) )
+                 );
+  }
+}
+
+export function deleteShare ( name ) {
+  return ( dispatch, getState ) => {
+    MC.submitTask( [ "share.delete", [ name ] ]
+                 , ( UUID ) =>
+                   dispatch( watchRequest( UUID, TYPES.DELETE_SHARE_TASK_SUBMIT_REQUEST ) )
+                 );
+  }
+}
