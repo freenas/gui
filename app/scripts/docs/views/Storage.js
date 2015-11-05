885ebedd1d38a0bc256195fe329308e4a0610c27
dlavigne <dru@freebsd.org>
2015-11-05 08:44:47 -0500

    Replace garbage text with a placeholder.

--- app/scripts/docs/views/Storage.js
+++ app/scripts/docs/views/Storage.js
@@ -5,17 +5,8 @@ import React from "react";
 
 export const STORAGE_GENERAL =
   <div>
-    <h4>Storage Help</h4>
+    <h4>Storage</h4>
     <p>
-    Marketing refrigerator physical computer wonton soup 3D-printed systemic
-    kanji semiotics DIY city fetishism knife drone tank-traps. Cardboard denim
-    dolphin order-flow shanty town cyber--ware assassin realism long-chain
-    hydrocarbons narrative.
-    </p>
-    <p>
-    Rebar carbon construct decay monofilament fetishism singularity tube realism
-    courier camera faded modem vinyl math. Chiba human face forwards otaku
-    3D-printed carbon gang numinous post-Tokyo media shanty town A.I. katana.
-    Uplink dead knife engine shanty town dissident bomb neural dome shrine post.
+    Placeholder for storage help.
     </p>
   </div>;
