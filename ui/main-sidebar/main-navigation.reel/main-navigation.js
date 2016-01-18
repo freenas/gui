084e82b63c6823693682b9c404c7fa35e78d7442
joshuarule <joshua@joshuarule.com>
2016-01-18 11:04:33 -0800

    add placeholder switching for main substitution

--- ui/main-sidebar/main-navigation.reel/main-navigation.js
+++ ui/main-sidebar/main-navigation.reel/main-navigation.js
@@ -4,4 +4,12 @@ var Component = require("montage/ui/component").Component;
  * @class MainNavigation
  * @extends Component
  */
-exports.MainNavigation = Component.specialize();
+exports.MainNavigation = Component.specialize({
+    handlePreferencesAction: {
+        value: function() {
+            this.application.section = 'preferences';
+        }
+    }
+});
+
+// FIXME: Selection needs to be managed by a selection controller
--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -4,4 +4,12 @@ var Component = require("montage/ui/component").Component;
  * @class Main
  * @extends Component
  */
-exports.Main = Component.specialize();
+exports.Main = Component.specialize({
+    enterDocument: {
+        value: function() {
+            this.application.section = 'storage';
+        }
+    }
+});
+
+// FIXME: Selection needs to be managed by a selection controller
