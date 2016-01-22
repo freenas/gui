03f7b157aefd65203b585af90c2e848e875ac812
Thibault Zanini <thibaultzanini@gmail.com>
2016-01-21 16:51:47 -0800

    Add Application delegate.

---
+++ core/application-delegate.js
@@ -0,0 +1,21 @@
+/*global require, exports, Error*/
+var Montage = require("montage").Montage;
+
+exports.ApplicationDelegate = Montage.specialize({
+
+
+    /**
+     * @function
+     * @public
+     *
+     * @description todo
+     *
+     */
+    willFinishLoading: {
+        value: function (app) {
+            //Todo
+        }
+    }
+
+
+});
