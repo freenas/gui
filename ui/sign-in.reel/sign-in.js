2fbb272bf7df5662e32b48b2f89d19a9269dbfeb
joshuarule <joshua@joshuarule.com>
2016-01-27 15:47:25 -0500

    markup and styles for sign-in modal, base button and input styles added to global styles, added sign-in component to main.reel and turned it on for development purposes

--- ui/sign-in.reel/sign-in.js
+++ ui/sign-in.reel/sign-in.js
@@ -1,8 +1,30 @@
-var Component = require("montage/ui/component").Component;
+var ModalOverlay = require("montage/ui/modal-overlay.reel").ModalOverlay,
+    Overlay = require("montage/ui/overlay.reel").Overlay;
+
+ModalOverlay.prototype.enterDocument = Overlay.prototype.enterDocument;
+ModalOverlay.prototype.draw = Overlay.prototype.draw;
 
 /**
  * @class SignIn
  * @extends Component
  */
-exports.SignIn = Component.specialize({
+exports.SignIn = ModalOverlay.specialize({
+
+    // $fix - for development purpose
+    enterDocument: {
+        value: function (firsTime) {
+            Overlay.prototype.enterDocument.call(this, firsTime);
+
+            if (firsTime) {
+                this.show();
+            }
+        }
+    },
+
+    handleSubmitAction: {
+        value: function() {
+            this.hide();
+        }
+    }
+
 });
