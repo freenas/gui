5850219f42bfad930a418b610ec15d6b43c7808c
Thibault Zanini <thibaultzanini@gmail.com>
2016-02-02 12:11:59 -0800

    Connect the login panel with the middleware.

--- ui/sign-in.reel/sign-in.js
+++ ui/sign-in.reel/sign-in.js
@@ -15,14 +15,77 @@ exports.SignIn = AuthorizationPanel.specialize({
         value: void 0
     },
 
+    submitButton: {
+        value: void 0
+    },
+
+    passwordTextField: {
+        value: void 0
+    },
+
+    userNameTextField: {
+        value: void 0
+    },
+
+    hasError: {
+        value: false
+    },
+
+    _errorMessage: {
+        value: null
+    },
+
+    errorMessage: {
+        get: function () {
+            return this._errorMessage;
+        },
+        set: function (errorMessage) {
+            this._errorMessage = errorMessage;
+            this.hasError = !!errorMessage;
+        }
+    },
+
+    _isAuthenticating: {
+        value: false
+    },
+
+    isAuthenticating: {
+        set: function (isAuthenticating) {
+            if (this._isAuthenticating !== isAuthenticating) {
+                this._isAuthenticating = isAuthenticating;
+                this._toggleUserInteraction();
+            }
+        },
+        get: function () {
+            return this._isAuthenticating;
+        }
+    },
+
     handleSubmitAction: {
         value: function() {
+            if (!this._isAuthenticating) {
+                this.isAuthenticating = true;
 
-            var promise = this.dataService.loginWithCredentials(this.userName, this.password).then(function(authorization){
-                this.authorizationManagerPanel.approveAuthorization(authorization);
-            }, function(error) {
-                this.authorizationManagerPanel.cancelAuthorization(error);
-            });
+                var self = this;
+
+                this.dataService.loginWithCredentials(this.userName, this.password).then(function (authorization) {
+                    self.authorizationManagerPanel.approveAuthorization(authorization);
+
+                }, function (error) {
+                    self.errorMessage = error.message || error;
+
+                }).finally(function () {
+                    self.isAuthenticating = false;
+                });
+            }
+        }
+    },
+
+    _toggleUserInteraction: {
+        value: function () {
+            this.submitButton.enabled =
+                this.passwordTextField.enabled =
+                    this.userNameTextField.enabled = !this._isAuthenticating;
         }
     }
 
