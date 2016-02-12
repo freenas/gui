431426edcf37428c2268d36c996153e961f9c804
joshuarule <joshua@joshuarule.com>
2016-02-12 14:14:25 -0500

    Updating styles for working with cascading list, working on network cascading list, and storage

--- ui/storage/volumes.reel/volumes.js
+++ ui/storage/volumes.reel/volumes.js
@@ -14,6 +14,7 @@ exports.Volumes = Component.specialize({
                 data = [
                     {
                         name: "Volume 1",
+                        icon: "ui/icons/volume.reel",
                         size: "2 TB",
                         inspector: "ui/inspectors/volume.reel",
                         shares: [
