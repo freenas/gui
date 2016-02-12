85a5a3dfc3b6d4ceed3311da1b66ca633d01245e
Ben Milman <bpmilman@gmail.com>
2016-02-11 19:51:01 -0800

    Update interface-alias with more computed data and rearrage example data

--- ui/inspectors/interface-alias.reel/interface-alias.js
+++ ui/inspectors/interface-alias.reel/interface-alias.js
@@ -26,6 +26,60 @@ var Component = require("montage/ui/component").Component;
     }
 ]*/
 
+/* The current example alias. */
+var _exampleAlias = {
+    "netmask": 24,
+    "address": "10.0.1.169",
+    "broadcast": "10.0.1.255",
+    "type": "INET",
+    "source": "dhcp"
+};
+
+/* Based on the source of the alias, return whether or not to display the input
+ * field for modifying the alias. All the auto-generated fields return false,
+ * to make it easier to use them with a single Condition component.
+ */
+function displayAliasInputField ( source ) {
+    var shouldDisplay;
+
+    switch ( source ) {
+
+        case "manual":
+            shouldDisplay = true;
+            break;
+
+        case "dhcp":
+        case "link-local":
+        case "rtadv":
+        default:
+            shouldDisplay = false;
+            break;
+    }
+    return shouldDisplay;
+}
+
+/* Based on the source of the alias, return "auto" or "manual". Collapses
+ * all the auto-generated cases into one for use with a Substitution. */
+function collapseAutomaticAliasSources ( source ) {
+    var sourceType;
+
+    switch ( source ) {
+
+        case "manual":
+            sourceType = "manual";
+            break;
+
+        case "dhcp":
+        case "link-local":
+        case "rtadv":
+        default:
+            sourceType = "auto";
+            break;
+    }
+
+    return sourceType;
+}
+
 // This is hardcoded. There will never be any other options.
 var _ALIAS_TYPE_OPTIONS = [
     {"label": "IPv4", "value": "INET"},
@@ -38,15 +92,18 @@ var _ALIAS_TYPE_OPTIONS = [
  */
 exports.InterfaceAlias = Component.specialize({
     "object": {
-        "value": {
-            "netmask": 24,
-            "address": "10.0.1.169",
-            "broadcast": "10.0.1.255",
-            "type": "INET",
-            "source": "manual"
-        }
+        "value": _exampleAlias
     },
-    aliasTypeOptions: {
+    "aliasTypeOptions": {
         "value": _ALIAS_TYPE_OPTIONS
+    },
+    "displayInput": {
+        "value": displayAliasInputField( _exampleAlias.source )
+    },
+    "sourceType": {
+        "value": collapseAutomaticAliasSources( _exampleAlias.source )
+    },
+    "displayBroadcast": {
+        "value": _exampleAlias.type === "INET"
     }
 });
