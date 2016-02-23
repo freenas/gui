b0e668d64fe91686ca34c5dac792d5507562e294
Ben Milman <bpmilman@gmail.com>
2016-02-22 19:02:44 -0800

    Add initial network configuration component with dummy data

--- ui/inspectors/network-configuration.reel/network-configuration.js
+++ ui/inspectors/network-configuration.reel/network-configuration.js
@@ -8,9 +8,35 @@ var Component = require("montage/ui/component").Component;
  * @extends Component
  */
 exports.NetworkConfiguration = Component.specialize(/** @lends NetworkConfiguration# */ {
-    constructor: {
-        value: function NetworkConfiguration() {
-            this.super();
+    _object: {
+        value: {
+            "autoconfigure": false,
+            "dns": {
+                "addresses": [],
+                "search": []
+            },
+            "netwait": {
+                "addresses": [],
+                "enabled": false
+            },
+            "http_proxy": null,
+            "dhcp": {
+                "assign_gateway": true,
+                "assign_dns": true
+            },
+            "gateway": {
+                "ipv4": null,
+                "ipv6": null
+            }
+        }
+    },
+
+    object: {
+        set: function (networkConfig) {
+            // TODO
+        },
+        get: function () {
+            return this._object;
         }
     }
 });
