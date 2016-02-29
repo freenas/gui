480c7f46d9ab55a78acbd1800bb9ecc6298aff70
joshuarule <joshua@joshuarule.com>
2016-02-29 11:34:28 -0500

    adds placeholder component for related interfaces on static interfaces

---
+++ ui/inspectors/related-interfaces.reel/related-interfaces.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/related-interfaces.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class RelatedInterfaces
+ * @extends Component
+ */
+exports.RelatedInterfaces = Component.specialize(/** @lends RelatedInterfaces# */ {
+    constructor: {
+        value: function RelatedInterfaces() {
+            this.super();
+        }
+    }
+});
