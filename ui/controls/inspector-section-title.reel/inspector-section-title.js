36d61de5a90850c19fea4818d49e06f7aa9ffc03
joshuarule <joshua@joshuarule.com>
2016-02-29 11:33:50 -0500

    adds inspector-section-title

---
+++ ui/controls/inspector-section-title.reel/inspector-section-title.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/inspector-section-title.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class InspectorSectionTitle
+ * @extends Component
+ */
+exports.InspectorSectionTitle = Component.specialize(/** @lends InspectorSectionTitle# */ {
+    constructor: {
+        value: function InspectorSectionTitle() {
+            this.super();
+        }
+    }
+});
