f2d7f2b1b96d371d64511ec6008e041c48b002b2
joshuarule <joshua@joshuarule.com>
2016-03-01 18:53:18 -0500

    adding component for svg icons

---
+++ ui/icons.reel/icons.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/icons.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Icons
+ * @extends Component
+ */
+exports.Icons = Component.specialize(/** @lends Icons# */ {
+    constructor: {
+        value: function Icons() {
+            this.super();
+        }
+    }
+});
