b4083584469e82378fa405d50f4a4ae7174e836e
Ben Milman <bpmilman@gmail.com>
2016-03-03 19:01:36 -0800

    Populate the user component

--- ui/inspectors/user.reel/user.js
+++ ui/inspectors/user.reel/user.js
@@ -1,7 +1,40 @@
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    Model = require("core/model/model").Model;
 
 /**
  * @class User
  * @extends Component
  */
-exports.User = Component.specialize();
+exports.User = Component.specialize({
+
+    _object: {
+        value: null
+    },
+
+    object: {
+        set: function (user) {
+            if (user) {
+                this._object = user;
+
+                this.application.dataService.fetchData(Model.Group).then(function (groups) {
+                    if (groups) {
+                        this.groupOptions = groups;
+                    } else {
+                        this.groupOptions = null;
+                    }
+                }.bind(this));
+
+            } else {
+                this._object = null;
+            }
+        },
+
+        get: function () {
+            return this._object;
+        }
+    },
+
+    groupOptions: {
+        value: null
+    }
+});
