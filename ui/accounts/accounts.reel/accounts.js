efdc27cc0bfefd9c4b009b7cab271a17007c47a4
Ben Milman <bpmilman@gmail.com>
2016-03-03 19:04:38 -0800

    Add Users cascading list to Accounts

--- ui/accounts/accounts.reel/accounts.js
+++ ui/accounts/accounts.reel/accounts.js
@@ -1,7 +1,41 @@
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    Model = require("core/model/model").Model;
 
 /**
  * @class Accounts
  * @extends Component
  */
-exports.Accounts = Component.specialize();
+exports.Accounts = Component.specialize({
+
+    enterDocument: {
+        value: function (isFirstTime) {
+            var self = this;
+
+            this.listUsers().then(function (users) {
+                self.users = users;
+            });
+        }
+    },
+
+    listUsers: {
+        value: function() {
+            return this.application.dataService.fetchData(Model.User).then(function (users) {
+                users.inspector = "ui/controls/viewer.reel";
+                users.name = "Users";
+
+                var user;
+
+                for (var i = 0, length = users.length; i < length; i++) {
+                    user = users[i];
+
+                    user.name = user.username;
+                    user.inspector = "ui/inspectors/user.reel";
+                    user.icon = "ui/icons/user.reel";
+                }
+
+                return users;
+            });
+        }
+    },
+
+});
