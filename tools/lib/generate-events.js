5f5853b027c45c172fd2bdbcf7bdacdfb0b7b333
Thibault Zanini <thibaultzanini@gmail.com>
2016-03-07 17:01:48 -0800

    Update montage-data cache to the version 201603071846 of the middleware.

--- tools/lib/generate-events.js
+++ tools/lib/generate-events.js
@@ -2,6 +2,7 @@ var FS = require('./fs-promise');
 var Service = require('./backend/service');
 var Promise = require('montage/core/promise').Promise;
 var Path = require('path');
+require('json.sortify');
 
 
 exports.generateEventTypesForEntities = function generateEventTypesForEntities (options) {
@@ -13,10 +14,10 @@ exports.generateEventTypesForEntities = function generateEventTypesForEntities (
                         var targetPath = Path.join(absoluteTarget, "events.mjson");
 
                         if (options.verbose) {
-                            console.log(JSON.stringify(eventTypesForEntities, null, 4));
+                            console.log(JSON.sortify(eventTypesForEntities, null, 4));
                         }
 
-                        return FS.writeFileAtPathWithData(targetPath, JSON.stringify(eventTypesForEntities, null, 4));
+                        return FS.writeFileAtPathWithData(targetPath, JSON.sortify(eventTypesForEntities, null, 4));
                     } else {
                         throw new Error("not a directory");
                     }
