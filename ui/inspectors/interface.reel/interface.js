c282010aea52990c31ccd14236fa9b9a7434c151
Ben Milman <bpmilman@gmail.com>
2016-03-09 17:42:15 -0800

    Add Static IP and Alias fields to Ethernet interfaces

--- ui/inspectors/interface.reel/interface.js
+++ ui/inspectors/interface.reel/interface.js
@@ -1,4 +1,6 @@
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    NetworkInterfaceType = require("core/model/enumerations/network-interface-type").NetworkInterfaceType,
+    Model = require("core/model/model").Model;
 
 /**
  * @class Interface
@@ -6,7 +8,69 @@ var Component = require("montage/ui/component").Component;
  */
 exports.Interface = Component.specialize({
 
-    object: {
+    _object: {
         value: null
+    },
+
+    dhcpAlias: {
+        value: null
+    },
+
+    staticIP: {
+        value: null
+    },
+
+    otherAliases: {
+        value: null
+    },
+
+    ipAddressSource: {
+        value: null
+    },
+
+    constructor: {
+        value: function() {
+            this.super();
+            this.dhcpAlias =
+                {address: "",
+                 netmask: null,
+                 type: "INET" };
+        }
+    },
+
+    object: {
+        set: function (networkInterface) {
+            if (networkInterface && networkInterface.type === NetworkInterfaceType.ETHER) {
+                this._object = networkInterface;
+
+                if (networkInterface) {
+                    if (networkInterface.dhcp) {
+                        this.ipAddressSource = "dhcp";
+                        // The first and only ipv4 address in the read-only aliases is
+                        // always the one assigned by dhcp if dhcp is enabled.
+                        // Otherwise the one pre-set in the inspector applies.
+                        for (var i = 0, length = networkInterface.status.aliases.length; i < length; i++ ) {
+                            if (networkInterface.status.aliases[i].type === "INET") {
+                                this.dhcpAlias = networkInterface.status.aliases[i];
+                                break;
+                            }
+                        }
+                    } else {
+                        this.ipAddressSource = "static";
+                    }
+                    // This always applies, in case they switch off DHCP
+                    this.staticIP = networkInterface.aliases.slice(0, 1);
+                    this.otherAliases = networkInterface.aliases.slice(1);
+                }
+            } else {
+                this._object = null;
+            }
+        },
+        get: function () {
+            var newObject = new Object(this._object);
+            newObject.aliases = this.staticIP.concat(this.otherAliases).filter(function(alias) {return !!alias});
+            return newObject;
+        }
     }
+
 });
