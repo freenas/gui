d29620fa9e104b9d2b8aecd999720524bbe97e27
Ben Milman <bpmilman@gmail.com>
2016-03-10 17:52:34 -0800

    Activate bridge interface editing and deletion

--- ui/inspectors/bridge.reel/bridge.js
+++ ui/inspectors/bridge.reel/bridge.js
@@ -32,6 +32,18 @@ var Bridge = exports.Bridge = Component.specialize({
         value: null
     },
 
+    save: {
+        value: function() {
+            this.application.dataService.saveDataObject(this.object);
+        }
+    },
+
+    delete: {
+        value: function() {
+            this.application.dataService.deleteDataObject(this.object);
+        }
+    },
+
     constructor: {
         value: function() {
             this.super();
