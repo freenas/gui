a2363a2294a9debb6c804c37fb8ca1b85ebe74bc
joshuarule <joshua@joshuarule.com>
2016-03-11 19:37:22 -0500

    add interface-overview to interfaces, removing status indicators until they can work on interface icon

--- ui/inspectors/interface.reel/interface.js
+++ ui/inspectors/interface.reel/interface.js
@@ -1,66 +1,12 @@
 var Component = require("montage/ui/component").Component,
     NetworkInterfaceType = require("core/model/enumerations/network-interface-type").NetworkInterfaceType,
-    Model = require("core/model/model").Model,
-    Bindings = require("montage/core/core").Bindings;
+    Model = require("core/model/model").Model;
 
 /**
  * @class Interface
  * @extends Component
  */
 exports.Interface = Component.specialize({
-    _statusClasses: {
-        value: [
-            'is-up',
-            'is-unknown',
-            'is-down',
-            'is-disabled'
-        ]
-    },
-
-    interfaceStatus: {
-        value: null
-    },
-
-    _linkState: {
-        value: null
-    },
-
-    linkState: {
-        get: function() {
-            return this._linkState;
-        },
-        set: function(linkState) {
-            if (this._linkState != linkState) {
-                this._linkState = linkState;
-                this._assignIconClass();
-            }
-        }
-    },
-
-    _cardEnabled: {
-        value: null
-    },
-
-    isCardEnabled: {
-        get: function() {
-            return this._cardEnabled;
-        },
-        set: function(cardEnabled) {
-            if (this._cardEnabled != cardEnabled) {
-                this._cardEnabled = cardEnabled;
-                this._assignIconClass();
-            }
-        }
-    },
-
-    enterDocument: {
-        value: function(isFirstTime) {
-            if (isFirstTime) {
-                Bindings.defineBinding(this, "linkState", {"<-": "object.status.link_state"});
-                Bindings.defineBinding(this, "isCardEnabled", {"<-": "object.enabled"});
-            }
-        }
-    },
 
     _object: {
         value: null
@@ -125,50 +71,12 @@ exports.Interface = Component.specialize({
             } else {
                 this._object = null;
             }
-        }
-    ,
+        },
+
         get: function () {
             var newObject = new Object(this._object);
             newObject.aliases = this.staticIP.concat(this.otherAliases).filter(function (alias) { return !!alias; });
             return newObject;
-        }
-    },
-
-    _assignIconClass: {
-        value: function() {
-            if (this.isCardEnabled) {
-                switch (this.linkState) {
-                    case "LINK_STATE_UP":
-                        this._activateIconClass('is-up');
-                        this.interfaceStatus = 'Active';
-                        break;
-                    case "LINK_STATE_UNKNOWN":
-                        this._activateIconClass('is-unknown');
-                        this.interfaceStatus = 'Unknown';
-                        break;
-                    case "LINK_STATE_DOWN":
-                        this._activateIconClass('is-down');
-                        this.interfaceStatus = 'Down';
-                        break;
-                }
-            } else {
-                this._activateIconClass('is-disabled');
-                this.interfaceStatus = 'Disabled';
-            }
-        }
-    },
-
-    _activateIconClass: {
-        value: function(className) {
-            var i, length, statusClass;
-            for (i = 0, length = this._statusClasses.length; i < length; i++) {
-                statusClass = this._statusClasses[i];
-                if (statusClass === className) {
-                    this.status.classList.add(className);
-                } else {
-                    this.status.classList.remove(statusClass);
-                }
-            }
         }
     }
 
