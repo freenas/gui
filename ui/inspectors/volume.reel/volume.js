41f4a39293d59563ccb40e5a3eeaf7f0a7ce4abd
Pierre Chaussalet <pchaussalet@gmail.com>
2016-03-21 11:38:24 -0700

    Fix volumes with no share display

--- ui/inspectors/volume.reel/volume.js
+++ ui/inspectors/volume.reel/volume.js
@@ -1,7 +1,48 @@
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    Model = require("core/model/model").Model;
 
 /**
  * @class Volume
  * @extends Component
  */
-exports.Volume = Component.specialize();
+exports.Volume = Component.specialize({
+    emptyShares: {
+        value: null
+    },
+
+    _allShares: {
+        value: null
+    },
+
+    allShares: {
+        get: function() {
+            return this._allShares;
+        },
+        set: function(allShares) {
+console.log(this._allShares, allShares);
+            this._allShares = allShares;
+        }
+    },
+
+    _shares: {
+        value: null
+    },
+
+    shares: {
+        get: function() {
+            return this._shares;
+        },
+        set: function(shares) {
+console.log(this._shares, shares);
+            this._shares = shares;
+        }
+    },
+
+    enterDocument: {
+        value: function(isFirstTime) {
+            if (isFirstTime) {
+                this.emptyShares = this.application.dataService.getEmptyCollectionForType(Model.Share);
+            }
+        }
+    }
+});
