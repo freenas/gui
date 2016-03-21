62e386a56fdf5fe35e669d477064736ed17c9c6e
Pierre Chaussalet <pchaussalet@gmail.com>
2016-03-21 11:37:24 -0700

    Add share save

--- ui/inspectors/share.reel/share.js
+++ ui/inspectors/share.reel/share.js
@@ -4,4 +4,10 @@ var Component = require("montage/ui/component").Component;
  * @class Share
  * @extends Component
  */
-exports.Share = Component.specialize();
+exports.Share = Component.specialize({
+    save: {
+        value: function() {
+            this.application.dataService.saveDataObject(this.object);
+        }
+    }
+});
