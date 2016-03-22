2e7cc0925eefb88d11db74a5e891c229512e670f
Pierre Chaussalet <pchaussalet@gmail.com>
2016-03-22 15:39:30 -0700

    Add default save and delete actions on inspector

--- ui/inspectors/inspector.reel/inspector.js
+++ ui/inspectors/inspector.reel/inspector.js
@@ -18,6 +18,8 @@ exports.Inspector = Component.specialize(/** @lends Inspector# */ {
         value: function(event) {
             if (typeof this.parentComponent.delete === 'function') {
                 this.parentComponent.delete();
+            } else if (this.object) {
+                this.application.dataService.deleteDataObject(this.object);
             } else {
                 console.warn('NOT IMPLEMENTED: delete() on', this.parentComponent.templateModuleId);
             }
@@ -40,6 +42,8 @@ exports.Inspector = Component.specialize(/** @lends Inspector# */ {
         value: function(event) {
             if (typeof this.parentComponent.save === 'function') {
                 this.parentComponent.save();
+            } else if (this.object) {
+                this.application.dataService.saveDataObject(this.object);
             } else {
                 console.warn('NOT IMPLEMENTED: save() on', this.parentComponent.templateModuleId);
             }
--- ui/inspectors/share.reel/share.js
+++ ui/inspectors/share.reel/share.js
@@ -4,10 +4,4 @@ var Component = require("montage/ui/component").Component;
  * @class Share
  * @extends Component
  */
-exports.Share = Component.specialize({
-    save: {
-        value: function() {
-            this.application.dataService.saveDataObject(this.object);
-        }
-    }
-});
+exports.Share = Component.specialize();
