992c8205f9a22c761d3e2bffdbe25e897791bebd
Pierre Chaussalet <pchaussalet@gmail.com>
2016-03-22 18:10:54 -0700

    Add NFS share inspector

--- ui/inspectors/nfs-share.reel/nfs-share.js
+++ ui/inspectors/nfs-share.reel/nfs-share.js
@@ -1,7 +1,43 @@
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    Model = require("core/model/model").Model;
 
 /**
  * @class NfsShare
  * @extends Component
  */
-exports.NfsShare = Component.specialize();
+exports.NfsShare = Component.specialize({
+    securityOptions: {
+        value: [
+            "sys",
+            "krb5",
+            "krb5i",
+            "krb5p"
+        ]
+    },
+
+    enterDocument: {
+        value: function(isFirstTime) {
+            var self = this;
+            if (isFirstTime) {
+                this._loadUsers().then(function(users) {
+                    self.users = users;
+                });
+                this._loadGroups().then(function(groups) {
+                    self.groups = groups;
+                });
+            }
+        }
+    },
+
+    _loadUsers: {
+        value: function() {
+            return this.application.dataService.fetchData(Model.User);
+        }
+    },
+
+    _loadGroups: {
+        value: function() {
+            return this.application.dataService.fetchData(Model.Group);
+        }
+    }
+});
