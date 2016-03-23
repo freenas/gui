3ed03faa2b1f3cec8d583c850d1ebfe0c3601483
Thibault Zanini <thibaultzanini@gmail.com>
2016-03-22 17:17:34 -0700

    Allow to hit the key enter in order to sign in.

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -1,5 +1,7 @@
 
-var Component = require("montage/ui/component").Component;
+var ComponentModule = require("montage/ui/component"),
+    Component = ComponentModule.Component,
+    rootComponent = ComponentModule.__root__;
 
 /**
  * @class Main
@@ -8,12 +10,22 @@ var Component = require("montage/ui/component").Component;
 exports.Main = Component.specialize({
 
     enterDocument: {
-        value: function() {
-            this.application.section = 'dashboard';
+        value: function (firsTime) {
+            if (firsTime) {
+                this.application.section = 'dashboard';
+            }
+        }
+    },
 
-            // Fixme: @benoit application modal is not added properly to the componentTree,
-            // so the method enterDocument of its child components are not called.
-            this.application.applicationModal.enterDocument(true);
+    draw: {
+        value: function () {
+            var applicationModal = this.application.applicationModal;
+
+            // Fixme: @benoit hacky application modal is not detached once the loader has finished its work.
+            if (applicationModal !== rootComponent) {
+                applicationModal.attachToParentComponent();
+                applicationModal.enterDocument(true);
+            }
         }
     }
 
--- ui/sign-in.reel/sign-in.js
+++ ui/sign-in.reel/sign-in.js
@@ -1,4 +1,5 @@
-var AuthorizationPanel = require("montage-data/ui/authorization-panel.reel").AuthorizationPanel;
+var AuthorizationPanel = require("montage-data/ui/authorization-panel.reel").AuthorizationPanel,
+    KeyComposer = require("montage/composer/key-composer").KeyComposer;
 
 
 /**
@@ -60,6 +61,50 @@ exports.SignIn = AuthorizationPanel.specialize({
             return this._isAuthenticating;
         }
     },
+
+
+    __keyComposer: {
+        value: null
+    },
+
+    _keyComposer: {
+        get: function () {
+            if (!this.__keyComposer) {
+                this.__keyComposer = new KeyComposer();
+                this.__keyComposer.keys = "enter";
+                this.__keyComposer.identifier = "enter";
+                this.addComposerForElement(this.__keyComposer, this.element.ownerDocument.defaultView);
+            }
+
+            return this.__keyComposer;
+        }
+    },
+
+    enterDocument: {
+        value: function () {
+            this.addEventListener("action", this, false);
+            this._keyComposer.addEventListener("keyPress", this, false);
+
+        }
+    },
+
+    exitDocument: {
+        value: function () {
+            this.removeEventListener("action", this, false);
+            this._keyComposer.removeEventListener("keyPress", this, false);
+        }
+    },
+
+
+    handleKeyPress: {
+        value: function (mutableEvent) {
+            //Fixme: bug?
+            if (mutableEvent._event.identifier === "enter") {
+                this.handleSubmitAction();
+            }
+        }
+    },
+
 
     handleSubmitAction: {
         value: function() {
