f79514db90733c1baa27b4df0ef5fb0f6cf5d2bd
Ben Milman <bpmilman@gmail.com>
2016-03-22 18:38:04 -0700

    Add initial network interface creation

---
+++ ui/inspectors/network-interface-create.reel/network-interface-create.js
@@ -0,0 +1,47 @@
+var Component = require("montage/ui/component").Component,
+    Model = require("core/model/model").Model;
+
+/**
+ * @class NetworkInterfaceCreate
+ * @extends Component
+ */
+exports.NetworkInterfaceCreate = Component.specialize({
+    newVlan: {
+        value: null
+    },
+
+    newLagg: {
+        value: null
+    },
+
+    newBridge: {
+        value: null
+    },
+
+    enterDocument: {
+        value: function(isFirstTime) {
+            if (isFirstTime) {
+                this.newVlan = this.createNewInterface();
+                this.newVlan.type = "VLAN";
+                this.newLagg = this.createNewInterface();
+                this.newLagg.type = "LAGG";
+                this.newBridge = this.createNewInterface();
+                this.newBridge.type = "BRIDGE";
+            }
+        }
+    },
+
+    createNewInterface: {
+        value: function() {
+            var newInterface = this.application.dataService.getDataObject(Model.NetworkInterface);
+            newInterface.aliases = [];
+            newInterface.enabled = false;
+            // FIXME: Hacks around combination of name not being nullable in the middleware
+            // and certain form elements initializing the bound value to null. Remove if
+            // either issue is resolved.
+            newInterface.name = "";
+            return newInterface;
+        }
+    }
+
+});
