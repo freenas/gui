b42ccd13ae8969bae2e33c491a9763a3413c99f0
Pierre Chaussalet <pchaussalet@gmail.com>
2016-03-25 13:16:03 -0700

    Fix networkconfiguration default gateway save

--- ui/inspectors/network-configuration.reel/network-configuration.js
+++ ui/inspectors/network-configuration.reel/network-configuration.js
@@ -3,60 +3,62 @@
  */
 var Component = require("montage/ui/component").Component,
     Model = require("core/model/model").Model,
-    Converter = require("montage/core/converter/converter").Converter;
+    Promise = require("montage/core/promise").Promise;
 
 /**
  * @class NetworkConfiguration
  * @extends Component
  */
 exports.NetworkConfiguration = Component.specialize(/** @lends NetworkConfiguration# */ {
-    _object: {
+    _isGatewayDhcpAssigned: {
         value: null
     },
 
-    _status: {
-        value: null
-    },
-
-    // These properties control the behavior of the gateway settings.
-    // When DHCP set gateway is enabled, we want to show the DHCP-set value.
-    // However, the only way to be sure the values in status were set by DHCP is
-    // if we record whether DHCP owned those settings when they arrived.
-
-    serverDhcpAssignGateway: {
-        value: null
-    },
-
-    serverDhcpAssignedGatewayValues: {
-        value: null
+    isGatewayDhcpAssigned: {
+        get: function() {
+            return this._isGatewayDhcpAssigned;
+        },
+        set: function(isGatewayDhcpAssigned) {
+            if (!isGatewayDhcpAssigned) {
+                this.object.gateway.ipv4 = this.object.status.gateway.ipv4;
+                this.object.gateway.ipv6 = this.object.status.gateway.ipv6;
+            }
+            this._isGatewayDhcpAssigned = isGatewayDhcpAssigned;
+        }
     },
 
     save: {
         value: function() {
-            this.application.dataService.saveDataObject(this.object);
-            this.application.dataService.saveDataObject(this.object.general);
+            var self = this,
+                dhcpLeaseRenewalPromise;
+            if (this.isGatewayDhcpAssigned && (this.object.gateway.ipv4 || this.object.gateway.ipv6)) {
+                this.object.gateway.ipv4 = this.object.gateway.ipv6 = null;
+                dhcpLeaseRenewalPromise = this.application.dataService.fetchData(Model.NetworkInterface).then(function(networkInterfaces) {
+                    var defaultInterface = networkInterfaces.filter(function(x) { return x.dhcp; })[0];
+                    return defaultInterface.renew(defaultInterface.id);
+                });
+            } else {
+                dhcpLeaseRenewalPromise = Promise.resolve();
+            }
+            dhcpLeaseRenewalPromise.then(function() {
+                self.application.dataService.saveDataObject(self.object);
+                self.application.dataService.saveDataObject(self.object.general);
+            });
         }
     },
 
-    object: {
-        set: function (networkConfig) {
-            if (networkConfig) {
-                this._object = networkConfig;
-                this._status = networkConfig.status;
-                if (networkConfig.dhcp.assign_gateway) {
-                    this.serverDhcpAssignGateway = true;
-                    this.serverDhcpAssignedGatewayValues = networkConfig.status.gateway;
-                } else {
-                    this.serverDhcpAssignGateway = false;
-                    this.serverDhcpAssignedGatewayValues = {ipv4: "", ipv6: ""};
+    enterDocument: {
+        value: function(isFirstTime) {
+            if (isFirstTime) {
+                if (this.object) {
+                    if (!this.object.gateway.ipv4) {
+                        this.object.gateway.ipv4 = this.object.status.gateway.ipv4;
+                    }
+                    if (!this.object.gateway.ipv6) {
+                        this.object.gateway.ipv6 = this.object.status.gateway.ipv6;
+                    }
                 }
-            } else {
-                this._object = null;
-                this._status = null;
             }
-        },
-        get: function () {
-            return this._object;
         }
     }
 });
