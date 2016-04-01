a359a1551bee850e9a511012139a0d4b661e3a39
joshuarule <joshua@joshuarule.com>
2016-03-31 20:07:41 -0400

    adds disk icons

---
+++ ui/icons/hdd.reel/hdd.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/hdd.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Hdd
+ * @extends Component
+ */
+exports.Hdd = Component.specialize(/** @lends Hdd# */ {
+    constructor: {
+        value: function Hdd() {
+            this.super();
+        }
+    }
+});
---
+++ ui/icons/ssd.reel/ssd.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/ssd.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Ssd
+ * @extends Component
+ */
+exports.Ssd = Component.specialize(/** @lends Ssd# */ {
+    constructor: {
+        value: function Ssd() {
+            this.super();
+        }
+    }
+});
