8a0df5fe31a15f3979468139c6adffa2682e9b5d
Ben Milman <bpmilman@gmail.com>
2016-04-12 19:15:04 -0700

    Populate SSH form

--- ui/inspectors/sshd-service.reel/sshd-service.js
+++ ui/inspectors/sshd-service.reel/sshd-service.js
@@ -4,4 +4,37 @@ var Component = require("montage/ui/component").Component;
  * @class SshdService
  * @extends Component
  */
-exports.SshdService = Component.specialize();
+exports.SshdService = Component.specialize({
+    SFTP_LOG_FACILITIES: {
+        get: function() {
+            return [
+                "DAEMON",
+                "USER",
+                "AUTH",
+                "LOCAL0",
+                "LOCAL1",
+                "LOCAL2",
+                "LOCAL3",
+                "LOCAL4",
+                "LOCAL5",
+                "LOCAL6",
+                "LOCAL7"
+            ];
+        }
+    },
+
+    SFTP_LOG_LEVELS: {
+        get: function() {
+            return [
+                "QUIET",
+                "FATAL",
+                "ERROR",
+                "INFO",
+                "VERBOSE",
+                "DEBUG",
+                "DEBUG2",
+                "DEBUG3"
+            ];
+        }
+    }
+});
