5bb4158f1430949466128f780c5f5d25f2c7fb77
joshuarule <joshua@joshuarule.com>
2016-04-18 16:55:03 -0700

    adds link status to network interfaces list items Ticket: #13999

---
+++ ui/network/interfaces-list-item.reel/interfaces-list-item.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/interfaces-list-item.reel
+ */
+var ListItem = require("ui/controls/list.reel/list-item.reel/list-item").ListItem;
+
+/**
+ * @class InterfacesListItem
+ * @extends Component
+ */
+exports.InterfacesListItem = ListItem.specialize(/** @lends InterfacesListItem# */ {
+    constructor: {
+        value: function InterfacesListItem() {
+            this.super();
+        }
+    }
+});
