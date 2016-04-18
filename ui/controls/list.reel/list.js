d8b6fb7b255f529d727e0f575b6249ea99920ba1
Thibault Zanini <thibaultzanini@gmail.com>
2016-04-18 14:52:10 -0700

    Add support of custom list-item when the cascading list if displaying a collection.

--- ui/controls/list.reel/list.js
+++ ui/controls/list.reel/list.js
@@ -1,7 +1,40 @@
 var Component = require("montage/ui/component").Component;
 
+var DEFAULT_LIST_ITEM_MODULE_ID = 'ui/controls/list.reel/list-item.reel';
+
 /**
  * @class List
  * @extends Component
  */
-exports.List = Component.specialize();
+exports.List = Component.specialize({
+
+    _content: {
+        value: null
+    },
+
+    content: {
+        set: function (content) {
+            if (this._content !== content) {
+                this._content = content;
+
+                if (content) {
+                    var self = this;
+
+                    this.application.delegate.userInterfaceDescriptorForObject(content).then(function (UIDescriptor) {
+                        self.listItemModuleId = UIDescriptor.collectionItemComponentModuleId || DEFAULT_LIST_ITEM_MODULE_ID;
+
+                    }).catch(function (error) {
+                        console.warn(error);
+                        self.listItemModuleId = DEFAULT_LIST_ITEM_MODULE_ID;
+                    });
+                } else {
+                    this.listItemModuleId = DEFAULT_LIST_ITEM_MODULE_ID;
+                }
+            }
+        },
+        get: function () {
+            return this._object;
+        }
+    }
+
+});
