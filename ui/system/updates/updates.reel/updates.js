66065c81df397e30e849618c5be258aa5c052456
Ben Milman <bpmilman@gmail.com>
2016-04-19 18:09:53 -0700

    Add dummy update config controls
    The trains dropdown needs a converter to work properly

--- ui/system/updates/updates.reel/updates.js
+++ ui/system/updates/updates.reel/updates.js
@@ -12,5 +12,44 @@ exports.Updates = Component.specialize(/** @lends Updates# */ {
         value: function Updates() {
             this.super();
         }
+    },
+
+    _config: {
+        value:
+            {
+              "check_auto": true,
+              "update_server": "http://update.freenas.org/FreeNAS",
+              "train": "FreeNAS-10-Nightlies"
+            }
+    },
+
+    _trains: {
+        value: [
+                  {
+                    "name": "FreeNAS-9.3-STABLE",
+                    "current": false,
+                    "description": null,
+                    "sequence": null
+                  },
+                  {
+                    "name": "FreeNAS-10-Nightlies",
+                    "current": true,
+                    "description": "Installed OS",
+                    "sequence": "120da5424ffa96ff197238e4c682b899"
+                  },
+                  {
+                    "name": "FreeNAS-9.10-STABLE",
+                    "current": false,
+                    "description": null,
+                    "sequence": null
+                  },
+                  {
+                    "name": "FreeNAS-9.10-Nightlies",
+                    "current": false,
+                    "description": null,
+                    "sequence": null
+                  }
+                ]
+    },
     }
 });
