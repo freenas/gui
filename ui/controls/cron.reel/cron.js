1f308e71df2fb87ecc2ade93c80c6ff68a453936
joshuarule <joshua@joshuarule.com>
2016-04-22 09:21:28 -0700

    reworks dashboard style

--- ui/controls/cron.reel/cron.js
+++ ui/controls/cron.reel/cron.js
@@ -7,10 +7,4 @@ var Component = require("montage/ui/component").Component;
  * @class Cron
  * @extends Component
  */
-exports.Cron = Component.specialize(/** @lends Cron# */ {
-    constructor: {
-        value: function Cron() {
-            this.super();
-        }
-    }
-});
+exports.Cron = Component.specialize();
