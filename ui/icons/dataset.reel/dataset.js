c49af4d0799c9bd86ca7f16beb703c4834f093b4
joshuarule <joshua@joshuarule.com>
2016-04-25 15:32:27 -0700

    updates share inspector and adds dataset and directory icon

---
+++ ui/icons/dataset.reel/dataset.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/dataset.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Dataset
+ * @extends Component
+ */
+exports.Dataset = Component.specialize(/** @lends Dataset# */ {
+    constructor: {
+        value: function Dataset() {
+            this.super();
+        }
+    }
+});
---
+++ ui/icons/directory.reel/directory.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/directory.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Directory
+ * @extends Component
+ */
+exports.Directory = Component.specialize(/** @lends Directory# */ {
+    constructor: {
+        value: function Directory() {
+            this.super();
+        }
+    }
+});
