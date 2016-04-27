dc8b01172ea3ea40221d50ea0eadb0b754b68ed1
joshuarule <joshua@joshuarule.com>
2016-04-27 15:10:09 -0400

    adds accounts system icon, adds freenas icon Ticket: #14867

---
+++ ui/icons/freenas-icon.reel/freenas-icon.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/freenas-icon.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class FreenasIcon
+ * @extends Component
+ */
+exports.FreenasIcon = Component.specialize(/** @lends FreenasIcon# */ {
+    constructor: {
+        value: function FreenasIcon() {
+            this.super();
+        }
+    }
+});
