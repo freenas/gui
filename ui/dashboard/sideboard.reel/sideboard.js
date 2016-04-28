021ec378188c99e8e651eafc443fcd4399daaa01
joshuarule <joshua@joshuarule.com>
2016-04-28 14:54:36 -0400

    adds reveal and hide functionality to sideboard

--- ui/dashboard/sideboard.reel/sideboard.js
+++ ui/dashboard/sideboard.reel/sideboard.js
@@ -4,4 +4,14 @@ var Component = require("montage/ui/component").Component;
  * @class Sideboard
  * @extends Component
  */
-exports.Sideboard = Component.specialize();
+exports.Sideboard = Component.specialize({
+    isCollapsed: {
+        value: false
+    },
+
+    handleToggleSideboardAction: {
+        value: function () {
+            this.isCollapsed = !this.isCollapsed;
+        }
+    }
+});
