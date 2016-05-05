8533bcac2c73cf3bf01be6d9d3674de8437a397e
Pierre Chaussalet <pchaussalet@gmail.com>
2016-05-05 11:01:08 -0700

    Replace networkTraffic repetition with hardcoded ther first interfaces

--- ui/dashboard/dashboard.reel/dashboard.js
+++ ui/dashboard/dashboard.reel/dashboard.js
@@ -14,15 +14,8 @@ exports.Dashboard = Component.specialize({
         value: function (isFirstTime) {
             var self = this;
             if (isFirstTime) {
-                this._dataService = this.application.dataService;
-
-                // Fixme: Right now if this is done again after adding a new interface
-                // it crashes due to missing data source. Once it's possible, have this
-                // respond dynamically to all working interfaces in the system.
-                this._dataService.fetchData(Model.NetworkInterface).then(function(interfaces){
-                    self.interfaces = interfaces.filter(function(networkInterface){
-                        return networkInterface.type === "ETHER" && networkInterface.enabled;
-                    });
+                this.application.dataService.fetchData(Model.NetworkInterface).then(function(interfaces){
+                    self.interfaces = interfaces;
                 });
             }
         }
