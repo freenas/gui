fa9679c0c89ce94d6d7d61d7fa5390ae6f5a9413
Ben Milman <bpmilman@gmail.com>
2016-05-04 18:21:39 -0700

    Show all enabled ethernet interfaces at the dashboard
    Ticket: #15001

--- ui/dashboard/dashboard.reel/dashboard.js
+++ ui/dashboard/dashboard.reel/dashboard.js
@@ -1,7 +1,30 @@
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    Model = require("core/model/model").Model;
 
 /**
  * @class Dashboard
  * @extends Component
  */
-exports.Dashboard = Component.specialize();
+exports.Dashboard = Component.specialize({
+    interfaces: {
+        value: null
+    },
+
+    enterDocument: {
+        value: function (isFirstTime) {
+            var self = this;
+            if (isFirstTime) {
+                this._dataService = this.application.dataService;
+
+                // Fixme: Right now if this is done again after adding a new interface
+                // it crashes due to missing data source. Once it's possible, have this
+                // respond dynamically to all working interfaces in the system.
+                this._dataService.fetchData(Model.NetworkInterface).then(function(interfaces){
+                    self.interfaces = interfaces.filter(function(networkInterface){
+                        return networkInterface.type === "ETHER" && networkInterface.enabled;
+                    });
+                });
+            }
+        }
+    }
+});
