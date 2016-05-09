6a6b44d68a42c8fb6a163d3eacd7a263f682536b
Joshua Rule Dobson <joshua@joshuarule.com>
2016-05-09 18:21:34 -0400

    adds spinner component

---
+++ ui/controls/spinner.reel/spinner.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/spinner.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Spinner
+ * @extends Component
+ */
+exports.Spinner = Component.specialize(/** @lends Spinner# */ {
+    constructor: {
+        value: function Spinner() {
+            this.super();
+        }
+    }
+});
