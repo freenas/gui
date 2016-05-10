f113d5ac77da085f5916b7b42041756607a097ea
Joshua Rule Dobson <joshua@joshuarule.com>
2016-05-10 18:56:25 -0400

    refactors disks to include mediaSize and checks for ssd

--- ui/icons/disk.reel/disk.js
+++ ui/icons/disk.reel/disk.js
@@ -1,7 +1,28 @@
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    BytesConverter = require("montage/core/converter/bytes-converter").BytesConverter;
 
 /**
  * @class Disk
  * @extends Component
  */
-exports.Disk = Component.specialize();
+exports.Disk = Component.specialize({
+
+    _object: {
+        value: null
+    },
+
+    object: {
+        set: function (object) {
+            if (this._object !== object) {
+                this._object = object;
+                this.disk = object ? object._disk ? object._disk : object : null;
+            }
+        },
+        get: function () {
+            return this._object;
+        }
+    }
+
+});
+
+exports.defaultBytesConverter = new BytesConverter();
