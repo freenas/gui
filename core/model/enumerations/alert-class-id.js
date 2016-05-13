597f4a93158f3488a4141d31984d81e803d0ccab
Thibault Zanini <thibaultzanini@gmail.com>
2016-05-13 11:10:30 -0700

    update montage-data cache to 201605130930.

---
+++ core/model/enumerations/alert-class-id.js
@@ -0,0 +1,3 @@
+var Enum = require("montage/core/enum").Enum;
+
+exports.AlertClassId = new Enum().initWithMembersAndValues(["DiskControllerFirmwareMismatch","SystemReboot","SystemShutdown","UpdateAvailable","UserMessage","VolumeDegraded","VolumeUpgradePossible"], ["DiskControllerFirmwareMismatch","SystemReboot","SystemShutdown","UpdateAvailable","UserMessage","VolumeDegraded","VolumeUpgradePossible"]);
