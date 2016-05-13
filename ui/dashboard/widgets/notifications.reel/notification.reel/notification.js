330ba40af3be04aaf4e6882a3cc687d586c1d645
Ben Milman <bpmilman@gmail.com>
2016-05-13 14:59:08 -0700

    Use middleware-provided descriptions for task notifications
    Ticket: #15133
    Ticket: #15192

--- ui/dashboard/widgets/notifications.reel/notification.reel/notification.js
+++ ui/dashboard/widgets/notifications.reel/notification.reel/notification.js
@@ -18,115 +18,6 @@ exports.Notification = Component.specialize(/** @lends Notification# */ {
         set: function (object) {
             if (this._object !== object) {
                 this._object = object;
-                if (object) {
-                    var data = object.data;
-
-                    if (data && data.length) {
-                        var self = this,
-                            entity = data[0];
-
-                        this.application.delegate.getUserInterfaceDescriptorForType(object.modelType).then(function (UIDescriptor) {
-                            var nameExpression = UIDescriptor.nameExpression,
-                                typeOfEntity = typeof entity,
-                                summary,
-                                expressionValue;
-
-                            // Fixme: Once there are entity names provided
-                            // directly by the middleware, use those instead
-                            // of the calculated entity names.
-                            if (nameExpression && entity && typeOfEntity === "object") {
-                                expressionValue = Evaluate(nameExpression, entity);
-                            }
-
-                            if (object.taskName) {
-                                switch (object.taskName) {
-                                    case "share.update":
-                                        summary = "Update " + entity.type.toUpperCase() + " Share " + (expressionValue || entity.id);
-                                        break;
-                                    case "share.create":
-                                        summary = "Create " + entity.type.toUpperCase() + " Share " + entity.name;
-                                        break;
-                                    case "share.delete":
-                                        summary = "Delete " + entity.type.toUpperCase() + " Share " + (expressionValue || entity.id);
-                                        break;
-                                    case "user.update":
-                                        summary = "Update User " + (expressionValue || entity.id);
-                                        break;
-                                    case "user.create":
-                                        summary = "Create User " + entity.username;
-                                        break;
-                                    case "user.delete":
-                                        summary = "Delete User " + (expressionValue || entity.id);
-                                        break;
-                                    case "group.update":
-                                        summary = "Update Group " + (expressionValue || entity.id);
-                                        break;
-                                    case "group.create":
-                                        summary = "Create Group " + entity.name;
-                                        break;
-                                    case "group.delete":
-                                        summary = "Delete Group " + (expressionValue || entity.id);
-                                        break;
-                                    case "volume.update":
-                                        summary = "Update Volume " + (expressionValue || entity.id);
-                                        break;
-                                    case "volume.create":
-                                        summary = "Create Volume " + entity.id;
-                                        break;
-                                    case "volume.delete":
-                                        summary = "Delete Volume " + (expressionValue || entity.id);
-                                        break;
-                                    case "network.interface.update":
-                                        summary = "Update " + entity.type + " " + (expressionValue || entity.id);
-                                        break;
-                                    case "network.interface.create":
-                                        summary = "Create new " + entity.type + " " + (entity.name ? entity.name : "");
-                                        break;
-                                    case "network.interface.delete":
-                                        summary = "Delete " + entity.type + " " + (expressionValue || entity.id);
-                                        break;
-                                    case "service.update":
-                                        summary = "Update " + expressionValue + " Settings";
-                                        break;
-                                    case "network.configuration.update":
-                                        summary = "Update " + expressionValue;
-                                        break;
-                                    case "system.general.update":
-                                        summary = "Update System General Settings";
-                                        break;
-                                    default:
-                                        summary = object.taskName + " " + (expressionValue || entity.id);
-                                }
-                            }
-
-                            self.summary = summary;
-                        }).catch(function (error) {
-                            var typeOfEntity,
-                                summary;
-                            // Fixme: This makes me really sad and there needs to be a
-                            // better way to handle editing things with no UI descriptor
-                            if (object.taskName) {
-                                switch (object.taskName) {
-                                    case "system.general.update":
-                                        summary = "Update System General Settings";
-                                        break;
-                                    default:
-                                        summary = object.taskName;
-                                }
-                            } else if (typeOfEntity === "string") {
-                                summary =  entity;
-                            } else {
-                                // I'm being explicit with this instead of leaving it undefined.
-                                // It will result in no summary displayed at all either way.
-                                summary = null;
-                            }
-                            self.summary = summary;
-                            console.warn(error);
-                        });
-                    } else {
-                        this.summary = this.object.modelType;
-                    }
-                }
             }
         },
         get: function () {
@@ -135,10 +26,6 @@ exports.Notification = Component.specialize(/** @lends Notification# */ {
     },
 
     UIDescriptor: {
-        value: null
-    },
-
-    summary: {
         value: null
     },
 
