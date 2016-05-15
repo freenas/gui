513720b9023d1d490228db2ed70b6960aed07481
Pierre Chaussalet <pchaussalet@gmail.com>
2016-05-14 21:57:28 -0700

    Wire sideboard icons to alerts

--- ui/dashboard/sideboard.reel/sideboard.js
+++ ui/dashboard/sideboard.reel/sideboard.js
@@ -1,10 +1,17 @@
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    notificationCenter = require("core/backend/notification-center").defaultNotificationCenter;
 
 /**
  * @class Sideboard
  * @extends Component
  */
 exports.Sideboard = Component.specialize({
+    enterDocument: {
+        value: function() {
+            this.notifications = notificationCenter.notifications;
+        }
+    },
+
     isCollapsed: {
         value: false
     },
