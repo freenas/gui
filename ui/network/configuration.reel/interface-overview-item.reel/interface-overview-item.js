b0df2f7a276b31c6167c3558538fb1c2c3907e1a
Joshua Rule Dobson <joshua@joshuarule.com>
2016-05-20 13:03:16 -0400

    places disabled interfaces into a foldable-section in network overview, cleans up styles on foldable-section and inspector-section-title Ticket: #15292

---
+++ ui/network/configuration.reel/interface-overview-item.reel/interface-overview-item.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/interface-overview-item.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class InterfaceOverviewItem
+ * @extends Component
+ */
+exports.InterfaceOverviewItem = Component.specialize(/** @lends InterfaceOverviewItem# */ {
+    constructor: {
+        value: function InterfaceOverviewItem() {
+            this.super();
+        }
+    }
+});
