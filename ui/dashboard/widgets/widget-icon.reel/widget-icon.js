f4e37f7e7f835f058e8df2ab1c663622fc014c38
Pierre Chaussalet <pchaussalet@gmail.com>
2016-05-20 08:46:43 -0700

    Fix widget-icon classes wiring

--- ui/dashboard/widgets/widget-icon.reel/widget-icon.js
+++ ui/dashboard/widgets/widget-icon.reel/widget-icon.js
@@ -8,49 +8,6 @@ var Component = require("montage/ui/component").Component;
  * @extends Component
  */
 exports.WidgetIcon = Component.specialize(/** @lends WidgetIcon# */ {
-
-    _setLevels: {
-        value: function () {
-            if(this._data) {
-                for(var i = 0; i < this._data.length; i++) {
-                    if (this._data[i].data.severity == 'CRITICAL') {
-                        this.hasCritical = true;
-                        return;
-                    }
-                    if (this._data[i].state == 'FAILED') {
-                        this.hasError = true;
-                        return;
-                    }
-                    if (this._data[i].data.severity == 'WARNING') {
-                        this.hasWarning = true;
-                    }
-                    if (this._data[i].data.severity == 'INFO') {
-                        this.hasInfo = true;
-                    }
-                }
-            } else {
-                console.log("no data");
-            }
-        }
-    },
-
-    _data: {
-        value: null
-    },
-
-    data: {
-        set: function (arr) {
-            if(arr) {
-                this._data = arr;
-            }
-
-        },
-        get: function () {
-            this._setLevels();
-            return this._data;
-        }
-    },
-
     enterDocument: {
         value: function (isFirstTime) {
             if(isFirstTime) {
