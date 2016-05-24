ea9add660cab0b3fd67b34254ab14cd3eeb0e939
Pierre Chaussalet <pchaussalet@gmail.com>
2016-05-23 19:34:00 -0700

    Humanize sizes in boot-pool inspector
    Ticket: #15520

---
+++ ui/inspectors/system-section.reel/boot-pool.reel/size-cell.reel/size-cell.js
@@ -0,0 +1,16 @@
+/**
+ * @module ./size-cell.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class SizeCell
+ * @extends Component
+ */
+exports.SizeCell = Component.specialize(/** @lends SizeCell# */ {
+    constructor: {
+        value: function SizeCell() {
+            this.super();
+        }
+    }
+});
