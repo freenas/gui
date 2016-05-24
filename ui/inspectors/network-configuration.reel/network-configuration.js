d7bf127a05f7ab79eaa74c969c94cc5e0ca4638b
Ben Milman <bpmilman@gmail.com>
2016-05-23 19:07:11 -0700

    Remove 0xDEADC0DE that will be replaced once #15313 is done
    Missing data for this logic was causing #15523; this commit fixes it.
    Note: this removes the feature wherein changing DHCP gateway settings renews the DHCP lease and updates the GUI with the new gateway(s), because we no longer get the DHCP-provided gateways.
    Therefore, this causes #15524 in turn.
    
    Ticket: #15313
    Ticket: #15523
    Ticket: #15524

--- ui/inspectors/network-configuration.reel/network-configuration.js
+++ ui/inspectors/network-configuration.reel/network-configuration.js
@@ -9,56 +9,11 @@ var Component = require("montage/ui/component").Component,
  * @extends Component
  */
 exports.NetworkConfiguration = Component.specialize(/** @lends NetworkConfiguration# */ {
-    _isGatewayDhcpAssigned: {
-        value: null
-    },
-
-    isGatewayDhcpAssigned: {
-        get: function() {
-            return this._isGatewayDhcpAssigned;
-        },
-        set: function(isGatewayDhcpAssigned) {
-            if (!isGatewayDhcpAssigned) {
-                this.object.gateway.ipv4 = this.object.status.gateway.ipv4;
-                this.object.gateway.ipv6 = this.object.status.gateway.ipv6;
-            }
-            this._isGatewayDhcpAssigned = isGatewayDhcpAssigned;
-        }
-    },
 
     save: {
         value: function() {
-            var self = this,
-                dhcpLeaseRenewalPromise;
-            if (this.isGatewayDhcpAssigned && (this.object.gateway.ipv4 || this.object.gateway.ipv6)) {
-                this.object.gateway.ipv4 = this.object.gateway.ipv6 = null;
-                dhcpLeaseRenewalPromise = this.application.networkInterfacesSevice.getNetworkInterfaces().then(function(networkInterfaces) {
-                    var defaultInterface = networkInterfaces.filter(function(x) { return x.dhcp; })[0];
-                    return defaultInterface.renew(defaultInterface.id);
-                });
-            } else {
-                dhcpLeaseRenewalPromise = Promise.resolve();
-            }
-
-            return dhcpLeaseRenewalPromise.then(function() {
-                self.application.dataService.saveDataObject(self.object);
-                self.application.dataService.saveDataObject(self.object.general);
-            });
-        }
-    },
-
-    enterDocument: {
-        value: function(isFirstTime) {
-            if (isFirstTime) {
-                if (this.object && this.object.status) {
-                    if (!this.object.gateway.ipv4) {
-                        this.object.gateway.ipv4 = this.object.status.gateway.ipv4;
-                    }
-                    if (!this.object.gateway.ipv6) {
-                        this.object.gateway.ipv6 = this.object.status.gateway.ipv6;
-                    }
-                }
-            }
+            this.application.dataService.saveDataObject(this.object);
+            this.application.dataService.saveDataObject(this.object.general);
         }
     }
 });
