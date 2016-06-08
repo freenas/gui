32b1908277abc98b9cb692c9747b53f8a643f4b4
Thibault Zanini <thibaultzanini@gmail.com>
2016-06-08 12:43:54 -0700

    Better Clock Synchronization and improvements. - Displayed time change every 10secs. - Reduce garbage by creating just one function that handle time changes instead of creating a new one each time the interval is raised. - performance improvements.

--- ui/main-sidebar/current-user-summary.reel/current-user-summary.js
+++ ui/main-sidebar/current-user-summary.reel/current-user-summary.js
@@ -5,20 +5,90 @@ var Component = require("montage/ui/component").Component;
  * @extends Component
  */
 exports.CurrentUserSummary = Component.specialize({
+
+    _synchronizeClockTimeoutId: {
+        value: null
+    },
+
+    _synchronizeClockIntervalId: {
+        value: null
+    },
+
+    _intervalTime: {
+        value: 10 // sec
+    },
+
     enterDocument: {
-        value: function(isFirstTime) {
-            var self = this;
-            if (!this.application.session) {
-                this.application.session = {
-                    username: '',
-                    host: ''
-                };
+        value: function (isFirstTime) {
+            if (isFirstTime) {
+                if (!this.application.session) {
+                    this.application.session = {
+                        username: '',
+                        host: ''
+                    };
+                }
+
+                this.session = this.application.session;
+
+                // Bind a function in order to avoid to create several time this function.
+                this._timeChangetimeoutCallBack = (function (initial) {
+                    var now = new Date(),
+                        seconds = now.getSeconds(),
+                        timeLag = seconds % this._intervalTime ;
+
+                    now.setSeconds(seconds + (
+                            initial ? -timeLag : timeLag > this._intervalTime / 2 ? this._intervalTime - timeLag : -timeLag
+                        ));
+                    now.setMilliseconds(0);
+
+                    this.now = now.getTime();
+
+                }).bind(this);
             }
-            this.session = this.application.session;
-            this.now = Date.now();
-            setInterval(function() {
-                self.now = Date.now();
-            }, 1000);
+
+            this._synchronizeTime();
+        }
+    },
+
+    exitDocument: {
+        value: function () {
+            if (this._synchronizeClockTimeoutId) {
+                clearTimeout(this._synchronizeClockTimeoutId);
+                this._synchronizeClockTimeoutId = null;
+            }
+
+            if (this._synchronizeClockIntervalId) {
+                clearInterval(this._synchronizeClockIntervalId);
+                this._synchronizeClockIntervalId = null;
+            }
+        }
+    },
+
+    _synchronizeTime: {
+        value: function () {
+            var now = new Date(),
+                timeRemainingBeforeSync = ((this._intervalTime - (now.getSeconds() % this._intervalTime)) * 1000) - now.getMilliseconds();
+
+            //Set initial time.
+            this._timeChangetimeoutCallBack(true);
+
+            if (timeRemainingBeforeSync === this._intervalTime * 1000) { // clock already synchronized with the interval set.
+                this._setIntervalTime();
+
+            } else { // need to be synchronized.
+                var self = this;
+                this._synchronizeClockTimeoutId = setTimeout(function () {
+                    self._timeChangetimeoutCallBack();
+                    self._setIntervalTime();
+                }, timeRemainingBeforeSync);
+            }
+        }
+    },
+
+    _setIntervalTime: {
+        value: function () {
+            this._synchronizeClockIntervalId = setInterval(this._timeChangetimeoutCallBack, this._intervalTime * 1000);
         }
     }
+
 });
