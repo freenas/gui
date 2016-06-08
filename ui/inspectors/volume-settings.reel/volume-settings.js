4e32386c13e3337e308d907305025956a6939619
Ben Milman <bpmilman@gmail.com>
2016-06-07 17:11:04 -0700

    Add temporary compression and deduplication option enums
    Remove after #15831 is resolved.
    
    Ticket: #15622
    Ticket: #15831

--- ui/inspectors/volume-settings.reel/volume-settings.js
+++ ui/inspectors/volume-settings.reel/volume-settings.js
@@ -8,9 +8,39 @@ var Component = require("montage/ui/component").Component;
  * @extends Component
  */
 exports.VolumeSettings = Component.specialize(/** @lends VolumeSettings# */ {
-    constructor: {
-        value: function VolumeSettings() {
 
-        }
+    COMPRESSION_OPTIONS: {
+        value: [
+            "on",
+            "off",
+            "lzjb",
+            "zle",
+            "lz4",
+            "gzip",
+            "gzip-1",
+            "gzip-2",
+            "gzip-3",
+            "gzip-4",
+            "gzip-5",
+            "gzip-6",
+            "gzip-7",
+            "gzip-8",
+            "gzip-9"
+        ]
+    },
+
+    DEDUP_OPTIONS: {
+        value: [
+            "on",
+            "off",
+            "verify",
+            "sha256",
+            "sha256,verify",
+            "sha512",
+            "sha512,verify",
+            "skein",
+            "skein,verify",
+            "edonr,verify"
+        ]
     }
 });
