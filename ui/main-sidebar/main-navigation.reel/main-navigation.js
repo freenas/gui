20c14646ca205ecda13b86acff4d4c23e24b1b84
Thibault Zanini <thibaultzanini@gmail.com>
2016-06-08 18:03:28 -0700

    Improvements and clean up the MainNavigation Component. - Improve loading time. - Avoid to add unused classname, which results to reduce the garbage collection impact and to set several (repeated)  bindings. - Reduce to require an additional component. (less files to require) - Add listener on action events when component has been prepareForActivationEvents.

--- ui/main-sidebar/main-navigation.reel/main-navigation.js
+++ ui/main-sidebar/main-navigation.reel/main-navigation.js
@@ -5,11 +5,53 @@ var Component = require("montage/ui/component").Component;
  * @extends Component
  */
 exports.MainNavigation = Component.specialize({
+
+    enterDocument: {
+        value: function () {
+            this._addEventListenerIfNeeded();
+        }
+    },
+
+    prepareForActivationEvents: {
+        value: function () {
+            this._addEventListener();
+        }
+    },
+
+    exitDocument: {
+        value: function () {
+            this._removeEventListenersIfNeeded();
+        }
+    },
+
     handlePreferencesAction: {
-        value: function() {
+        value: function () {
             this.application.section = 'preferences';
         }
+    },
+
+    _addEventListenerIfNeeded: {
+        value: function () {
+            if (this.preparedForActivationEvents) {
+                this._addEventListener();
+            }
+        }
+    },
+
+    _addEventListener: {
+        value: function () {
+            this.addEventListener("action", this, false);
+        }
+    },
+
+    _removeEventListenersIfNeeded: {
+        value: function () {
+            if (this.preparedForActivationEvents) {
+                this.addEventListener("action", this, false);
+            }
+        }
     }
+
 });
 
 // FIXME: Selection needs to be managed by a selection controller
