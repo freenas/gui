59b4f6f998dc784bc892096a4300a11ec09168af
Ben Milman <bpmilman@gmail.com>
2016-06-13 17:08:25 -0700

    Add empty Volume Dataset inspector
    Ticket: #15616
    Ticket: #15617
    Ticket: #15618

---
+++ ui/inspectors/volume-dataset.reel/volume-dataset.js
@@ -0,0 +1,14 @@
+/**
+ * @module ui/volume-dataset.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class VolumeDataset
+ * @extends Component
+ */
+exports.VolumeDataset = Component.specialize(/** @lends VolumeDataset# */ {
+    object: {
+        value: null
+    }
+});
