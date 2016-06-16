b511b60ae9129f5f34fa7f68dddf90f3f6c205fb
Xin Tan <mactanxin@gmail.com>
2016-06-16 14:59:02 -0700

    Added : webdav sharing service only
    Ticket: #15653

--- ui/inspectors/web-dav-service.reel/web-dav-service.js
+++ ui/inspectors/web-dav-service.reel/web-dav-service.js
@@ -1,7 +1,33 @@
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    Model = require("core/model/model").Model,
+    Converter = require("montage/core/converter/converter").Converter,
+    Validator = require("montage/core/converter/converter").Validator;
 
 /**
  * @class WebDavService
  * @extends Component
  */
-exports.WebDavService = Component.specialize();
+exports.WebDavService = Component.specialize({
+
+    //document for web-dav sharing:
+    //http://doc.freenas.org/9.10/freenas_services.html#webdav
+    //
+
+
+    PROTOCOL_OPTIONS: {
+        value: [
+            "HTTP",
+            "HTTPS"
+        ]
+    },
+
+
+
+    //HTTP Authentication List
+    AUTHENTICATIONS: {
+        value: [
+            "BASIC",
+            "DIGEST"
+        ]
+    }
+});
