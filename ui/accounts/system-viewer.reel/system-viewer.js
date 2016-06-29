1afa4d024ab2183d33d6a1282e8cecc0f5f01a14
Joshua Rule Dobson <joshua@joshuarule.com>
2016-06-29 11:12:19 -0400

    adds postCss css files

---
+++ ui/accounts/system-viewer.reel/system-viewer.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/system-viewer.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class SystemViewer
+ * @extends Component
+ */
+exports.SystemViewer = Component.specialize(/** @lends SystemViewer# */ {
+    constructor: {
+        value: function SystemViewer() {
+            this.super();
+        }
+    }
+});
