39001e2a921a4512900b7fe332d1e0a427fa59b3
Thibault Zanini <thibaultzanini@gmail.com>
2016-06-30 11:37:31 -0700

    Add a workaround for a bug within the event manager when adding event listeners.

--- core/ui/abstract-component-action-delege.js
+++ core/ui/abstract-component-action-delege.js
@@ -6,6 +6,12 @@ var Component = require("montage/ui/component").Component;
  */
 var AbstractComponentActionDelegate = exports.AbstractComponentActionDelegate = Component.specialize({
 
+
+    //Fixme: workaround for the eventManager
+    __ACTION_EVENT_SET: {
+        value: false
+    },
+
     enterDocument: {
         value: function () {
             AbstractComponentActionDelegate.prototype._addEventListenerIfNeeded.call(this);
@@ -38,7 +44,7 @@ var AbstractComponentActionDelegate = exports.AbstractComponentActionDelegate = 
             // -> It's possible to add several event listeners for the same listener and the event type.
             // FIXME: Possible bug in within the placeholder:
             // -> enterDocument gets call when the component is not in the document.
-            if (this.preparedForActivationEvents && this._inDocument) {
+            if (this.preparedForActivationEvents && this._inDocument && !this.__ACTION_EVENT_SET) {
                 AbstractComponentActionDelegate.prototype._addEventListener.call(this);
             }
         }
@@ -46,6 +52,7 @@ var AbstractComponentActionDelegate = exports.AbstractComponentActionDelegate = 
 
     _addEventListener: {
         value: function () {
+            this.__ACTION_EVENT_SET = true;
             this.addEventListener("action", this, false);
         }
     },
@@ -53,6 +60,7 @@ var AbstractComponentActionDelegate = exports.AbstractComponentActionDelegate = 
     _removeEventListenersIfNeeded: {
         value: function () {
             if (this.preparedForActivationEvents) {
+                this.__ACTION_EVENT_SET = false;
                 this.removeEventListener("action", this, false);
             }
         }
