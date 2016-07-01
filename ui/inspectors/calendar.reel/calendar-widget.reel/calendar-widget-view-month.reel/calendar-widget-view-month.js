d36a35a988a345696f3f800badb125a584324377
Joshua Rule Dobson <joshua@joshuarule.com>
2016-07-01 18:50:26 -0400

    starts to refactor calendar views

---
+++ ui/inspectors/calendar.reel/calendar-widget.reel/calendar-widget-view-month.reel/calendar-widget-view-month.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/calendar-widget-view-month.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class CalendarWidgetViewMonth
+ * @extends Component
+ */
+exports.CalendarWidgetViewMonth = Component.specialize(/** @lends CalendarWidgetViewMonth# */ {
+    constructor: {
+        value: function CalendarWidgetViewMonth() {
+            this.super();
+        }
+    }
+});
