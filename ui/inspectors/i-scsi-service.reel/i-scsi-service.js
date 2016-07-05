7836bd897a268aa5100f1867228e2f456f85144d
Xin Tan <mactanxin@gmail.com>
2016-07-05 09:30:56 -0700

    FIXED : pool space threshold now has a default value : 90 Ticket: #16188

--- ui/inspectors/i-scsi-service.reel/i-scsi-service.js
+++ ui/inspectors/i-scsi-service.reel/i-scsi-service.js
@@ -4,4 +4,14 @@ var Component = require("montage/ui/component").Component;
  * @class IScsiService
  * @extends Component
  */
-exports.IScsiService = Component.specialize();
+exports.IScsiService = Component.specialize({
+    enterDocument: {
+        value: function(isFirstTime) {
+            if (isFirstTime) {
+                if (this.object.pool_space_threshold === null) {
+                    this.object.pool_space_threshold = 90;
+                }
+            }
+        }
+    }
+});
