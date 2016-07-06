7659ac29240d5ee5b7b61a4bd138a8007ee2c5cc
Ben Milman <bpmilman@gmail.com>
2016-07-05 18:17:53 -0700

    Update montage-data cache to 201607051959

--- core/model/models/vm-device-nic.js
+++ core/model/models/vm-device-nic.js
@@ -14,6 +14,19 @@ exports.VmDeviceNic = AbstractModel.specialize({
             return this._bridge;
         }
     },
+    _device: {
+        value: null
+    },
+    device: {
+        set: function (value) {
+            if (this._device !== value) {
+                this._device = value;
+            }
+        },
+        get: function () {
+            return this._device;
+        }
+    },
     _link_address: {
         value: null
     },
@@ -46,6 +59,11 @@ exports.VmDeviceNic = AbstractModel.specialize({
             mandatory: false,
             name: "bridge",
             valueType: "String"
+        }, {
+            mandatory: false,
+            name: "device",
+            valueObjectPrototypeName: "VmDeviceNicDevice",
+            valueType: "object"
         }, {
             mandatory: false,
             name: "link_address",
