a36078eed90c390aea97924c348de43c2de979b4
Xin Tan <mactanxin@gmail.com>
2016-07-06 14:12:28 -0700

    ADDED: Serial console

--- ui/inspectors/system-section.reel/serial-console.reel/serial-console.js
+++ ui/inspectors/system-section.reel/serial-console.reel/serial-console.js
@@ -1,7 +1,8 @@
 /**
  * @module ui/serial-console.reel
  */
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    Model = require("core/model/model").Model
 
 /**
  * @class SerialConsole
@@ -11,6 +12,28 @@ exports.SerialConsole = Component.specialize(/** @lends SerialConsole# */ {
     constructor: {
         value: function SerialConsole() {
             this.super();
+        }
+    },
+
+    serialPortOptions: {
+        value: null
+    },
+
+
+    _getSerialPort: {
+        value: function(){
+            var self = this;
+            return Model.populateObjectPrototypeForType(Model.SystemAdvanced).then(function (SystemAdvanced){
+                return SystemAdvanced.constructor.services.serialPorts();
+            }).then(function(serialPorts) {
+                self.serialPortOptions = self.serialPorts = serialPorts;
+            });
+        }
+    },
+
+    enterDocument: {
+        value: function(isFirstTime) {
+            this._getSerialPort();
         }
     }
 });
