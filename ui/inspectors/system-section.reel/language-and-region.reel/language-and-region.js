d9fea3ecbcfdebd7bc94a7be44f238f99374d85f
Xin Tan <mactanxin@gmail.com>
2016-07-07 14:33:40 -0700

    ADDED: local timezone

--- ui/inspectors/system-section.reel/language-and-region.reel/language-and-region.js
+++ ui/inspectors/system-section.reel/language-and-region.reel/language-and-region.js
@@ -1,7 +1,8 @@
 /**
  * @module ui/language-and-region.reel
  */
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    Model = require("core/model/model").Model
 
 /**
  * @class LanguageAndRegion
@@ -11,6 +12,28 @@ exports.LanguageAndRegion = Component.specialize(/** @lends LanguageAndRegion# *
     constructor: {
         value: function LanguageAndRegion() {
             this.super();
+        }
+    },
+
+    timezoneOptions: {
+        value: null
+    },
+
+
+    _getTimezones: {
+        value: function() {
+            var self = this;
+            return Model.populateObjectPrototypeForType(Model.SystemGeneral).then(function(SystemGeneral) {
+                return SystemGeneral.constructor.services.timezones();
+            }).then(function(timezones) {
+                self.timezoneOptions = self.timezones = timezones;
+            });
+        }
+    },
+
+    enterDocument: {
+        value: function(isFirstTime) {
+            this._getTimezones();
         }
     }
 });
