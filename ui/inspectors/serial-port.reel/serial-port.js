61f7f4b61054023e7e3e5d97625021572563a442
Xin Tan <mactanxin@gmail.com>
2016-07-07 14:33:03 -0700

    ADDED: serial port speed variable

--- ui/inspectors/serial-port.reel/serial-port.js
+++ ui/inspectors/serial-port.reel/serial-port.js
@@ -4,4 +4,15 @@ var Component = require("montage/ui/component").Component;
  * @class SerialPort
  * @extends Component
  */
-exports.SerialPort = Component.specialize();
+exports.SerialPort = Component.specialize({
+
+    PORT_SPEED_OPTIONS: {
+        value: [
+            9600,
+            19200,
+            38400,
+            57600,
+            115200
+        ]
+    }
+});
