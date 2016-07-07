ac31e71e215b17cc3b90bf739c64101606f64efb
Ben Milman <bpmilman@gmail.com>
2016-07-06 17:49:16 -0700

    Add virtual machine inspector section
    Ticket: #16013

---
+++ ui/inspectors/virtual-machine-section.reel/virtual-machine-section.js
@@ -0,0 +1,10 @@
+/**
+ * @module ui/virtual-machine-section.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class VirtualMachineSection
+ * @extends Component
+ */
+exports.VirtualMachineSection = Component.specialize();
