16bec4622beafd0c580f934bc50076c7c8b8a53d
Xin Tan <mactanxin@gmail.com>
2016-07-07 14:29:15 -0700

    ADDED: serial port speed options

--- ui/inspectors/system-section.reel/serial-console.reel/serial-console.js
+++ ui/inspectors/system-section.reel/serial-console.reel/serial-console.js
@@ -15,15 +15,21 @@ exports.SerialConsole = Component.specialize(/** @lends SerialConsole# */ {
         }
     },
 
-    serialPortOptions: {
-        value: null
+    PORT_SPEED_OPTIONS: {
+        value: [
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"
+        ]
     },
 
 
     _getSerialPort: {
         value: function(){
             var self = this;
-            return Model.populateObjectPrototypeForType(Model.SystemAdvanced).then(function (SystemAdvanced){
+            return Model.populateObjectPrototypeForType(Model.SystemAdvanced).then(function(SystemAdvanced){
                 return SystemAdvanced.constructor.services.serialPorts();
             }).then(function(serialPorts) {
                 self.serialPortOptions = self.serialPorts = serialPorts;
