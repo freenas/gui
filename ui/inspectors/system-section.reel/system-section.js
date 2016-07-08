676675199c0e7f5dd29308de0408cd7e134e909f
Xin Tan <mactanxin@gmail.com>
2016-07-07 18:50:45 -0700

    FIXED: save buttons now behave as expected in system tab

--- ui/inspectors/system-section.reel/system-section.js
+++ ui/inspectors/system-section.reel/system-section.js
@@ -1,7 +1,43 @@
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    Model = require("core/model/model").Model;
 
 /**
  * @class SystemSection
  * @extends Component
  */
-exports.SystemSection = Component.specialize();
+exports.SystemSection = Component.specialize({
+
+    _object: {
+        value: null
+    },
+
+    object: {
+        set: function(object) {
+            switch (object.identifier) {
+                case "general":
+                case "serialConsole":
+                case "languageAndRegion":
+                    this.canSave = true;
+                    this.canRevert = true;
+                    break;
+                case "updates":
+                case "bootPool":
+                    this.canSave = false;
+                    this.canRevert = false;
+                    break;
+            }
+            this._object = object;
+        },
+        get: function() {
+            return this._object;
+        }
+    },
+
+    canSave: {
+        value: null
+    },
+
+    canRevert: {
+        value: null
+    }
+});
