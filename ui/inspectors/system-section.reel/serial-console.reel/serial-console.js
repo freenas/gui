a1cc169fbdd2cdb4b6ef6888990b2daef0b8cec4
Xin Tan <mactanxin@gmail.com>
2016-07-11 16:49:06 -0700

    DELETED: redundant middleware call

--- ui/inspectors/system-section.reel/serial-console.reel/serial-console.js
+++ ui/inspectors/system-section.reel/serial-console.reel/serial-console.js
@@ -27,7 +27,6 @@ exports.SerialConsole = Component.specialize(/** @lends SerialConsole# */ {
     enterDocument: {
         value: function(isFirstTime) {
             var self = this,
-                loadingPromises = [],
                 speed = SystemAdvancedSerialspeed;
             if (isFirstTime){
                 this.isLoading = true;
@@ -36,24 +35,14 @@ exports.SerialConsole = Component.specialize(/** @lends SerialConsole# */ {
                     this.serialSpeedOptions.push({label: speed.members[i], value: speed[speed.members[i]]})
                 }
                 this.serialSpeedOptions.unshift({label: "---", value: "none"});
-                loadingPromises.push(
-                    this.application.systemAdvancedService.getSerialConsoleData().then(function(consoleData) {
-                        self.consoleData = consoleData;
-                        self.object = consoleData.systemAdvanced;
-                    }),
-                    Model.populateObjectPrototypeForType(Model.SystemAdvanced).then(function(SystemAdvanced) {
-                        return SystemAdvanced.constructor.services.serialPorts();
-                    }).then(function(serialPorts) {
-                        // self.serialPortOptions = serialPorts;
-                        self.serialPortOptions = [];
-                        for(var i=0; i<serialPorts.length; i++) {
-                            self.serialPortOptions.push({label: serialPorts[i].name, value: serialPorts[i].name});
-                        }
-                        self.serialPortOptions.unshift({label:"---", value: "none"});
-                    })
-                );
-                Promise.all(loadingPromises).then(function() {
-                    self.isLoading = false;
+                this.application.systemAdvancedService.getSerialConsoleData().then(function(consoleData) {
+                    self.consoleData = consoleData;
+                    self.object = consoleData.systemAdvanced;
+                    self.serialPortOptions = [];
+                    for(var i=0; i<consoleData.serialPorts.length; i++) {
+                        self.serialPortOptions.push({label: consoleData.serialPorts[i].name, value: consoleData.serialPorts[i].name});
+                    }
+                    self.serialPortOptions.unshift({label:"---", value: "none"});
                 });
             }
         }
