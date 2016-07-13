56b08a88ffa2f8a06fdb58877ef4b2cb8582f750
Xin Tan <mactanxin@gmail.com>
2016-07-13 15:16:50 -0700

    ADDED: debug section

---
+++ ui/inspectors/system-section.reel/debug.reel/debug.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/debug.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Debug
+ * @extends Component
+ */
+exports.Debug = Component.specialize(/** @lends Debug# */ {
+    constructor: {
+        value: function Debug() {
+            this.super();
+        }
+    }
+});
