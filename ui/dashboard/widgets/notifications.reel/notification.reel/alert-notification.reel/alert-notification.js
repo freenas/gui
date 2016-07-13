5c539f3a7e72112e3a567a86cde6a776a5a6a0b8
Thibault Zanini <thibaultzanini@gmail.com>
2016-07-13 10:46:40 -0500

    Remove AlertService from notification.reel. - Better code maintenance. - Better Performance and less memory consumption.

--- ui/dashboard/widgets/notifications.reel/notification.reel/alert-notification.reel/alert-notification.js
+++ ui/dashboard/widgets/notifications.reel/notification.reel/alert-notification.reel/alert-notification.js
@@ -1,9 +1,7 @@
 /**
  * @module ui/notification.reel
  */
-var Component = require("montage/ui/component").Component,
-    Promise = require("montage/core/promise").Promise,
-    Model = require("core/model/model").Model;
+var Component = require("montage/ui/component").Component;
 
 /**
  * @class AlertNotification
@@ -30,35 +28,12 @@ exports.AlertNotification = Component.specialize(/** @lends AlertNotification# *
         value: null
     },
 
-    // infoExpanded: {
-    //     value: false
-    // },
-
-    enterDocument: {
-        value: function() {
-            this._loadAlertService();
-        }
-    },
-
     handleDismissButtonAction: {
         value: function () {
             var self = this;
-            this._loadAlertService().then(function() {
-                self._alertService.services.dismiss(self._object.jobId);
+            this.application.alertServicePromise.then(function (alertService) {
+                alertService.services.dismiss(self._object.jobId);
             });
-        }
-    },
-
-    _loadAlertService: {
-        value: function() {
-            var self = this;
-            if (this._alertService) {
-                return Promise.resolve(this._alertService);
-            } else {
-                return Model.populateObjectPrototypeForType(Model.Alert).then(function (Alert) {
-                    self._alertService = Alert.constructor;
-                });
-            }
         }
     }
 
