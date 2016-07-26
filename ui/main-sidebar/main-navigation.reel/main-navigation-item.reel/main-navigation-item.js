eb96c7becb43a83e6f1631907a6eba0c27b674e5
Joshua Rule Dobson <joshua@joshuarule.com>
2016-07-26 11:26:57 -0400

    adds several icons to main navigation

---
+++ ui/main-sidebar/main-navigation.reel/main-navigation-item.reel/main-navigation-item.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/main-navigation-item.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class MainNavigationItem
+ * @extends Component
+ */
+exports.MainNavigationItem = Component.specialize(/** @lends MainNavigationItem# */ {
+    enterDocument: {
+        value: function(isFirstTime) {
+            this.icon.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', "#i-" + this.object.icon);
+        }
+    }
+});
