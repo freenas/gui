9b2a5670646c291e7dacfdacd0535bb21e0f9133
Xin Tan <mactanxin@gmail.com>
2016-08-01 14:18:34 -0700

    ADDED: disk traffic widget

---
+++ ui/widgets/disk-traffic.reel/disk-traffic.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/disk-traffic.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class DiskTraffic
+ * @extends Component
+ */
+exports.DiskTraffic = Component.specialize(/** @lends DiskTraffic# */ {
+    constructor: {
+        value: function DiskTraffic() {
+            this.super();
+        }
+    }
+});
