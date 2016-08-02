b2d131b7023be6c7412b2f491bbb5c8776ca9ae4
Thibault Zanini <thibaultzanini@gmail.com>
2016-08-01 17:03:56 -0700

    Preparatory works for saving the application settings to the middleware. - Blocked by #16616

--- core/service/application-context-service.js
+++ core/service/application-context-service.js
@@ -1,16 +1,13 @@
 var Montage = require("montage").Montage,
+    Model = require("core/model/model").Model,
+    application = require("montage/core/application").application,
+    FreeNASService = require("core/service/freenas-service").FreeNASService,
     WidgetService = require("core/service/widget-service").WidgetService,
     ApplicationContext = require("core/application-context").ApplicationContext,
     Promise = require("montage/core/promise").Promise;
 
 
 var ApplicationContextService = exports.ApplicationContextService = Montage.specialize({
-
-    _widgetService: {
-        get: function () {
-            return this.constructor._widgetService;
-        }
-    },
 
     save: {
         value: function () {
@@ -24,18 +21,25 @@ var ApplicationContextService = exports.ApplicationContextService = Montage.spec
                     constructor = this.constructor;
 
                 this._saveContextPromise = this._saveContextPromise = new Promise(function (resolve, reject) {
-                    var applicationContextJSON = JSON.stringify(constructor.applicationContext);
+                    var promise;
 
-                    if (this._dataSevice) { //TODO
-                        //applicationContextJSON = this._dataSevice.
+                    //FIXME: @pierre freenas-service L277 self._selectionService.saveTaskSelection(taskId, object);??
+                    if (self._dataService && false) { //experimental
+                        promise = self.findCurrentUser().then(function (user) {
+                            user.attributes = constructor.applicationContext;
+
+                            self._dataService.saveDataObject(user).then(function () {
+                                return self._saveApplicationContextLocally(constructor.applicationContext);
+                            });
+                        });
                     } else {
-                        localStorage.setItem(constructor.LOCAL_STORAGE_KEY, applicationContextJSON);
+                        promise = self._saveApplicationContextLocally(constructor.applicationContext);
                     }
 
-                    resolve(applicationContextJSON);
+                    resolve(promise);
 
                 }).then(function () {
-                    self._getContextPromise = null;
+                    self._saveContextPromise = null;
                 });
             }
 
@@ -64,8 +68,14 @@ var ApplicationContextService = exports.ApplicationContextService = Montage.spec
                     if ((applicationContext = localStorage.getItem(constructor.LOCAL_STORAGE_KEY))) {
                         applicationContext = ApplicationContext.FromJSON(applicationContext);
 
-                    } else if (this._dataSevice) { //TODO
-                        //applicationContext = this._dataSevice.
+                    } else if (self._dataService) {
+                        applicationContext = self.findCurrentUser().then(function (user) {
+                            if (user.attributes && user.attributes.dashboardContext) {
+                                return ApplicationContext.FromJSON(applicationContext);
+                            }
+
+                            return self._getDefaultApplicationContext();
+                        });
 
                     } else {
                         applicationContext = self._getDefaultApplicationContext();
@@ -93,6 +103,37 @@ var ApplicationContextService = exports.ApplicationContextService = Montage.spec
         }
     },
 
+
+    //TODO: session service?
+    findCurrentUser: {
+        value: function () {
+            var sessionUsername = application.session.username;
+
+            if (sessionUsername) {
+                if (this._currentUser && this._currentUser.username === sessionUsername) {
+                    return Promise.resolve(this._currentUser);
+                }
+
+                this._currentUser = null;
+
+                var self = this;
+
+                return this._dataService.fetchData(Model.User).then(function (users) {
+                    for (var i = 0, length = users.length; i < length; i++) {
+                        if (users[i].username === sessionUsername) {
+                            self._currentUser = users[i];
+                            break;
+                        }
+                    }
+
+                    return self._currentUser;
+                });
+            }
+
+            return Promise.reject("not logged");
+        }
+    },
+
     _getDefaultApplicationContext: {
         value: function () {
             return this._widgetService.getAvailableWidgets().then(function (widgets) {
@@ -101,6 +142,15 @@ var ApplicationContextService = exports.ApplicationContextService = Montage.spec
 
                 return applicationContext;
             });
+        }
+    },
+
+
+    //TODO: throw an error?
+    _saveApplicationContextLocally: {
+        value: function () {
+            var applicationContextJSON = JSON.stringify(constructor.applicationContext);
+            return localStorage.setItem(constructor.LOCAL_STORAGE_KEY, applicationContextJSON);
         }
     }
 
@@ -114,7 +164,8 @@ var ApplicationContextService = exports.ApplicationContextService = Montage.spec
         get: function() {
             if (!this._instance) {
                 this._instance = new ApplicationContextService();
-                this._widgetService = WidgetService.instance;
+                this._instance._dataService = FreeNASService.instance;
+                this._instance._widgetService = WidgetService.instance;
             }
 
             return this._instance;
--- ui/drawer.reel/drawer.js
+++ ui/drawer.reel/drawer.js
@@ -18,23 +18,8 @@ exports.Drawer = AbstractDropZoneComponent.specialize(/** @lends Drawer# */ {
             AbstractDropZoneComponent.prototype.enterDocument.call(this, isFirstTime);
             AbstractComponentActionDelegate.prototype.enterDocument.call(this, isFirstTime);
 
-            if (!this.applicationContext && !this._loadingPromise) {
-                var self = this;
-
-                this._loadingPromise = Promise.all([
-                    this.application.applicationContextService.get(),
-                    this.application.widgetService.getAvailableWidgets()
-                ]).then(function (arguments) {
-                    var applicationContext = arguments[0],
-                        availableWidgets = arguments[1];
-
-                    self.items = availableWidgets.toArray();
-                    self.userWidgets = applicationContext.dashboardContext.widgets;
-
-                    self.addRangeAtPathChangeListener("userWidgets", self, "_handleUserWidgetsChange");
-                }).finally(function () {
-                    self._loadingPromise = null;
-                });
+            if (isFirstTime) {
+                this.application.addEventListener("userLogged", this, false);
             }
         }
     },
@@ -45,6 +30,27 @@ exports.Drawer = AbstractDropZoneComponent.specialize(/** @lends Drawer# */ {
             AbstractComponentActionDelegate.prototype.exitDocument.call(this);
 
             this._unToggledCurrentDrawerItemIfNeeded();
+        }
+    },
+
+    handleUserLogged: {
+        value: function () {
+            var self = this;
+
+            this._loadingPromise = Promise.all([
+                this.application.applicationContextService.get(),
+                this.application.widgetService.getAvailableWidgets()
+            ]).then(function (arguments) {
+                var applicationContext = arguments[0],
+                    availableWidgets = arguments[1];
+
+                self.items = availableWidgets.toArray();
+                self.userWidgets = applicationContext.dashboardContext.widgets;
+
+                self.addRangeAtPathChangeListener("userWidgets", self, "_handleUserWidgetsChange");
+            }).finally(function () {
+                self._loadingPromise = null;
+            });
         }
     },
 
