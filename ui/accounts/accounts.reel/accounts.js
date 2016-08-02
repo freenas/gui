5962d0bd0da6e6024b1c1bb2b42ecbaff685b6c2
Thibault Zanini <thibaultzanini@gmail.com>
2016-08-02 14:40:51 -0700

    Fix getting new instance of directoryServices.

--- ui/accounts/accounts.reel/accounts.js
+++ ui/accounts/accounts.reel/accounts.js
@@ -38,10 +38,11 @@ exports.Accounts = Component.specialize({
                     accountCategories.user = dataService.getEmptyCollectionForType(Model.User);
                     accountCategories.group = dataService.getEmptyCollectionForType(Model.Group);
                     accountCategories.system = dataService.getEmptyCollectionForType(Model.AccountSystem);
-                    accountCategories.directoryServices = dataService.getNewInstanceForType(Model.AccountDirectoryServices);
 
-                    return Promise.all([self._listUsers(), self._listGroups()]);
-                }).then(function() {
+                    return dataService.getNewInstanceForType(Model.AccountDirectoryServices).then(function (directoryServices) {
+                        accountCategories.directoryServices = directoryServices;
+                    });
+                }).then(Promise.all([self._listUsers(), self._listGroups()])).then(function() {
                     self.accountCategories.isLoading = false;
                     self._loadDataPromise = null;
                 });
