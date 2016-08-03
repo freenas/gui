482afd4f474c4378fdd8147240953480dcf4df34
Joshua Rule Dobson <joshua@joshuarule.com>
2016-08-03 15:01:31 -0400

    adds directoryServices icon component

---
+++ ui/icons/directory-services.reel/directory-services.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/directory-services.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class DirectoryServices
+ * @extends Component
+ */
+exports.DirectoryServices = Component.specialize(/** @lends DirectoryServices# */ {
+    constructor: {
+        value: function DirectoryServices() {
+            this.super();
+        }
+    }
+});
