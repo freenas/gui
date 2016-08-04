d385a76005477c10d8e0b9f122d7c90911fa7afd
Joshua Rule Dobson <joshua@joshuarule.com>
2016-08-04 16:35:54 -0400

    adds wip services status

---
+++ ui/services/services-list-item.reel/services-list-item.js
@@ -0,0 +1,10 @@
+/**
+ * @module ui/services-list-item.reel
+ */
+var ListItem = require("ui/controls/list.reel/list-item.reel/list-item").ListItem;
+
+/**
+ * @class InterfacesListItem
+ * @extends Component
+ */
+exports.ServicesListItem = ListItem.specialize();
