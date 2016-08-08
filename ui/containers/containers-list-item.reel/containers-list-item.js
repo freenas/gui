248ce6ecc2c16403fbb1a6120dc374790b774408
Joshua Rule Dobson <joshua@joshuarule.com>
2016-08-08 15:37:45 -0400

    adds containers-list-item component to add status lights

---
+++ ui/containers/containers-list-item.reel/containers-list-item.js
@@ -0,0 +1,10 @@
+/**
+ * @module ui/services-list-item.reel
+ */
+var ListItem = require("ui/controls/list.reel/list-item.reel/list-item").ListItem;
+
+/**
+ * @class InterfacesListItem
+ * @extends Component
+ */
+exports.ContainersListItem = ListItem.specialize();
