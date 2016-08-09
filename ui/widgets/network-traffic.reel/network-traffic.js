81412c2f56940e83b96fd6ddc0c14bcf4f37dc58
Thibault Zanini <thibaultzanini@gmail.com>
2016-08-08 18:57:19 -0700

    Bring back to life the network widget. Ticket: #16663

--- ui/widgets/network-traffic.reel/network-traffic.js
+++ ui/widgets/network-traffic.reel/network-traffic.js
@@ -1,7 +1,22 @@
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    Model = require("core/model/model").Model;
 
 /**
  * @class NetworkTraffic
  * @extends Component
  */
-exports.NetworkTraffic = Component.specialize();
+exports.NetworkTraffic = Component.specialize({
+
+    enterDocument: {
+        value: function () {
+            if (!this.interfaces) {
+                var self = this;
+
+                this.application.dataService.fetchData(Model.NetworkInterface).then(function (interfaces) {
+                    self.interfaces = interfaces;
+                });
+            }
+        }
+    }
+
+});
