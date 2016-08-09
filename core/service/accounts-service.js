26dcbce20c38a9c3fd27040859902d47743e4744
Ben Milman <bpmilman@gmail.com>
2016-08-09 15:05:46 -0700

    Add accounts service

---
+++ core/service/accounts-service.js
@@ -0,0 +1,46 @@
+var Montage = require("montage").Montage,
+    BackEndBridgeModule = require("../backend/backend-bridge");
+
+var AccountsService = exports.AccountsService = Montage.specialize({
+
+    _NAMESPACE: {
+        value: ""
+    },
+
+    _instance: {
+        value: null
+    },
+
+    _backendBridge: {
+        value: null
+    },
+
+    getShells: {
+        value: function(){
+            return this._callBackend("shell.get_shells").then(function(response) {
+                return response.data;
+            });
+        }
+    },
+
+    _callBackend: {
+        value: function(method, args) {
+            args = args || [];
+            return this._backendBridge.send("rpc", "call", {
+                method: this._NAMESPACE + method,
+                args: args
+            });
+        }
+    }
+
+}, {
+    instance: {
+        get: function() {
+            if (!this._instance) {
+                this._instance = new AccountsService();
+                this._instance._backendBridge = BackEndBridgeModule.defaultBackendBridge;
+            }
+            return this._instance;
+        }
+    }
+});
