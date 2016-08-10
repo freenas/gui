46c0f3ea1dfb280da9a60e2b9a1149f120eea22b
Pierre Chaussalet <pierre@ixsystems.com>
2016-08-10 16:12:33 -0700

    Use bar graph in disk i/o widget

--- ui/widgets/disk-traffic.reel/disk-traffic.js
+++ ui/widgets/disk-traffic.reel/disk-traffic.js
@@ -11,9 +11,9 @@ var Component = require("montage/ui/component").Component,
 exports.DiskTraffic = Component.specialize(/** @lends DiskTraffic# */ {
     enterDocument: {
         value: function() {
-            if (!this.interfaces) {
+            if (!this.disks) {
                 var self = this;
-                this.application.dataService.fetchData(Model.Disk).then(function (disks) {
+                this.application.dataService.fetchData(Model.Disk).then(function(disks) {
                     self.disks = disks;
                 });
             }
