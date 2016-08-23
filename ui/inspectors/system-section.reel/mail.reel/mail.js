0de13614d9bcf5d1f530a813deaa73406eb89d9f
Xin Tan <mactanxin@gmail.com>
2016-08-23 11:06:48 -0700

    ADDED: save mail data function

--- ui/inspectors/system-section.reel/mail.reel/mail.js
+++ ui/inspectors/system-section.reel/mail.reel/mail.js
@@ -8,19 +8,30 @@ var Component = require("montage/ui/component").Component;
  * @extends Component
  */
 exports.Mail = Component.specialize(/** @lends Mail# */ {
+
+    handlesendTestMailAction: {
+        value: function() {
+            this._mailService.sendTestMail();
+        }
+    },
+
     enterDocument: {
         value: function(isFirstTime) {
             var self = this;
-            
+
             if(isFirstTime) {
                 this.isLoading = true;
                 this.application.mailService.getMailData().then(function(mailData) {
-                    self.mailData = mailData;
                     self.object = mailData.mail;
-                    console.log(self.object);
                 });
                 self.isLoading = false;
             }
+        }
+    },
+
+    save: {
+        value: function() {
+            return this.application.mailService.saveMailData(this.object);
         }
     }
 });
