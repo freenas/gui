58e488b80829fd49669ddeffa246f810de07908d
Xin Tan <mactanxin@gmail.com>
2016-08-22 17:32:59 -0700

    FIXED: now dropdown data shows correctly

--- ui/controls/timezone-picker.reel/timezone-picker.js
+++ ui/controls/timezone-picker.reel/timezone-picker.js
@@ -8,6 +8,24 @@ var Component = require("montage/ui/component").Component;
  * @extends Component
  */
 exports.TimezonePicker = Component.specialize(/** @lends TimezonePicker# */ {
+    _timezone: {
+        value: null
+    },
+
+    timezone: {
+        get: function() {
+            return this._timezone;
+        },
+        set: function(timezone) {
+            if (this._timezone !== timezone) {
+                this._timezone = timezone;
+                if (timezone) {
+                    $(this.imageElement).timezonePicker('updateTimezone', timezone);
+                }
+            }
+        }
+    },
+
     draw: {
         value: function() {
             $(this.imageElement).timezonePicker({
@@ -17,6 +35,7 @@ exports.TimezonePicker = Component.specialize(/** @lends TimezonePicker# */ {
             });
         }
     },
+
     handleChange: {
         value: function(timezone, country, offset) {
             this.timezone = timezone;
