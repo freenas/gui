d31fe17827ccbc38ec4673e88b9cca1782b220f5
Thibault Zanini <thibaultzanini@gmail.com>
2016-08-24 14:54:37 -0700

    Add directory-service-item list

---
+++ ui/inspectors/directory-services.reel/directory-service-item-list.reel/directory-service-item-list.js
@@ -0,0 +1,7 @@
+var ListItem = require("ui/controls/list.reel/list-item.reel/list-item").ListItem;
+
+/**
+ * @class DirectoryServiceItemList
+ * @extends Component
+ */
+exports.DirectoryServiceItemList = ListItem.specialize();
