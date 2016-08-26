9b0dcce6fbf36e22caaf147c978a09167f32d8ff
Xin Tan <mactanxin@gmail.com>
2016-08-26 16:14:32 -0700

    UPDATED: temporally hide alert&filter section

--- ui/settings/settings.reel/settings.js
+++ ui/settings/settings.reel/settings.js
@@ -18,10 +18,10 @@ exports.Settings = Component.specialize({
                 sectionsPromises.push(this._createSection("updates", "Updates", "path", i++));
                 sectionsPromises.push(this._createSection("languageAndRegion", "Language & Region", "path", i++));
                 sectionsPromises.push(this._createSection("WebUI", "WebUI", "path", i++));
-                sectionsPromises.push(this._createSection("mail", "Alerts & Reporting", "path", i++));
                 sectionsPromises.push(this._createSection("serialConsole", "Console", "path", i++));
                 sectionsPromises.push(this._createSection("debug", "Debug", "path", i++));
 /* FIXME: Uncomment when implemented
+                sectionsPromises.push(this._createSection("mail", "Alerts & Reporting", "path", i++));
                 sectionsPromises.push(this._createSection("kernel", "kernel", "path", i++));
                 sectionsPromises.push(this._createSection("hardware", "Hardware", "path", i++));
                 sectionsPromises.push(this._createSection("advanced", "Hardware", "path", i++));
