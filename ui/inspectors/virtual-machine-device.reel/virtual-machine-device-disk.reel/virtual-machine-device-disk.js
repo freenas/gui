c944d0633fbb73c5732617681fc82b00e1520041
Ben Milman <bpmilman@gmail.com>
2016-08-26 22:22:16 -0700

    Make sure VM disk devices still display humanized size after save
    Ticket: #16599

--- ui/inspectors/virtual-machine-device.reel/virtual-machine-device-disk.reel/virtual-machine-device-disk.js
+++ ui/inspectors/virtual-machine-device.reel/virtual-machine-device-disk.reel/virtual-machine-device-disk.js
@@ -7,6 +7,29 @@ var Component = require("montage/ui/component").Component,
  */
 exports.VirtualMachineDeviceDisk = Component.specialize({
 
+    _diskSize: {
+        value: null
+    },
+
+    diskSize: {
+        get: function () {
+            if (typeof this._diskSize === "string") {
+                return this._diskSize;
+            } else if (!!this.object.size && typeof this.object.size === "number") {
+                return this.application.storageService.convertBytesToSizeString(this.object.size);
+            }
+            return "";
+        },
+
+        set: function(diskSize) {
+            var convertedBytes = this.application.storageService.convertSizeStringToBytes(diskSize);
+            if (this._diskSize !== diskSize) {
+                this._diskSize = diskSize;
+                this.object.size = convertedBytes ? convertedBytes : this.object.size;
+            }
+        }
+    },
+
     diskModeOptions: {
         value: null
     },
@@ -14,6 +37,20 @@ exports.VirtualMachineDeviceDisk = Component.specialize({
     constructor: {
         value: function() {
             this.diskModeOptions = VmDeviceDiskMode.members;
+        }
+    },
+
+    enterDocument: {
+        value: function() {
+            if (typeof this.object.size === "number") {
+                this.diskSize = this.application.storageService.convertBytesToSizeString(this.object.size);
+            }
+        }
+    },
+
+    exitDocument: {
+        value: function() {
+            this.diskSize = null;
         }
     }
 });
