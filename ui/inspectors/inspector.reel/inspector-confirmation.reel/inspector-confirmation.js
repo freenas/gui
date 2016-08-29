67887452d6d35fa823e8147f5722c1ac08d19a9f
Joshua Rule Dobson <joshua@joshuarule.com>
2016-08-29 12:15:02 -0400

    adds template for inspector confirmation Ticket: #15575

---
+++ ui/inspectors/inspector.reel/inspector-confirmation.reel/inspector-confirmation.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/inspector-confirmation.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class InspectorConfirmation
+ * @extends Component
+ */
+exports.InspectorConfirmation = Component.specialize(/** @lends InspectorConfirmation# */ {
+    constructor: {
+        value: function InspectorConfirmation() {
+            this.super();
+        }
+    }
+});
