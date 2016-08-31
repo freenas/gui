b5d2f2e5b76f680f348e4f31e60ac295787f9b5b
Thibault Zanini <thibaultzanini@gmail.com>
2016-08-31 09:46:31 -0700

    Refactor container inspectors.

---
+++ ui/inspectors/container-creator.reel/container-creator.js
@@ -0,0 +1,152 @@
+/**
+ * @module ui/container-creator.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class ContainerCreator
+ * @extends Component
+ */
+exports.ContainerCreator = Component.specialize(/** @lends ContainerCreator# */ {
+    
+    templateDidLoad: {
+        value: function () {
+            var self = this;
+            this._environement = {};
+
+            this.blockDrawGate.setField("volume", false);
+
+            this.application.dataService.getNewInstanceForType(Model.DockerVolume).then(function (volume) {
+                self._volume = volume;
+                self.blockDrawGate.setField("volume", true);
+            });
+
+            Model.populateObjectPrototypeForType(Model.DockerImage).then(function (DockerImage) {
+                self._dockerImageService = DockerImage.constructor.services;
+            });
+        }
+    },
+
+    enterDocument: {
+        value: function () {
+            this._fetchDataIfNeeded();
+        }  
+    },
+
+    exitDocument: {
+        value: function () {
+            this._reset();
+        }
+    },
+
+    _fetchDataIfNeeded: {
+        value: function () {
+            if (!this._images) {
+                var self = this;
+                this._dockerImageService.getTemplates().then(function(templates) {
+                    var templatesNames = Object.keys(templates);
+                    self._images = templatesNames.map(function(x) {
+                        return {
+                            label: x,
+                            value: templates[x].image
+                        };
+                    });
+
+                    self._templates = templates;
+                });
+            }
+        } 
+    },
+
+    _reset: {
+        value: function () {
+            this._volume.clear();
+            this._environement.clear();
+        }
+    },
+
+    _getPortsFromArray: {
+        value: function (array) {
+            var ports = null,
+                string = array.join(" "),
+                error;
+
+            if (string) {
+                var data = string.split(/:|\/| /),
+                    port, containerPort, hostPort;
+
+                ports = [];
+
+                for (var i = 0, length = data.length; i + 3 <= length; i = i + 3) {
+                    ports.push({ 
+                        container_port: +data[i],
+                        host_port: +data[i + 1],
+                        protocol: data[i + 2].toUpperCase()
+                    });
+                }
+            }
+
+            return ports;
+        }
+    },
+
+    _getEnvironmentVariableFromArray: {
+        value: function (array) {
+            var env = null,
+                string = array.join(" ");
+
+            if (string) {
+                var data = string.split(/ |=/);
+                env = this._environement;
+
+                for (var i = 0, length = data.length; i + 2 <= length; i = i + 2) {
+                    env[data[i]] = data[i + 1];
+                }
+            }
+
+            return env;
+        }
+    },
+
+    save: {
+        value: function () {
+            var environmentComponentValues = this._environmentComponent.values,
+                commandString = this._commandComponent.value,
+                namesString = this._nameComponent.value,
+                portsValues = this._portsComponent.values,
+                spaceString = " ",
+                self = this;
+
+            if (commandString) {
+                this.object.command = commandString.split(spaceString);
+            }
+
+            if (namesString) {
+                if (Array.isArray(this.object.names)) {
+                    this.object.names[0] = spaceString;
+                } else {
+                    this.object.names = [spaceString];
+                }
+            }
+
+            if (environmentComponentValues) {
+                this.object.environment = this._getEnvironmentVariableFromArray(environmentComponentValues);
+            }
+
+            if (portsValues.length) {
+                this.object.ports = this._getPortsFromArray(portsValues);
+            }
+
+            if (this._volume.container_path && this._volume.host_path) {
+                this.object.volumes = [this._volume];
+            } else if (this.object.volumes && this.object.volumes.length) {
+                this.object.volumes = null;
+            }
+
+            return this.application.dataService.saveDataObject(this.object).then(function () {
+                self._reset();
+            });
+        }
+    }
+
+});
