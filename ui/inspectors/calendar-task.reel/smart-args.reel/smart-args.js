4798ae502f9f53228359c3adaa515b987378c338
Pierre Chaussalet <pierre@ixsystems.com>
2016-08-31 00:49:05 -0700

    Fix smart task disks parameter format

--- ui/inspectors/calendar-task.reel/smart-args.reel/smart-args.js
+++ ui/inspectors/calendar-task.reel/smart-args.reel/smart-args.js
@@ -15,41 +15,7 @@ exports.SmartArgs = Component.specialize(/** @lends SmartArgs# */ {
             this.application.storageService.listDisks().then(function(disks) {
                 self.disks = disks;
             });
-            if (this.args) {
-                this.selectedDisks = this._objectToArray(this.args[0]);
-            } else {
-                this.args = [];
-            }
-            this.addRangeAtPathChangeListener("selectedDisks", this, "_handleSelectedDisksChange");
             this.testTypes = DiskSelftestType.members;
-        }
-    },
-
-    _handleSelectedDisksChange: {
-        value: function() {
-            this.args[0] = this._arrayToObject(this.selectedDisks);
-        }
-    },
-
-    _objectToArray: {
-        value: function(object) {
-            var array = [];
-            for (var i in object) {
-                if (object.hasOwnProperty(i)) {
-                    array.push(object[i]);
-                }
-            }
-            return array;
-        }
-    },
-
-    _arrayToObject: {
-        value: function(array) {
-            var object = {};
-            for (var i = 0, length = array.length; i < length; i++) {
-                object[i] = array[i];
-            }
-            return object;
         }
     }
 });
