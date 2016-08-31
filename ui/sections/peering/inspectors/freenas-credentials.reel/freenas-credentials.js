f2b8a385280080f0bb673c6c5b76a8bdba0a0eb7
Ben Milman <bpmilman@gmail.com>
2016-08-31 14:46:11 -0700

    Rename replication-credentials component to freenas-credentials

---
+++ ui/sections/peering/inspectors/freenas-credentials.reel/freenas-credentials.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/sections/peering/inspectors/freenas-credentials.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class FreenasCredentials
+ * @extends Component
+ */
+exports.FreenasCredentials = Component.specialize(/** @lends FreenasCredentials# */ {
+    constructor: {
+        value: function FreenasCredentials() {
+            this.super();
+        }
+    }
+});
