c81ff901125d023ac305419a07f1f2c0e3e30106
Thibault Zanini <thibaultzanini@gmail.com>
2016-08-30 21:27:12 -0700

    Add "Run Now" button to Calendar tasks Ticket: #17116

--- ui/inspectors/calendar-task.reel/calendar-task.js
+++ ui/inspectors/calendar-task.reel/calendar-task.js
@@ -1,10 +1,11 @@
-var Component = require("montage/ui/component").Component;
+var AbstractComponentActionDelegate = require("core/ui/abstract-component-action-delegate").AbstractComponentActionDelegate,
+    Model = require("core/model/model").Model;
 
 /**
  * @class CalendarTask
  * @extends Component
  */
-exports.CalendarTask = Component.specialize({
+exports.CalendarTask = AbstractComponentActionDelegate.specialize({
     templateDidLoad: {
         value: function() {
             this.taskCategories = [{ name: '---', value: null }].concat(this.application.calendarService.taskCategories);
@@ -13,17 +14,29 @@ exports.CalendarTask = Component.specialize({
 
     enterDocument: {
         value: function(isFirstTime) {
+            AbstractComponentActionDelegate.prototype.enterDocument.call(this, isFirstTime);
+
             if (this.object && this.object.task) {
                 this.classList.add('type-' + this.object.task.replace('.', '_').toLowerCase());
             }
             if (this.object._isNew) {
                 this.object.args = [];
             }
+
+            if (isFirstTime) {
+                var self = this;
+
+                Model.populateObjectPrototypeForType(Model.CalendarTask).then(function () {
+                    self._calendarTaskService = Model.CalendarTask.objectPrototype.services;
+                });
+            }
         }
     },
 
     exitDocument: {
         value: function() {
+            AbstractComponentActionDelegate.prototype.exitDocument.call(this);
+
             if (this.object && this.object.task) {
                 this.classList.remove('type-' + this.object.task.replace('.', '_').toLowerCase());
                 console.log(this.object.schedule);
@@ -38,6 +51,14 @@ exports.CalendarTask = Component.specialize({
                 return !!x || (typeof x !== "undefined" && typeof x !== "object") ; 
             });
             this.inspector.save();
+        }
+    },
+
+    handleRunNowAction: {
+        value: function () {
+            if (!this.object._isNew) {
+                this._calendarTaskService.run(this.object.id);
+            }
         }
     }
 
