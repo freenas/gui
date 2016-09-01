7b026a306cf3cf19ec2b23040ef8df33b17c0be8
Thibault Zanini <thibaultzanini@gmail.com>
2016-08-31 17:06:44 -0700

    Add actions to container inspectors. Ticket: #17109

--- ui/inspectors/container.reel/container.js
+++ ui/inspectors/container.reel/container.js
@@ -1,10 +1,34 @@
 /**
  * @module ui/container.reel
  */
-var Component = require("montage/ui/component").Component;
+var AbstractComponentActionDelegate = require("core/ui/abstract-component-action-delegate").AbstractComponentActionDelegate;
 
 /**
  * @class Container
  * @extends Component
  */
-exports.Container = Component.specialize();
+exports.Container = AbstractComponentActionDelegate.specialize({
+
+    handleStartAction: {
+        value: function() {
+            this.object.services.start(this.object.id);
+        }
+    },
+
+    handleStopAction: {
+        value: function() {
+            this.object.services.stop(this.object.id);
+        }
+    },
+
+    handleSerialConsoleAction: {
+        value: function() {
+            var self = this;
+
+            this.application.consoleService.getSerialToken(this.object.id).then(function(token) {
+                window.open("/serial-console-app/#" + token, self.object.names[0] + " Serial Console");
+            });
+        }
+    }
+
+});
