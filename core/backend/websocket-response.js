d99bfb96242c7ade558ec637b156d3ff1f66f00b
Thibault Zanini <thibaultzanini@gmail.com>
2016-09-01 10:20:00 -0700

    Remove a pointer to the original “raw” response from the middleware. [Rebase failed] This property was for development purpose but we never used it. Removing it will improve the memory consumption.

--- core/backend/websocket-response.js
+++ core/backend/websocket-response.js
@@ -1,7 +1,4 @@
 exports.WebSocketResponse = function WebSocketResponse (rawResponse) {
     this.data = rawResponse.args;
     this.timestamp = !isNaN(rawResponse.timestamp) ? rawResponse.timestamp * 1000 : 0;
-
-    //FIXME: Development purpose.
-    this._rawResponse = rawResponse;
 };
