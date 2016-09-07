01010404ce80043c5e782f7e8d13353f16e616a7
Joshua Rule Dobson <joshua@joshuarule.com>
2016-09-07 18:08:57 -0400

    adds zvol icon component

---
+++ ui/icons/zvol.reel/zvol.js
@@ -0,0 +1,10 @@
+/**
+ * @module ui/zvol.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Zvol
+ * @extends Component
+ */
+exports.Zvol = Component.specialize();
