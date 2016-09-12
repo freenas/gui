a15a2b7cfa3e0a3770e964ea3f93251c9e0c0250
Xin Tan <mactanxin@gmail.com>
2016-09-12 15:44:21 -0700

    ADDED: tunable components

---
+++ ui/inspectors/system-section.reel/tunable.reel/tunable.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/tunable.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Tunable
+ * @extends Component
+ */
+exports.Tunable = Component.specialize(/** @lends Tunable# */ {
+    constructor: {
+        value: function Tunable() {
+            this.super();
+        }
+    }
+});
