93ba8ae05e3bd9071e8a4b467af3760cca91ee4d
Xin Tan <mactanxin@gmail.com>
2016-09-16 10:37:16 -0700

    ADDED: system overview components

---
+++ ui/inspectors/system-section.reel/overview.reel/overview.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/inspectors/system-section.reel/overview.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Overview
+ * @extends Component
+ */
+exports.Overview = Component.specialize(/** @lends Overview# */ {
+    constructor: {
+        value: function Overview() {
+            this.super();
+        }
+    }
+});
