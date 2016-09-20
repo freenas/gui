111da07f4776720da7e2194bc188ba4955fcbf3f
Ben Milman <bpmilman@gmail.com>
2016-09-20 10:42:24 -0700

    Remove replication-related dead code in freenas-service

--- core/service/freenas-service.js
+++ core/service/freenas-service.js
@@ -277,10 +277,7 @@ var FreeNASService = exports.FreeNASService = RawDataService.specialize({
                     serviceDescriptor = isUpdate ?
                         Services.findUpdateServiceForType(type) : Services.findCreateServiceForType(type);
 
-                if (type === Model.ReplicationLink) {
-                    serviceDescriptor = isUpdate ?
-                        Services.findUpdateServiceForType(Model.Replication) : Services.findCreateServiceForType(Model.Replication);
-                } else if (type === Model.DirectoryserviceConfig) {
+                if (type === Model.DirectoryserviceConfig) {
                     serviceDescriptor = {
                         "method": "task.submit",
                         "name": "call",
