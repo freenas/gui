9ed887c77a06610c4684987b390b67e803e483c9
Joshua Rule Dobson <joshua@joshuarule.com>
2016-09-20 09:24:16 -0400

    wip widget size classes

--- ui/dashboard/widgets/widget-wrapper.reel/widget-wrapper.js
+++ ui/dashboard/widgets/widget-wrapper.reel/widget-wrapper.js
@@ -11,6 +11,31 @@ exports.WidgetWrapper = AbstractDraggableComponent.specialize(/** @lends WidgetW
 
     _placeHolderStrategy: {
         value: "remove"
+    },
+
+    _size: {
+        value: null
+    },
+
+    size: {
+        get: function () {
+            return this._size;
+        },
+        set: function (option) {
+            if(this._size !== option) {
+                this.classList.remove("Widget--" + this._size);
+                this.classList.add("Widget--" + option);
+                this._size = option;
+            }
+        }
+    },
+
+    enterDocument: {
+        value: function (isFirstTime) {
+            if(isFirstTime) {
+                this.size = "small";
+            }
+        }
     }
 
 });
--- ui/dashboard/widgets/widget.reel/widget.js
+++ ui/dashboard/widgets/widget.reel/widget.js
@@ -5,22 +5,6 @@ var Component = require("montage/ui/component").Component;
  * @extends Component
  */
 exports.Widget = Component.specialize({
-    _size: {
-        value: null
-    },
-
-    size: {
-        get: function () {
-            return this._size;
-        },
-        set: function (option) {
-            if(this._size !== option) {
-                this.classList.remove("is-" + this._size);
-                this.classList.add("is-" + option);
-                this._size = option;
-            }
-        }
-    },
 
     isFlipped: {
         value: false
