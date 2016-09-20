9117ffe0a5595a906c39852e662d7ed1e42c9f7f
Xin Tan <mactanxin@gmail.com>
2016-09-20 10:28:18 -0700

    UPDATED: push all promises into loading promise

--- ui/inspectors/system-section.reel/overview.reel/overview.js
+++ ui/inspectors/system-section.reel/overview.reel/overview.js
@@ -16,29 +16,36 @@ exports.Overview = Component.specialize(/** @lends Overview# */ {
 
     templateDidLoad: {
         value: function () {
-            var self = this;
+            var self = this,
+                loadingPromises = [];
+            this.isLoading = true;
             this.systemInfo = {};
-            this.application.dataService.fetchData(Model.SystemGeneral).then(function(systemGeneral) {
-                console.log(systemGeneral[0]);
-                self.systemInfo.general = systemGeneral[0];
-            });
-            this.application.storageService.listDatasets().then(function(datasetList) {
-                self.systemInfo.totalDatasets = datasetList.length;
-            });
-            this.application.dataService.fetchData(Model.VolumeSnapshot).then(function (snapshots) {
-                self.systemInfo.totalSnapshots = snapshots.length;
-            });
-            this.application.dataService.fetchData(Model.Share).then(function(shares) {
-                self.systemInfo.totalShares = shares.length;
-            });
-            this.application.systemInfoService.getVersion().then(function(version) {
-                self.systemInfo.systemVersion = version;
-            });
-            this.application.dataService.fetchData(Model.Peer).then(function (peers) {
-                self.systemInfo.totalPeers = peers.length;
-            });
-            this.application.dataService.fetchData(Model.Replication).then(function (replications) {
-                self.systemInfo.totalReplications = replications.length;
+            loadingPromises.push(
+                this.application.dataService.fetchData(Model.SystemGeneral).then(function(systemGeneral) {
+                    console.log(systemGeneral[0]);
+                    self.systemInfo.general = systemGeneral[0];
+                }),
+                this.application.storageService.listDatasets().then(function(datasetList) {
+                    self.systemInfo.totalDatasets = datasetList.length;
+                }),
+                this.application.dataService.fetchData(Model.VolumeSnapshot).then(function (snapshots) {
+                    self.systemInfo.totalSnapshots = snapshots.length;
+                }),
+                this.application.dataService.fetchData(Model.Share).then(function(shares) {
+                    self.systemInfo.totalShares = shares.length;
+                }),
+                this.application.systemInfoService.getVersion().then(function(version) {
+                    self.systemInfo.systemVersion = version;
+                }),
+                this.application.dataService.fetchData(Model.Peer).then(function (peers) {
+                    self.systemInfo.totalPeers = peers.length;
+                }),
+                this.application.dataService.fetchData(Model.Replication).then(function (replications) {
+                    self.systemInfo.totalReplications = replications.length;
+                })
+            );
+            Promise.all(loadingPromises).then(function() {
+                this.isLoading = false;
             })
         }
     }
