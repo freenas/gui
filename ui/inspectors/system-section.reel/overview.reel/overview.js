910b69c1e0ff7b225dd94f6bd3b5196c113f49c3
Xin Tan <mactanxin@gmail.com>
2016-09-20 10:01:27 -0700

    ADDED: fetch data from models

--- ui/inspectors/system-section.reel/overview.reel/overview.js
+++ ui/inspectors/system-section.reel/overview.reel/overview.js
@@ -1,16 +1,45 @@
 /**
  * @module ui/inspectors/system-section.reel/overview.reel
  */
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    Model = require("core/model/model").Model;
 
 /**
  * @class Overview
  * @extends Component
  */
 exports.Overview = Component.specialize(/** @lends Overview# */ {
-    constructor: {
-        value: function Overview() {
-            this.super();
+
+    systemInfo: {
+        value: null
+    },
+
+    templateDidLoad: {
+        value: function () {
+            var self = this;
+            this.systemInfo = {};
+            this.application.dataService.fetchData(Model.SystemGeneral).then(function(systemGeneral) {
+                console.log(systemGeneral[0]);
+                self.systemInfo.general = systemGeneral[0];
+            });
+            this.application.storageService.listDatasets().then(function(datasetList) {
+                self.systemInfo.totalDatasets = datasetList.length;
+            });
+            this.application.dataService.fetchData(Model.VolumeSnapshot).then(function (snapshots) {
+                self.systemInfo.totalSnapshots = snapshots.length;
+            });
+            this.application.dataService.fetchData(Model.Share).then(function(shares) {
+                self.systemInfo.totalShares = shares.length;
+            });
+            this.application.systemInfoService.getVersion().then(function(version) {
+                self.systemInfo.systemVersion = version;
+            });
+            this.application.dataService.fetchData(Model.Peer).then(function (peers) {
+                self.systemInfo.totalPeers = peers.length;
+            });
+            this.application.dataService.fetchData(Model.Replication).then(function (replications) {
+                self.systemInfo.totalReplications = replications.length;
+            })
         }
     }
 });
