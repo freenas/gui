80c6dc506f744788c7eeee9f702e70e700f5c094
Xin Tan <mactanxin@gmail.com>
2016-09-20 17:08:32 -0700

    DELETED: remove irrelevant functions to other services

--- core/service/storage-service.js
+++ core/service/storage-service.js
@@ -59,7 +59,7 @@ var StorageService = exports.StorageService = Montage.specialize({
         }
     },
 
-    getVolumeSnapshotData: {
+    listVolumeSnapshotData: {
         value: function() {
             return this._dataService.fetchData(Model.VolumeSnapshot).then(function (snapshots) {
                 return snapshots;
@@ -71,22 +71,6 @@ var StorageService = exports.StorageService = Montage.specialize({
         value: function() {
             return this._dataService.fetchData(Model.Share).then(function (shares) {
                 return shares;
-            });
-        }
-    },
-
-    getPeerData: {
-        value: function() {
-            return this._dataService.fetchData(Model.Peer).then(function (peers) {
-                return peers;
-            });
-        }
-    },
-
-    getReplicationData: {
-        value: function() {
-            return this._dataService.fetchData(Model.Replication).then(function (replications) {
-                return replications;
             });
         }
     },
