dfe89a97bed53d4642775cdd87d59cabbbb9f69a
Xin Tan <mactanxin@gmail.com>
2016-09-20 17:06:15 -0700

    ADDED: move get replication data to service

---
+++ core/service/replication-service.js
@@ -0,0 +1,30 @@
+var Montage = require("montage").Montage,
+    FreeNASService = require("core/service/freenas-service").FreeNASService,
+    Promise = require("montage/core/promise").Promise,
+    Model = require("core/model/model").Model;
+
+var ReplicationService = exports.ReplicationService = Montage.specialize({
+    constructor: {
+        value: function() {
+            this._dataService = FreeNASService.instance;
+        }
+    },
+    
+    listReplicationData: {
+        value: function() {
+            return this._dataService.fetchData(Model.Replication).then(function (replications) {
+                return replications;
+            });
+        }
+    }
+
+}, {
+    instance: {
+        get: function() {
+            if (!this._instance) {
+                this._instance = new ReplicationService();
+            }
+            return this._instance;
+        }
+    }
+});
