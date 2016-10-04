e3de0d3c1d2fbc122ba1ce3d926b44e0f1ca7890
Joshua Rule Dobson <joshua@joshuarule.com>
2016-10-04 12:13:39 -0400

    adds cron-job inspector

---
+++ ui/inspectors/cron-job.reel/cron-job-rule.reel/cron-job-rule.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/cron-job-rule.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class CronJobRule
+ * @extends Component
+ */
+exports.CronJobRule = Component.specialize(/** @lends CronJobRule# */ {
+    constructor: {
+        value: function CronJobRule() {
+            this.super();
+        }
+    }
+});
