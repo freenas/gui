da6eea9f17c2938d64bd90418d284bbf88ca05f4
Joshua Rule Dobson <joshua@joshuarule.com>
2016-10-05 11:13:34 -0400

    update cron-job-rule to work with mulitple-select-grid updates, removes constructor object

--- ui/inspectors/cron-job.reel/cron-job-rule.reel/cron-job-rule.js
+++ ui/inspectors/cron-job.reel/cron-job-rule.reel/cron-job-rule.js
@@ -7,10 +7,4 @@ var Component = require("montage/ui/component").Component;
  * @class CronJobRule
  * @extends Component
  */
-exports.CronJobRule = Component.specialize(/** @lends CronJobRule# */ {
-    constructor: {
-        value: function CronJobRule() {
-            this.super();
-        }
-    }
-});
+exports.CronJobRule = Component.specialize();
