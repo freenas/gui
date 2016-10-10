c9234ffa0de45be64759ae8d20303dbac66ed4dd
Erin Clark <erin@ixsystems.com>
2016-10-10 10:28:22 -0700

    Add tftpd service (#119)
    Ticket: #17865

--- ui/inspectors/service.reel/tftp-service.reel/tftp-service.js
+++
@@ -1,7 +0,0 @@
-var Component = require("montage/ui/component").Component;
-
-/**
- * @class TftpService
- * @extends Component
- */
-exports.TftpService = Component.specialize();
---
+++ ui/inspectors/service.reel/tftpd-service.reel/tftpd-service.js
@@ -0,0 +1,66 @@
+var Component = require("montage/ui/component").Component,
+    Model = require("core/model/model").Model;
+
+/**
+ * @class TftpdService
+ * @extends Component
+ */
+exports.TftpdService = Component.specialize({
+
+    users: {
+        value: null
+    },
+
+    templateDidLoad: {
+        value: function () {
+            var self = this;
+
+            this.application.dataService.fetchData(Model.User).then(function(users) {
+                self.users = users;
+            });
+        }
+    },
+
+    enterDocument: {
+        value: function () {
+            if (this.object.umask) {
+                this.umaskModes = {
+                    user: this.object.umask.user,
+                    group: this.object.umask.group,
+                    others: this.object.umask.others
+                };
+                delete this.object.umask.value;
+            } else {
+                this.umaskModes = {
+                    user: {
+                        read: false,
+                        write: false,
+                        execute: false
+                    },
+                    group: {
+                        read: false,
+                        write: false,
+                        execute: false
+                    },
+                    others: {
+                        read: false,
+                        write: false,
+                        execute: false
+                    }
+                };
+            }
+        }
+    },
+
+    save: {
+        value: function() {
+            this.object.umask = this.umaskModes;
+        }
+    },
+
+    exitDocument: {
+        value: function() {
+            this.umaskModes = null;
+        }
+    }
+});
