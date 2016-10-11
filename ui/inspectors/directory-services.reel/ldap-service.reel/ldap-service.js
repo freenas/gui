de6a1c33ee9529f84f6256881dda3e7f340b7904
Jakub Klama <jakub@ixsystems.com>
2016-10-12 01:10:02 +0200

    Add basic LDAP UI.

---
+++ ui/inspectors/directory-services.reel/ldap-service.reel/ldap-service.js
@@ -0,0 +1,30 @@
+/**
+ * @module ui/ldap-service.reel
+ */
+var Component = require("montage/ui/component").Component,
+    Model = require("core/model/model").Model;
+
+/**
+ * @class LdapService
+ * @extends Component
+ */
+exports.LdapService = Component.specialize(/** @lends LdapService# */ {
+
+    enterDocument: {
+        value: function () {
+            this._populateObjectIfNeeded();
+        }
+    },
+
+    _populateObjectIfNeeded: {
+        value: function () {
+            if (this.object && !this.object.parameters) {
+                var self = this;
+
+                return this.application.dataService.getNewInstanceForType(Model.LdapDirectoryParams).then(function (ldapDirectoryParams) {
+                    return (self.object.parameters = ldapDirectoryParams);
+                });
+            }
+        }
+    }
+});
