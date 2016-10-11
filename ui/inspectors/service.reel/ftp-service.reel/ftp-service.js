6930d3c80535bf4eee8da7ae1b04867a539e0b64
Erin Clark <erin@ixsystems.com>
2016-10-11 14:42:11 -0700

    Initial commit of ftp service ui
    Ticket: #17863

--- ui/inspectors/service.reel/ftp-service.reel/ftp-service.js
+++ ui/inspectors/service.reel/ftp-service.reel/ftp-service.js
@@ -4,4 +4,39 @@ var Component = require("montage/ui/component").Component;
  * @class FtpService
  * @extends Component
  */
-exports.FtpService = Component.specialize();
+exports.FtpService = Component.specialize({
+    _handleOnlyAnonChange: {
+        value: function() {
+            if (!this.object.only_anonymous) {
+                this.object.only_local = true;
+            };
+        }
+    },
+
+    _handleOnlyLocalChange: {
+        value: function() {
+            if (!this.object.only_local) {
+                this.object.only_anonymous = true;
+            };
+        }
+    },
+
+    enterDocument: {
+        value: function() {
+            this.addPathChangeListener("object.only_anonymous", this, "_handleOnlyAnonChange");
+            this.addPathChangeListener("object.only_local", this, "_handleOnlyLocalChange");
+        }
+    },
+
+    exitDocument: {
+        value: function() {
+            if (this.getPathChangeDescriptor("object.only_anonymous", this)) {
+                this.removePathChangeListener("object.only_anonymous", this);   
+            }
+            if (this.getPathChangeDescriptor("object.only_local", this)) {
+                this.removePathChangeListener("object.only_local", this);   
+            }
+
+        }
+    },
+});
