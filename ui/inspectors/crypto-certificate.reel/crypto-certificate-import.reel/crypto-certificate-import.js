e5648fd0bc036a4d043283e1373569fb2437ae89
Xin Tan <mactanxin@gmail.com>
2016-10-12 11:14:27 -0700

    ADDED: crypto-import component

---
+++ ui/inspectors/crypto-certificate.reel/crypto-certificate-import.reel/crypto-certificate-import.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/inspectors/crypto-certificate.reel/crypto-certificate-import.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class CryptoCertificateImport
+ * @extends Component
+ */
+exports.CryptoCertificateImport = Component.specialize(/** @lends CryptoCertificateImport# */ {
+    constructor: {
+        value: function CryptoCertificateImport() {
+            this.super();
+        }
+    }
+});
