0decbd57d9c20e37e1c175045d63ad3ca3384d3e
Xin Tan <mactanxin@gmail.com>
2016-10-12 09:59:05 -0700

    UPDATED: overwrite save function for import

--- ui/inspectors/crypto-certificate.reel/crypto-certificate.js
+++ ui/inspectors/crypto-certificate.reel/crypto-certificate.js
@@ -4,59 +4,13 @@ var Component = require("montage/ui/component").Component,
     CryptoCertificateDigestalgorithm = require("core/model/enumerations/crypto-certificate-digestalgorithm").CryptoCertificateDigestalgorithm;
 
 exports.CryptoCertificate = Component.specialize({
-
-    keyLenghtOptions: {
-        value: [
-            1024,
-            2048,
-            4096
-        ]
-    },
-
-    caIdOptions: {
-        value: null
-    },
-
-    algorithmOptions: {
-        value: null
-    },
-
-    signing_ca_id: {
-        value: null
-    },
-
-    templateDidLoad: {
-        value: function() {
-        }
-    },
-    enterDocument: {
-        value: function(isFirstTime) {
-            if (!!this.object._isNew && !this.object.type) {
-                this.object.type = "LOADER";
-            }
-            this.algorithmOptions = [];
-            this.caIdOptions = [];
-            this.object.certificate = {};
-            for (var i = 0; i < CryptoCertificateDigestalgorithm.members.length; i++) {
-                this.algorithmOptions.push({label: CryptoCertificateDigestalgorithm.members[i], value: CryptoCertificateDigestalgorithm[CryptoCertificateDigestalgorithm.members[i]]});
-            };
-            this.application.cryptoCertificateService.listCertificates().then(function (certificates) {
-                console.log(certificates);
-            });
-            this.caIdOptions.unshift({label:"---", value:null});
-        }
-    },
-
-    handleImportCertificateAction: {
-        value: function() {
-            this.object.creationType = "IMPORT";
-            this.object.certificate = CryptoCertificateType.CERT_EXISTING;
-        }
-    },
-
-    handleCreateCertificateAction: {
+    save: {
         value: function () {
-            this.object.creationType = "CREATE";
+            if (this.object._action === 'import') {
+                this.application.cryptoCertificateService.import(this.object);
+            } else {
+                this.inspector.save();
+            }
         }
     }
 });
