f8486b38d3e58155479f0a726fda94315d158ea9
Erin Clark <erin@ixsystems.com>
2016-10-13 12:31:52 -0700

    Add active_media_type and capabilities to network interface ui (#127)
    Ticket: #18069

--- ui/sections/network/inspectors/network-interface.reel/interface.reel/interface.js
+++ ui/sections/network/inspectors/network-interface.reel/interface.reel/interface.js
@@ -4,4 +4,14 @@ var Component = require("montage/ui/component").Component;
  * @class Interface
  * @extends Component
  */
-exports.Interface = Component.specialize();
+exports.Interface = Component.specialize({
+    active_media_type: {
+        value: null
+    },
+
+    enterDocument: {
+        value: function () {
+            this.active_media_type = this.object.status.active_media_type + " " + this.object.status.active_media_subtype;
+        }
+    }
+});
