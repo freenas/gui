80527afa5304feda827dac0b87416c0cadb9c4dd
Thibault Zanini <thibaultzanini@gmail.com>
2016-10-12 21:30:32 -0700

    Add host field to the docker-image inspector.

--- ui/sections/containers/inspectors/docker-image.reel/docker-image.js
+++ ui/sections/containers/inspectors/docker-image.reel/docker-image.js
@@ -9,11 +9,28 @@ var AbstractInspector = require("ui/abstract/abstract-inspector").AbstractInspec
  */
 exports.DockerImage = AbstractInspector.specialize({
 
+    templateDidLoad: {
+        value: function () {
+            var self = this,
+                blockGateKey = this.constructor.DATA_GATE_BLOCK_KEY;
+
+            this._canDrawGate.setField(blockGateKey, false);
+
+            this._sectionService.listDockerHosts().then(function (dockersHost) {
+                self._dockerHosts = dockersHost;
+                self._canDrawGate.setField(blockGateKey, true);
+            });
+        }
+    },
+
     object: {
         set: function (object) {
             if (this._object !== object) {
                 this._object = object;
                 this.dispatchOwnPropertyChange("createdAtDate", this.createdAtDate);
+                
+                // //frb...
+                // this.dispatchOwnPropertyChange("_dockerHosts", this._dockerHosts);
             }
         },
         get: function () {
@@ -25,6 +42,40 @@ exports.DockerImage = AbstractInspector.specialize({
         get: function () {
             return this.object ? this.object.created_at["$date"] : null;
         }
+    },
+
+
+    handleMultipleSelectAddAction: {
+        value: function (multipleSelect) {
+            // this._sectionService.addImageToHost().then(function () {
+            // });
+        }
+    },
+
+    handleMultipleSelectDeleteAction: {
+        value: function (multipleSelect, multipleSelectValue, hostName) {
+            // this._sectionService.removeImageFromHost().then(function () {
+            // });
+
+            var host;
+
+            for (var i = 0, length = this._dockerHosts.length; i < length; i++) {
+                if (this._dockerHosts[i].name === hostName) {
+                    host = this._dockerHosts[i];
+                    break;
+                }
+            }
+
+            if (host) {
+                this.object.hosts.splice(this.object.hosts.indexOf(host.id), 1);
+            }
+        }
+    }
+
+}, {
+
+    DATA_GATE_BLOCK_KEY: {
+        value: "dataLoaded"
     }
 
 });
