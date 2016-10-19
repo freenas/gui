0239c0826572060decd2775daf6f867b5c78c9ef
Xin Tan <mactanxin@gmail.com>
2016-10-19 16:40:48 -0700

    UPDATED: descriptor components

--- ui/sections/peering/inspectors/peer.reel/peer.js
+++ ui/sections/peering/inspectors/peer.reel/peer.js
@@ -8,41 +8,22 @@ var AbstractInspector = require("ui/abstract/abstract-inspector").AbstractInspec
  * @extends Component
  */
 exports.Peer = AbstractInspector.specialize(/** @lends Peer# */ {
-    credentialsType: {
-        get: function() {
-            return this.object.type;
-        }
-    },
-
-    _inspectorTemplateDidLoad: {
-        value: function() {
-            var self = this;
-            this._peeringService = this.application.peeringService;
-            this._peeringService.getSupportedTypes().then(function (peerTypes) {
-                self.typeOptions = peerTypes;
-            })
-        }
-    },
-
 
     enterDocument: {
-        value: function() {
-            this.super();
-            if (this.object && this.object._isNew) {
-                var self = this;
-                this._peeringService.populateDefaultType(this.object).then(function() {
-                    self.dispatchOwnPropertyChange("credentialsType", self.credentialsType);
-                });
+        value: function () {
+            if (!this.object._action && !this.object._isNew) {
+                this.object._action = 'creation';
             }
         }
     },
 
     save: {
         value: function() {
-            if (this.credentialsComponent && typeof this.credentialsComponent.save === "function") {
-                this.credentialsComponent.save();
+            if (this.object.type === 'freenas') {
+                this.inspector.save({username: this.object.credentials.username, password: this.object.credentials.password});
+            }else {
+                this.inspector.save();
             }
-            this.inspector.save({username: this.object.credentials.username, password: this.object.credentials.password});
         }
     }
 });
