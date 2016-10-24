9600deaa755c20c6a07719442ba535536e0fb05f
Thibault Zanini <thibaultzanini@gmail.com>
2016-10-24 16:45:55 -0700

    Initial commit that use the new Table UI within the container creator inspector.

---
+++ ui/sections/containers/controls/table-volume.reel/table-row-volume.reel/table-row-volume.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/table-row-volume.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class TableRowVolume
+ * @extends Component
+ */
+exports.TableRowVolume = Component.specialize(/** @lends TableRowVolume# */ {
+    constructor: {
+        value: function TableRowVolume() {
+            this.super();
+        }
+    }
+});
---
+++ ui/sections/containers/controls/table-volume.reel/table-volume.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/table-volume.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class TableVolume
+ * @extends Component
+ */
+exports.TableVolume = Component.specialize(/** @lends TableVolume# */ {
+    constructor: {
+        value: function TableVolume() {
+            this.super();
+        }
+    }
+});
