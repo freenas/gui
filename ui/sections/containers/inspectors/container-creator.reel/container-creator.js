4db403bcde4c8a300723a1b7c359f84e70ce3c0f
Thibault Zanini <thibaultzanini@gmail.com>
2016-10-25 16:47:57 -0700

    Brings editable tables to the container creator inspectors.

--- ui/sections/containers/inspectors/container-creator.reel/container-creator.js
+++ ui/sections/containers/inspectors/container-creator.reel/container-creator.js
@@ -81,73 +81,29 @@ exports.ContainerCreator = AbstractInspector.specialize(/** @lends ContainerCrea
                 this.object.memory_limit = memoryLimit || void 0;
             }
 
-            if (environmentComponentValues) {
-                this.object.environment = environmentComponentValues;
+            if (environmentComponentValues && environmentComponentValues.length) {
+                this.object.environment = environmentComponentValues.filter(function (entry) {
+                    return entry.variable && entry.value;
+                }).map(function (entry) {
+                    return entry.variable + "=" + entry.value;
+                });
             }
 
-            if (portsValues.length) {
-                this.object.ports = this._getPortsFromArray(portsValues);
+            if (portsValues && portsValues.length) {
+                this.object.ports = portsValues.filter(function (entry) {
+                    return entry.host_port && entry.container_port;
+                });
             }
 
-            if (volumesValues.length) {
-                this.object.volumes = this._getVolumesFromArray(volumesValues);
+            if (volumesValues && volumesValues.length) {
+                this.object.volumes = volumesValues.filter(function (entry) {
+                    return entry.host_path && entry.container_path;
+                });
             }
 
             return this._sectionService.saveContainer(this.object).then(function () {
                 self._reset();
             });
-        }
-    },
-
-    //@deprecated will be removed with the new Table UI
-    _getPortsFromArray: {
-        value: function (array) {
-            var regEx = new RegExp(/^([0-9]+) -> ([0-9]+) (TCP|UDP)$/),
-                ports = null;
-
-            if (array && array.length) {
-                ports = [];
-
-                for (var i = 0, length = array.length; i < length; i++) {
-                    data = array[0].match(regEx);
-
-                    if (data.length === 4) {
-                        ports.push({
-                            host_port: +data[1],
-                            container_port: +data[2],
-                            protocol: data[3].toUpperCase()
-                        });
-                    }
-                }
-            }               
-
-            return ports;
-        }
-    },
-
-    //@deprecated will be removed with the new Table UI
-    _getVolumesFromArray: {
-        value: function (array) {
-            var regEx = new RegExp(/^([^ ]+) -> ([^ ]+) ?(\(ro\))?$/),
-                ports = null;
-
-            if (array && array.length) {
-                ports = [];
-
-                for (var i = 0, length = array.length; i < length; i++) {
-                    data = array[0].match(regEx);
-
-                    if (data.length >= 3) {
-                        ports.push({
-                            host_path: data[1],
-                            container_path: data[2],
-                            readonly: !!data[3]
-                        });
-                    }
-                }
-            }               
-
-            return ports;
         }
     }
 
