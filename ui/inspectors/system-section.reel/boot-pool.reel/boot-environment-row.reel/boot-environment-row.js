8ead515f07e00de6c7c5ae79c47adcb238618494
Joshua Rule Dobson <joshua@joshuarule.com>
2016-10-31 15:34:47 -0400

    adds boot-environment-table and row component

---
+++ ui/inspectors/system-section.reel/boot-pool.reel/boot-environment-row.reel/boot-environment-row.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/boot-environment-row.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class BootEnvironmentRow
+ * @extends Component
+ */
+exports.BootEnvironmentRow = Component.specialize(/** @lends BootEnvironmentRow# */ {
+    constructor: {
+        value: function BootEnvironmentRow() {
+            this.super();
+        }
+    }
+});
