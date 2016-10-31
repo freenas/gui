468248514cd4588b16f85ee7882333137783dc83
Xin Tan <mactanxin@gmail.com>
2016-10-31 10:30:55 -0700

    ADDED: init service

---
+++ core/service/power-management-service.js
@@ -0,0 +1,31 @@
+var Montage = require("montage").Montage,
+    FreeNASService = require("core/service/freenas-service").FreeNASService,
+    Model = require("core/model/model").Model;
+
+var PowerManagementService = exports.PowerManagementService = Montage.specialize({
+    _instance: {
+        value: null
+    },
+
+    _dataService: {
+        value: null
+    },
+
+    listDrivers: {
+        value: function () {
+            return Model.populateObjectPrototypeForType(Model.ServiceUps).then(function (ServiceUps) {
+                return ServiceUps.constructor.services.drivers();
+            });
+        }
+    }
+}, {
+    instance: {
+        get: function() {
+            if(!this._instance) {
+                this._instance = new PowerManagementService();
+                this._instance._dataService = FreeNASService.instance;
+            }
+            return this._instance;
+        }
+    }
+});
