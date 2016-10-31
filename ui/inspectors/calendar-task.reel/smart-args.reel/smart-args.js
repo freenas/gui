c2406dd520067ae86cb50d11d8564d76ee8373fe
Pierre Chaussalet <pierre@ixsystems.com>
2016-10-31 09:42:40 -0700

    Fix smart test type capitalization
    Ticket: #18614

--- ui/inspectors/calendar-task.reel/smart-args.reel/smart-args.js
+++ ui/inspectors/calendar-task.reel/smart-args.reel/smart-args.js
@@ -9,13 +9,23 @@ var Component = require("montage/ui/component").Component,
  * @extends Component
  */
 exports.SmartArgs = Component.specialize(/** @lends SmartArgs# */ {
+    templateDidLoad: {
+        value: function() {
+            this.testTypes = DiskSelftestType.members.map(function(x) {
+                return {
+                    label: x.toLowerCase().toCapitalized(),
+                    value: x
+                };
+            });
+        }
+    },
+
     enterDocument: {
         value: function() {
             var self = this;
             this.application.storageService.listDisks().then(function(disks) {
                 self.disks = disks;
             });
-            this.testTypes = DiskSelftestType.members;
         }
     }
 });
