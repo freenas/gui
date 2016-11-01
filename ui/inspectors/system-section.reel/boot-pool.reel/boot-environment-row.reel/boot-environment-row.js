0a50a92cf1ff09cca2055ea078181df5b520ef84
Joshua Rule Dobson <joshua@joshuarule.com>
2016-11-01 16:52:18 -0400

    cleans up css commented code and removes constructor js code from environment row

--- ui/inspectors/system-section.reel/boot-pool.reel/boot-environment-row.reel/boot-environment-row.js
+++ ui/inspectors/system-section.reel/boot-pool.reel/boot-environment-row.reel/boot-environment-row.js
@@ -7,10 +7,4 @@ var Component = require("montage/ui/component").Component;
  * @class BootEnvironmentRow
  * @extends Component
  */
-exports.BootEnvironmentRow = Component.specialize(/** @lends BootEnvironmentRow# */ {
-    constructor: {
-        value: function BootEnvironmentRow() {
-            this.super();
-        }
-    }
-});
+exports.BootEnvironmentRow = Component.specialize();
