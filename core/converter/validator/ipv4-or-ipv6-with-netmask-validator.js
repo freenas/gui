8991b04170072b42618c50716d90dfce5b71156f
Erin Clark <erin@ixsystems.com>
2016-11-01 15:38:32 -0700

    Allow network aliases creation to accept and validate ipv4 and ipv6
    Ticket: #18568

---
+++ core/converter/validator/ipv4-or-ipv6-with-netmask-validator.js
@@ -0,0 +1,43 @@
+/**
+ * @requires montage/core/converter/converter
+ */
+var Validator = require("montage/core/converter/converter").Validator;
+var Ipv4WithNetmaskValidator = require("core/converter/validator/ipv4-with-netmask-validator").Ipv4WithNetmaskValidator;
+var Ipv6WithNetmaskValidator = require("core/converter/validator/ipv6-with-netmask-validator").Ipv6WithNetmaskValidator;
+
+
+/**
+  * Verifies that a string is a valid ipv6 address with a netmask separated by a slash
+  * 
+  * @class Ipv4OrIpv6WithNetmaskValidator
+  * @extends Validator
+  */
+exports.Ipv4OrIpv6WithNetmaskValidator = Validator.specialize({
+
+    constructor: {
+        value: function() {
+            this.v4validator = new Ipv4WithNetmaskValidator();
+            this.v6validator = new Ipv6WithNetmaskValidator();
+        }
+    },
+
+    validate: {
+        value: function (value) {
+            if (this.v4validator.validate(value) || this.v6validator.validate(value)) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    },
+
+    v4validator: {
+        value: null
+    },
+
+    v6validator: {
+        value: null
+    }
+
+});
