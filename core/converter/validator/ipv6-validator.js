0592e4591ec364d57d5b71c8d5f83e0f03bc8a44
Erin Clark <erin@ixsystems.com>
2016-11-01 10:44:37 -0700

    Add ipv6 validator
    Ticket: #18567

---
+++ core/converter/validator/ipv6-validator.js
@@ -0,0 +1,21 @@
+/**
+ * @requires montage/core/converter/converter
+ */
+var Validator = require("montage/core/converter/converter").Validator;
+var Montage = require("montage/core/core").Montage;
+
+/**
+  * Verifies that a string is a valid ipv4 address
+  * @class Ipv4Validator
+  * @extends Validator
+  */
+
+var Ipv6Validator = exports.Ipv6Validator = Validator.specialize({
+
+    validate: {
+        value: function (address) {
+            return /^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$/.test(address);
+        }
+    }
+
+});
