170f78666651bb5ad49c386221b89f718a8e2238
Xin Tan <mactanxin@gmail.com>
2016-11-02 14:05:25 -0700

    UPDATED: refactor

--- ui/inspectors/service.reel/ups-service.reel/ups-service.js
+++ ui/inspectors/service.reel/ups-service.reel/ups-service.js
@@ -8,40 +8,35 @@ var Component = require("montage/ui/component").Component,
  * @extends Component
  */
 exports.UpsService = Component.specialize({
-
-    enterDocument: {
-        value: function (isFirstTime) {
-            var self = this,
-                loadingPromises = [];
-            if(isFirstTime) {
-                this.isLoading = true;
-                this.driverOptions = [];
-                this.driverPortOptions = [];
-                this.modeOptions = ServiceUpsMode.members.map(function (x) {
-                    return {
-                        label: x,
-                        value: x
-                    };
-                });
-                this.shutdownModeOptions = ServiceUpsShutdownmode.members.map(function (x) {
-                    return {
-                        label: x,
-                        value: x
-                    };
+    templateDidLoad: {
+        value: function () {
+            var self = this;
+            this.driverOptions = [];
+            this.driverPortOptions = [];
+            this.modeOptions = ServiceUpsMode.members.map(function (x) {
+                return {
+                    label: x,
+                    value: x
+                };
+            });
+            this.shutdownModeOptions = ServiceUpsShutdownmode.members.map(function (x) {
+                return {
+                    label: x,
+                    value: x
+                };
+            });
+            Promise.all([
+                this.application.powerManagementService.listDrivers().then(function (drivers) {
+                    for (var i = 0; i < drivers.length; i++) {
+                        self.driverOptions.push({label: drivers[i][1], value: drivers[i][0]});
+                    }
+                }),
+                this.application.powerManagementService.listUsbDevices().then(function (usbDevices) {
+                    for (var i = 0; i < usbDevices.length; i++) {
+                        self.driverPortOptions.push({label: usbDevices[i][1], value: usbDevices[i][0]});
+                    }
                 })
-                loadingPromises.push(
-                    this.application.powerManagementService.listDrivers().then(function (drivers) {
-                        for (var i = 0; i < drivers.length; i++) {
-                            self.driverOptions.push({label: drivers[i][1], value: drivers[i][0]});
-                        }
-                    }),
-                    this.application.powerManagementService.listUsbDevices().then(function (usbDevices) {
-                        for (var i = 0; i < usbDevices.length; i++) {
-                            self.driverPortOptions.push({label: usbDevices[i][1], value: usbDevices[i][0]});
-                        }
-                    })
-                );
-            }
+            ]);
         }
     }
 });;
