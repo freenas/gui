3d9c18a940c65003d7deab57ac487e0f74184cd2
Xin Tan <mactanxin@gmail.com>
2016-11-03 10:37:21 -0700

    ADDED: init commit for alert reel

---
+++ ui/inspectors/system-section.reel/alert.reel/table-predicates.reel/table-predicates.js
@@ -0,0 +1,10 @@
+/**
+ * @module ui/table-port.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class TablePort
+ * @extends Component
+ */
+exports.TablePort = Component.specialize();
