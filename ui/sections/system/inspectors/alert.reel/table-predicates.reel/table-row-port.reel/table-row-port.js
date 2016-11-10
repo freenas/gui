8e954773c80e870ee186224da58f7ef692a0ca1c
Xin Tan <mactanxin@gmail.com>
2016-11-09 17:11:03 -0800

    UPDATED: replace all enum

--- ui/sections/system/inspectors/alert.reel/table-predicates.reel/table-row-port.reel/table-row-port.js
+++ ui/sections/system/inspectors/alert.reel/table-predicates.reel/table-row-port.reel/table-row-port.js
@@ -1,36 +1,33 @@
 /**
  * @module ui/table-row-port.reel
  */
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+    AlertClassId = require("core/model/enumerations/alert-class-id").AlertClassId,
+    AlertSeverity = require("core/model/enumerations/alert-severity").AlertSeverity;
 
 /**
  * @class TableRowPort
  * @extends Component
  */
 exports.TableRowPort = Component.specialize({
-    classValues: {
-        value:  [
-            "SystemShutdown",
-            "VolumeDiskReplaced",
-            "UpdateDownloaded",
-            "VolumeDegraded",
-            "DockerContainerDied",
-            "SystemReboot",
-            "VolumeUpgradePossible",
-            "UpdateInstalled",
-            "DirectoryServiceBindFailed",
-            "VMwareSnapshotFailed",
-            "DiskControllerFirmwareMismatch",
-            "UserMessage",
-            "UpdateAvailable"
-        ]
-    },
 
-    severityValues: {
-        value: [
-            "CRITICAL",
-            "WARNING",
-            "INFO"
-        ]
+    enterDocument: {
+        value: function (isFirstTime) {
+            var self = this;
+            this.classValues = AlertClassId.members.map(function (x) {
+                console.log(x);
+                return {
+                    label: x,
+                    value: x
+                };
+            });
+            this.severityValues = AlertSeverity.members.map(function (x) {
+                return {
+                    label: x,
+                    value: x
+                };
+            });
+        }
     }
+
 });
