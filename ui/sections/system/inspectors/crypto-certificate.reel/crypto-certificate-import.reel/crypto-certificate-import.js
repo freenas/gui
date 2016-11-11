0289d910f5e1fa2bc7438b745ac4064f90286bcb
Xin Tan <mactanxin@gmail.com>
2016-11-11 13:10:23 -0800

    UPDATED: use field-file-upload

--- ui/sections/system/inspectors/crypto-certificate.reel/crypto-certificate-import.reel/crypto-certificate-import.js
+++ ui/sections/system/inspectors/crypto-certificate.reel/crypto-certificate-import.reel/crypto-certificate-import.js
@@ -8,9 +8,18 @@ var Component = require("montage/ui/component").Component;
  * @extends Component
  */
 exports.CryptoCertificateImport = Component.specialize(/** @lends CryptoCertificateImport# */ {
-    constructor: {
-        value: function CryptoCertificateImport() {
-            this.super();
+    save: {
+        value: function () {
+            return this._saveWithFileUpload(this.object, this.certificateFile, this.privateKeyFile);
+        }
+    },
+
+    _saveWithFileUpload: {
+        value: function (object, certKey, privKey) {
+            object.certificate = { "$binary": certKey };
+            object.privatekey = {"$binary": privKey};
+            console.log(this.certificate);
+            // return this._dataService.saveDataObject.apply(this.object);
         }
     }
 });
