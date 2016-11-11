2f0fa146e01446072d9734c79f807263d9170439
Thibault Zanini <thibaultzanini@gmail.com>
2016-11-10 19:41:22 -0800

    Add support for kerberos keytab file.

--- ui/sections/accounts/inspectors/kerberos-keytab.reel/kerberos-keytab.js
+++ ui/sections/accounts/inspectors/kerberos-keytab.reel/kerberos-keytab.js
@@ -1,7 +1,23 @@
-var Component = require("montage/ui/component").Component;
+var AbstractInspector = require("ui/abstract/abstract-inspector").AbstractInspector,
+    AccountSectionService = require("core/service/section/account-section-service").AccountSectionService;
 
 /**
  * @class KerberosKeytab
  * @extends Component
  */
-exports.KerberosKeytab = Component.specialize();
+exports.KerberosKeytab = AbstractInspector.specialize({
+
+    //TODO: remove when account will have been migrated to the new architecture.
+    _sectionService: {
+        get: function () {
+            return AccountSectionService.instance;
+        }
+    },
+
+    save: {
+        value: function () {
+            return this._sectionService.saveKerberosKeytabWithKeytabStringBase64(this.object, this.keytab);
+        }
+    }
+
+});
