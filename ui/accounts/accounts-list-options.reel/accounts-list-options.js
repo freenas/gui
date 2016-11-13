d54471325a83d16790a205d6a0f2d13c758e30eb
Joshua Rule Dobson <joshua@joshuarule.com>
2016-11-12 16:20:41 -0800

    adds accounts-list-option to toggle domain label

---
+++ ui/accounts/accounts-list-options.reel/accounts-list-options.js
@@ -0,0 +1,10 @@
+/**
+ * @module ui/user-list-options.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class UserListOptions
+ * @extends Component
+ */
+exports.AccountsListOptions = Component.specialize();
