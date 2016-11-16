0d2f9fa708427d755377d1e59b22d04c6013f697
Pierre Chaussalet <pierre@ixsystems.com>
2016-11-16 10:18:36 -0800

    Fix smart calendar task arguments
    Ticket: #18895

--- ui/inspectors/calendar-task.reel/smart-args.reel/smart-args.js
+++ ui/inspectors/calendar-task.reel/smart-args.reel/smart-args.js
@@ -11,6 +11,10 @@ var Component = require("montage/ui/component").Component,
 exports.SmartArgs = Component.specialize(/** @lends SmartArgs# */ {
     templateDidLoad: {
         value: function() {
+            var self = this;
+            this.application.storageService.listDisks().then(function(disks) {
+                self.disks = disks;
+            });
             this.testTypes = DiskSelftestType.members.map(function(x) {
                 return {
                     label: x.toLowerCase().toCapitalized(),
@@ -22,10 +26,10 @@ exports.SmartArgs = Component.specialize(/** @lends SmartArgs# */ {
 
     enterDocument: {
         value: function() {
-            var self = this;
-            this.application.storageService.listDisks().then(function(disks) {
-                self.disks = disks;
-            });
+            if (!this.args || this.args.length != 2) {
+                this.args = [[], null];
+                this.args.length = 2;
+            }
         }
     }
 });
