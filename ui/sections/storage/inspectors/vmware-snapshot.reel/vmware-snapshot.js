c258552c6f2e17395aa1f5a0cb91d41d53fd96d6
Pierre Chaussalet <pierre@ixsystems.com>
2016-11-16 09:12:24 -0800

    Fix missing Model in vmware-snapshot.reel

--- ui/sections/storage/inspectors/vmware-snapshot.reel/vmware-snapshot.js
+++ ui/sections/storage/inspectors/vmware-snapshot.reel/vmware-snapshot.js
@@ -80,7 +80,7 @@ exports.VmwareSnapshot = AbstractInspector.specialize({
             if (this._context) {
                 var currentSelection = this.application.selectionService.getCurrentSelection();
                 for (var i = this._context.columnIndex - 1; i >= 0; i--) {
-                    if (currentSelection[i].constructor.Type == Model.VolumeDataset) {
+                    if (currentSelection[i].constructor.Type == this._sectionService.VMWARE_DATASET_TYPE) {
                         return currentSelection[i];
                     }
                 }
