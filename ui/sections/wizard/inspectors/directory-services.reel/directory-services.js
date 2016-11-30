c98f23caf555d9102fa18cfd334623cd2aa274f3
Thibault Zanini <thibaultzanini@gmail.com>
2016-11-30 18:04:16 -0500

    Make sure the directory services wizard is reset when launching again the wizard.

--- ui/sections/wizard/inspectors/directory-services.reel/directory-services.js
+++ ui/sections/wizard/inspectors/directory-services.reel/directory-services.js
@@ -15,4 +15,35 @@ var DirectoryServices = exports.DirectoryServices = Component.specialize(/** @le
         }
     },
 
+    enterDocument: {
+        value: function () {
+            if (this.object._isNew) {
+                this._clearTables();
+                // this object won't be saved,
+                // only the object in object.__directoryServices.
+                this.object._isNew = false;
+            }
+        }
+    },
+
+    _clearTables: {
+        value: function () {
+            if (this._nisTable.values) {
+                this._nisTable.values.clear();
+            }
+
+            if (this._windbindTable.values) {
+                this._windbindTable.values.clear();
+            }
+
+            if (this._freeipaTable.values) {
+                this._freeipaTable.values.clear();
+            }
+
+            if (this._ldapTable.values) {
+                this._ldapTable.values.clear();
+            }
+        }
+    }
+
 });
