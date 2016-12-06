6051387812bd2961fa2756266b510a9e216b36d3
Xin Tan <mactanxin@gmail.com>
2016-12-05 18:54:38 -0800

    task service

---
+++ core/service/task-service.ts
@@ -0,0 +1,21 @@
+import { TaskRepository } from "core/repository/task-repository";
+export class TaskService {
+    private static instance: TaskService;
+
+    private constructor(private taskRepository: TaskRepository) {}
+
+    public static getInstance() {
+        if (!TaskService.instance) {
+            TaskService.instance = new TaskService(TaskRepository.instance);
+        }
+        return TaskService.instance;
+    }
+
+    public loadEntries(): Promise< Array<Object> > {
+        return this.taskRepository.listTasks();
+    }
+
+    public findTasks(filter: Object): Promise< Array<Object>> {
+        return this.taskRepository.findTasks(filter);
+    }
+}
