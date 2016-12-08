c25661e1a66006c61cfe0c3a3a683dce9ba0c4eb
Suraj Ravichandran <suraj@ixsystems.com>
2016-12-07 18:25:27 -0800

    Hacky way to hook into volume datasets updated event in order to do data obj --> json for d3 conversion
    Thanks Pierre for pointing this out.
    
    Ticket: #8246

--- ui/widgets/volume-space.reel/volume-space.js
+++ ui/widgets/volume-space.reel/volume-space.js
@@ -1,7 +1,25 @@
 var Component = require("montage/ui/component").Component,
-    Model = require("core/model/model").Model;
+    Model = require("core/model/model").Model,
+    EventDispatcherService = require("core/service/event-dispatcher-service").EventDispatcherService;
 
 exports.VolumeSpace = Component.specialize({
+
+    _handleVolumeDatasetUpdate: {
+        value: function() {
+            // this is where one would convert the volume dataset's object
+            // into json which is favored by d3 and friends
+            // PS: I do not like that I have to do this myself (i.e the conversion to json)
+        }
+    }
+    templateDidLoad: {
+        value: function () {
+            this._eventDispatcherService = EventDispatcherService.getInstance();
+            this._eventDispatcherService.addEventListener(
+                "volumeDatasetUpdated",
+                this._handleVolumeDatasetUpdate.bind(this)
+            )
+        }
+    },
 
     enterDocument: {
         value: function () {
@@ -10,6 +28,9 @@ exports.VolumeSpace = Component.specialize({
 
                 this.application.dataService.fetchData(Model.VolumeDataset).then(function(datasets) {
                     self._datasets = datasets;
+                    // call the function below to make the initial json representation of
+                    // the volume datasets obj
+                    self._handleVolumeDatasetUpdate();
                 });
             }
         }
