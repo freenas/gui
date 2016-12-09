ef8cafd45f2f2f3415393fb098653f069aaf348d
Suraj Ravichandran <suraj@ixsystems.com>
2016-12-09 03:16:09 -0800

    Currently this is not needed in radial-chart (will add back if needed)
    Ticket: #8246

--- ui/controls/radial-chart.reel/radial-chart.js
+++ ui/controls/radial-chart.reel/radial-chart.js
@@ -31,33 +31,6 @@ exports.RadialChart = Component.specialize(/** @lends Chart# */ {
 
     data: {
         value: []
-    },
-
-    _getOptionValue: {
-        value: function(optionName) {
-            var optionDefinition = this.OPTIONS[optionName],
-                realValue = this[optionName];
-            if (optionDefinition) {
-                var minimum = optionDefinition.minimum,
-                    maximum = optionDefinition.maximum,
-                    list = optionDefinition.list;
-                if (typeof realValue == 'undefined' || (typeof realValue == 'object' && !realValue)) {
-                    realValue = optionDefinition.defaultValue;
-                } else {
-                    if (typeof minimum != 'undefined') {
-                        realValue = Math.max(realValue, minimum);
-                    }
-                    if (typeof maximum != 'undefined') {
-                        realValue = Math.min(realValue, maximum);
-                    }
-                    if (typeof list != 'undefined') {
-                        if (list.indexOf(realValue) == -1) {
-                            realValue = optionDefinition.defaultValue;
-                        }
-                    }
-                }
-            }
-            return realValue;
-        }
     }
+
 });
