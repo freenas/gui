2ab97660ca1ee338c2e9950bc6bab044b8bc150c
Joshua Rule Dobson <joshua@joshuarule.com>
2016-12-09 15:10:30 -0500

    adds back accientally deleted section

---
+++ ui/services/services.reel/services.js
@@ -0,0 +1,69 @@
+var Component = require("montage/ui/component").Component,
+    Promise = require("montage/core/promise").Promise,
+    Model = require("core/model/model").Model;
+
+/**
+ * @class Services
+ * @extends Component
+ */
+exports.Services = Component.specialize({
+    services: {
+        value: null
+    },
+
+    categories: {
+        value: null
+    },
+
+    enterDocument: {
+        value: function(isFirstTime) {
+            if (isFirstTime) {
+                var self = this;
+                self._listServices().then(function (services) {
+                    return Promise.all([
+                        self._getServicesCategory('Sharing', services, [
+                            'smb',
+                            'nfs',
+                            'afp',
+                            'webdav',
+                            'iscsi'
+                        ]),
+                        self._getServicesCategory('Management', services, [
+                            'sshd',
+                            'smartd',
+                            'dyndns',
+                            'snmp',
+                            'lldp',
+                            'ups',
+                            'dc'
+                        ]),
+                        self._getServicesCategory('File Transfer', services, [
+                            'ftp',
+                            'rsyncd',
+                            'tftpd'
+                        ])
+                    ]);
+                }).then(function(categories) {
+                    self.categories = categories;
+                });
+            }
+        }
+    },
+
+    _listServices: {
+        value: function() {
+            return this.application.dataService.fetchData(Model.Service);
+        }
+    },
+
+    _getServicesCategory: {
+        value: function(name, services, typesInCategory) {
+            return this.application.dataService.getNewInstanceForType(Model.ServicesCategory).then(function(category) {
+                category.name = name;
+                category.services = services;
+                category.types = typesInCategory.map(function(x) { return 'service-' + x; });
+                return category;
+            });
+        }
+    }
+});
