66ab83b35d64e304f860b45c8f473a701a7f3886
Joshua Rule Dobson <joshua@joshuarule.com>
2016-12-09 13:09:21 -0500

    removes unused documentation folder and bug report component

--- ui/documentation/bug-report.reel/bug-report.js
+++
@@ -1,7 +0,0 @@
-var Component = require("montage/ui/component").Component;
-
-/**
- * @class BugReport
- * @extends Component
- */
-exports.BugReport = Component.specialize();
--- ui/documentation/documentation.reel/documentation.js
+++
@@ -1,7 +0,0 @@
-var Component = require("montage/ui/component").Component;
-
-/**
- * @class Documentation
- * @extends Component
- */
-exports.Documentation = Component.specialize();
