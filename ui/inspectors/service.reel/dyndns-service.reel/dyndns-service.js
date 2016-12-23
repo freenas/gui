62f8c76a72399ef69e48691d527fe73280af1bd4
Erin Clark <erin@ixsystems.com>
2016-12-23 15:04:41 -0800

    Make dyndns work with the new world order and allow saving without password changes
    Ticket: #19730

--- ui/inspectors/service.reel/dyndns-service.reel/dyndns-service.js
+++ ui/inspectors/service.reel/dyndns-service.reel/dyndns-service.js
@@ -1,29 +1,31 @@
-var Component = require("montage/ui/component").Component,
-    ServiceDyndnsService = require("core/service/service-dyndns-service.js").ServiceDyndnsService;
+var AbstractInspector = require("ui/abstract/abstract-inspector").AbstractInspector;
 
 /**
  * @class DynamicDnsService
  * @extends Component
  */
-exports.DynamicDnsService = Component.specialize({
+exports.DynamicDnsService = AbstractInspector.specialize({
     providerOptions: {
-        value: null
-    },
-
-    _serviceDyndnsService: {
         value: null
     },
 
     templateDidLoad: {
         value: function() {
             var self = this;
-            this._serviceDyndnsService = ServiceDyndnsService.instance;
-            this._serviceDyndnsService.getProviders().then(function (dyndnsProviders) {
+            this._sectionService.getDyndnsProviders().then(function (dyndnsProviders) {
                 self.providerOptions = Object.keys(dyndnsProviders).map(function(x) {
                     return {label: x, value: dyndnsProviders[x]};
                 });
                 self.providerOptions.unshift({label:"-",value:null});
             });
+        }
+    },
+
+    save: {
+        value: function() {
+            if (this.object.password === null) {
+                delete this.object.password;
+            }
         }
     }
 });
