762a377b4faf88683aba771de6cd88403ac43ec1
Xin Tan <mactanxin@gmail.com>
2017-01-05 16:25:38 -0500

    ADDED: certificate list watcher

--- ui/sections/system/inspectors/certificates.reel/certificates.js
+++ ui/sections/system/inspectors/certificates.reel/certificates.js
@@ -1,7 +1,51 @@
-var AbstractInspector = require("ui/abstract/abstract-inspector").AbstractInspector;
+var AbstractInspector = require("ui/abstract/abstract-inspector").AbstractInspector,
+    EventDispatcherService = require("core/service/event-dispatcher-service").EventDispatcherService,
+    ModelEventName = require("core/model-event-name").ModelEventName;
 
 
 exports.Certificates = AbstractInspector.specialize({
+
+    eventDispatcherService: {
+        get: function() {
+            if (!this._eventDispatcherService) {
+                this._eventDispatcherService = EventDispatcherService.getInstance();
+            }
+            return this._eventDispatcherService;
+        }
+    },
+
+    _object: {
+        value: null
+    },
+
+    object: {
+        get: function() {
+            return this._object;
+        },
+        set: function(object) {
+            this._object = object;
+            this.availableCertsEventListener = this.eventDispatcherService.addEventListener(Model.CryptoCertificate.listChange, this._handleChange.bind(this));
+        }
+    },
+
+    _unregisterUpdates: {
+        value: function () {
+            this.eventDispatcherService.removeEventListener(Model.CryptoCertificate.listChange, this.availableCertsEventListener);
+        }
+    },
+
+    exitDocument: {
+        value: function() {
+            this._unregisterUpdates();
+        }
+    },
+
+    _handleChange: {
+        value: function(state) {
+            _.assign(this.object, state.toJS());
+            this._unregisterUpdates();
+        }
+    },
 
     _inspectorTemplateDidLoad: {
         value: function (){
