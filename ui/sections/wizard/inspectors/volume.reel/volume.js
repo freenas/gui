a4076849b673c83b228a46c6dedca0ba1b5b2b04
Thibault Zanini <thibaultzanini@gmail.com>
2017-01-06 11:53:24 +0100

    Update the wizard section to the new architecture.

--- ui/sections/wizard/inspectors/volume.reel/volume.js
+++ ui/sections/wizard/inspectors/volume.reel/volume.js
@@ -1,8 +1,7 @@
 /**
  * @module ui/volume.reel
  */
-var Component = require("montage/ui/component").Component,
-    StorageSectionService = require("core/service/section/storage-section-service").StorageSectionService;
+var Component = require("montage/ui/component").Component;
 
 /**
  * @class Volume
@@ -10,27 +9,54 @@ var Component = require("montage/ui/component").Component,
  */
 exports.Volume = Component.specialize(/** @lends Volume# */ {
 
+    profile: {
+        value: null
+    },
+
+    profiles: {
+        value: null
+    },
+
     templateDidLoad: {
         value: function () {
-            this.volumeCreator.__sectionService = this.context.sectionService;
+            this._sectionService = this.context.sectionService;
+            this.profiles = this._sectionService.getProfiles();
         }
     },
 
     enterDocument:{
         value: function (isFirstTime) {
             if (isFirstTime) {
-                this.defineBinding("volumeCreator.context", {
-                    "<-": "context"
-                });
-
-                this.defineBinding("volumeCreator.object", {
-                    "<-": "object"
-                });
+                this.addPathChangeListener("profile", this, "handleProfileChange");
             }
 
             //Thanks to the binding.
             if (this.object.topology && this.object.topology.data) {
                 this.context.isNextStepDisabled = !this.object.id || this.object.topology.data.length === 0;
+            }
+        }
+    },
+
+    handleProfileChange: {
+        value: function () {
+            if (this.profile) {
+                var topologyProfile = this.profiles.get(this.profile);
+
+                if (topologyProfile) {
+                    var self = this;
+
+                    this._sectionService.listAvailableDisks().then(function (disks) {
+                        if (disks && disks.length) {
+                            return self._sectionService.generateTopology(disks, topologyProfile);
+                        }
+                    }).then(function (topology) {
+                        self.object.topology = topology;
+                    });
+                } else {
+                    this.object.topology = null;
+                }
+            } else {
+                this.object.topology = null;
             }
         }
     }
