19a14f20095908fdb126f6422fede30dc33366c2
Pierre Chaussalet <pchaussalet@gmail.com>
2017-01-06 12:23:18 -0800

    Fix Alert filters viewer
    Ticket: #19915

--- ui/sections/system/inspectors/alert.reel/alert.js
+++ ui/sections/system/inspectors/alert.reel/alert.js
@@ -1,28 +1,27 @@
-var Component = require("montage/ui/component").Component,
+var AbstractInspector = require("ui/abstract/abstract-inspector").AbstractInspector,
     AlertService = require("core/service/alert-service").AlertService,
     EventDispatcherService = require("core/service/event-dispatcher-service").EventDispatcherService,
-    _ = require("lodash");
+    ModelEventName = require("core/model-event-name").ModelEventName,
+    DataObjectChangeService = require("core/service/data-object-change-service").DataObjectChangeService,
+_ = require("lodash");
 
-exports.Alert = Component.specialize( {
-    templateDidLoad: {
+exports.Alert = AbstractInspector.specialize( {
+    _inspectorTemplateDidLoad: {
         value: function () {
             var self = this;
             this._service = AlertService.instance;
-            this._service.loadEntries().then(function (entries) {
+            this._dataObjectChangeService = new DataObjectChangeService();
+            this._eventDispatcherService = EventDispatcherService.getInstance();
+            this._eventDispatcherService.addEventListener(ModelEventName.AlertFilter.listChange, this._handleAlertFilterUpdate.bind(this));
+            return this._service.loadEntries().then(function (entries) {
                 self.entries = entries;
             });
-            this._eventDispatcherService = EventDispatcherService.getInstance();
-            this._eventDispatcherService.addEventListener("alertFilterUpdated", this._handleAlertFilterUpdate.bind(this));
         }
     },
 
     _handleAlertFilterUpdate: {
-        value: function() {
-            var self = this;
-            this._service.loadEntries().then(function (entries) {
-                self.entries = _.clone(entries);
-                self.entries._objectType = 'AlertFilter';
-            });
+        value: function(state) {
+            this._dataObjectChangeService.handleDataChange(this.entries, state);
         }
     }
 });
