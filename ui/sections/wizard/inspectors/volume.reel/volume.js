0a1689a94b8f438427f13de5afea2f66cc85cb51
Thibault Zanini <thibaultzanini@gmail.com>
2017-01-08 18:27:08 +0100

    Clean up old code.

--- ui/sections/wizard/inspectors/volume.reel/volume.js
+++ ui/sections/wizard/inspectors/volume.reel/volume.js
@@ -9,18 +9,9 @@ var Component = require("montage/ui/component").Component;
  */
 exports.Volume = Component.specialize(/** @lends Volume# */ {
 
-    profile: {
-        value: null
-    },
-
-    profiles: {
-        value: null
-    },
-
     templateDidLoad: {
         value: function () {
             this._sectionService = this.context.sectionService;
-            this.profiles = this._sectionService.getProfiles();
         }
     },
 
@@ -28,8 +19,6 @@ exports.Volume = Component.specialize(/** @lends Volume# */ {
         value: function (isFirstTime) {
             if (isFirstTime) {
                 self = this;
-
-                this.addPathChangeListener("profile", this, "handleProfileChange");
 
                 this._sectionService.listAvailableDisks().then(function (disks) {
                     self.availableDisks = disks;
@@ -39,30 +28,6 @@ exports.Volume = Component.specialize(/** @lends Volume# */ {
             //Thanks to the binding.
             if (this.object.topology && this.object.topology.data) {
                 this.context.isNextStepDisabled = !this.object.id || this.object.topology.data.length === 0;
-            }
-        }
-    },
-
-    handleProfileChange: {
-        value: function () {
-            if (this.profile) {
-                var topologyProfile = this.profiles.get(this.profile);
-
-                if (topologyProfile) {
-                    var self = this;
-
-                    this._sectionService.listAvailableDisks().then(function (disks) {
-                        if (disks && disks.length) {
-                            return self._sectionService.generateTopology(disks, topologyProfile);
-                        }
-                    }).then(function (topology) {
-                        self.object.topology = topology;
-                    });
-                } else {
-                    this.object.topology = null;
-                }
-            } else {
-                this.object.topology = null;
             }
         }
     }
