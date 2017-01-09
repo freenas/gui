6175db1522c03f13545d8667de2f1fbb401daf2d
Hans Van Mil <hans.vanmil72@gmail.com>
2017-01-09 17:49:09 +0100

    new inspectors for vm datastore

---
+++ ui/sections/vms/inspectors/virtual-machine-datastore.reel/virtual-machine-datastore-volume.reel/virtual-machine-datastore-volume.js
@@ -0,0 +1,10 @@
+/**
+ * @module ui/virtual-machine-datastore-volume.reel
+ */
+var AbstractInspector = require("ui/abstract/abstract-inspector").AbstractInspector;
+
+/**
+ * @class VirtualMachineDatastoreVolume
+ * @extends AbstractInspector
+ */
+exports.VirtualMachineDatastoreVolume = AbstractInspector.specialize({});
---
+++ ui/sections/vms/inspectors/virtual-machine-datastore.reel/virtual-machine-datastore.js
@@ -0,0 +1,10 @@
+/**
+ * @module ui/virtual-machine-datastore.reel
+ */
+var AbstractInspector = require("ui/abstract/abstract-inspector").AbstractInspector;
+
+/**
+ * @class VirtualMachineDatastore
+ * @extends AbstractInspector
+ */
+exports.VirtualMachineDatastore = AbstractInspector.specialize({});
