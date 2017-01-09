f99f2e8b633850c921324068ec8cb463a8d60082
Hans Van Mil <hans.vanmil72@gmail.com>
2017-01-09 17:48:54 +0100

    new vm datastore functions in section service & repository

---
+++ core/dao/vm-datastore-dao.js
@@ -0,0 +1,16 @@
+"use strict";
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var abstract_dao_1 = require("./abstract-dao");
+var model_1 = require("../model");
+var VmDatastoreDao = (function (_super) {
+    __extends(VmDatastoreDao, _super);
+    function VmDatastoreDao() {
+        return _super.call(this, model_1.Model.VmDatastore) || this;
+    }
+    return VmDatastoreDao;
+}(abstract_dao_1.AbstractDao));
+exports.VmDatastoreDao = VmDatastoreDao;
