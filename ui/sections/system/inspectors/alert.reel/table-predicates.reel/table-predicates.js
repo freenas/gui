48d8e2b1754974e9eaf0980b08a5360124f127ee
Xin Tan <mactanxin@gmail.com>
2017-01-11 10:02:18 -0800

    make predicate uneditable after create same type

--- ui/sections/system/inspectors/alert.reel/table-predicates.reel/table-predicates.js
+++ ui/sections/system/inspectors/alert.reel/table-predicates.reel/table-predicates.js
@@ -3,10 +3,30 @@ var Component = require("montage/ui/component").Component;
 exports.TablePredicates = Component.specialize({
     tableWillUseNewEntry: {
         value: function () {
-            return {
-                property: "class",
-                operator: null,
-                value: null
+            if(this.rows && this.rows.length == 0) {
+                return {
+                    property: "class",
+                    operator: null,
+                    value: null,
+                    _disabled: false
+                }
+            } else if(this.rows && this.rows.length == 1){
+                switch (this.rows[0].property) {
+                    case "class": 
+                        return {
+                            property: "severity",
+                            operator: null,
+                            value: null,
+                            _disabled: true
+                        }
+                    case "severity":
+                        return {
+                            property: "class",
+                            operator: null,
+                            value: null,
+                            _disabled: true
+                        }
+                }
             }
         }
     }
