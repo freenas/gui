b08444a5dc905f8f55fb17c6b118018944a9de63
Pierre Chaussalet <pchaussalet@gmail.com>
2017-01-10 16:20:00 -0800

    Fix save method on inspector not returning a Promise

--- ui/sections/containers/inspectors/docker-collection.reel/docker-collection.js
+++ ui/sections/containers/inspectors/docker-collection.reel/docker-collection.js
@@ -1,10 +1,3 @@
-/**
- * @module ui/docker-collection.reel
- */
 var AbstractInspector = require("ui/abstract/abstract-inspector").AbstractInspector;
 
-/**
- * @class DockerCollection
- * @extends Component
- */
 exports.DockerCollection = AbstractInspector.specialize();
