02c0aa1407f64e5f1454f08b3509a3963602bd84
Pierre Chaussalet <pchaussalet@gmail.com>
2017-01-10 18:00:54 -0800

    Remove generated files

--- core/repository/data-store.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var redux_1 = require("redux");
-var DataStore = (function () {
-    function DataStore() {
-        this.store = redux_1.createStore(function () { });
-    }
-    return DataStore;
-}());
-exports.DataStore = DataStore;
