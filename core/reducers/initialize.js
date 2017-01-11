ba4ea7cf9ef141499306415b10dc82672fbcb585
Pierre Chaussalet <pchaussalet@gmail.com>
2017-01-10 18:00:54 -0800

    Remove generated files

--- core/reducers/initialize.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Immutable = require("immutable");
-function initialize(state, action) {
-    if (action.type === '@@INIT') {
-        return Immutable.Map();
-    }
-}
-exports.initialize = initialize;
