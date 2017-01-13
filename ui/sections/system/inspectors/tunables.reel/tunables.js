59d31d0befa512041af23f14393f9402aa809944
Pierre Chaussalet <pchaussalet@gmail.com>
2017-01-12 17:58:36 -0800

    Fix tunables list refresh
    Ticket: #20175

--- ui/sections/system/inspectors/tunables.reel/tunables.js
+++ ui/sections/system/inspectors/tunables.reel/tunables.js
@@ -1,4 +1,7 @@
-var AbstractInspector = require("ui/abstract/abstract-inspector").AbstractInspector;
+var AbstractInspector = require("ui/abstract/abstract-inspector").AbstractInspector,
+    EventDispatcherService = require("core/service/event-dispatcher-service").EventDispatcherService,
+    ModelEventName = require("core/model-event-name").ModelEventName,
+    DataObjectChangeService = require("core/service/data-object-change-service").DataObjectChangeService;
 
 
 exports.Tunables = AbstractInspector.specialize({
@@ -6,9 +9,30 @@ exports.Tunables = AbstractInspector.specialize({
     _inspectorTemplateDidLoad: {
         value: function (){
             var self = this;
+            this._eventDispatcherService = EventDispatcherService.getInstance();
+            this._dataObjectChangeService = new DataObjectChangeService();
             return this._sectionService.listTunables().then(function (tunables) {
                 self.tunables = tunables;
             });
+        }
+    },
+
+    enterDocument: {
+        value: function (isFirstTime) {
+            this.super(isFirstTime);
+            this._changeListener = this._eventDispatcherService.addEventListener(ModelEventName.Tunable.listChange, this._handleListChange.bind(this));
+        }
+    },
+
+    exitDocument: {
+        value: function () {
+            this._eventDispatcherService.removeEventListener(ModelEventName.Tunable.listChange, this._changeListener);
+        }
+    },
+
+    _handleListChange: {
+        value: function (state) {
+            this._dataObjectChangeService.handleDataChange(this.tunables, state);
         }
     }
 });
