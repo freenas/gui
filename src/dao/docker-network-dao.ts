1ce1add12c9dc591f5795005dc641415c58e97ec
Thibault Zanini <thibaultzanini@gmail.com>
2017-01-14 16:02:37 -0500

    Workaround an issue with bindings.

--- src/dao/docker-network-dao.ts
+++ src/dao/docker-network-dao.ts
@@ -7,4 +7,18 @@ export class DockerNetworkDao extends AbstractDao {
         });
     }
 
+     public getNewInstance(): Promise<any> {
+        let self = this,
+            object = new Object({
+            _isNew: true,
+            _objectType: Model.DockerNetwork
+            });
+
+        //FIXME: needed because there is a bug in frb or collection
+        //that doesn't changes in the select-multiple when this property is undefined.
+        (object as any).containers = [];
+
+        return Promise.resolve(object);
+    }
+
 }
