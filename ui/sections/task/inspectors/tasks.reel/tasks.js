5b3c75134e2ceb3140823e8aa22cf34397fb266e
Xin Tan <mactanxin@gmail.com>
2017-01-18 14:11:47 -0800

    UPDATED: new task ui with preset tabs and details

--- ui/sections/task/inspectors/tasks.reel/tasks.js
+++ ui/sections/task/inspectors/tasks.reel/tasks.js
@@ -2,62 +2,44 @@
  * @module ui/sections/task/inspectors/tasks.reel
  */
 var AbstractInspector = require("ui/abstract/abstract-inspector").AbstractInspector,
-    TaskService = require("core/service/task-service").TaskService;
+    TaskService = require("core/service/task-service").TaskService,
+    EventDispatcherService = require("core/service/event-dispatcher-service").EventDispatcherService,
+    ModelEventName = require("core/model-event-name").ModelEventName,
+    DataObjectChangeService = require("core/service/data-object-change-service").DataObjectChangeService,
+_ = require("lodash");
 
 /**
  * @class Tasks
  * @extends AbstractInspector
  */
 exports.Tasks = AbstractInspector.specialize(/** @lends Tasks# */ {
-    stateOptions: {
-        value: [
-            "CREATED",
-            "WAITING",
-            "EXECUTING",
-            "ROLLBACK",
-            "FINISHED",
-            "FAILED",
-            "ABORTED"
-        ]
-    },
 
     templateDidLoad: {
         value: function () {
             var self = this;
                 this.filter = {};
             this._service = TaskService.getInstance();
+            this._dataObjectChangeService = new DataObjectChangeService();
+            this._eventDispatcherService = EventDispatcherService.getInstance();
+            this._eventDispatcherService.addEventListener(ModelEventName.Task.contentChange, this._handleTaskListUpdate.bind(this));
+            this.entries = [];
+        }
+    },
+
+    enterDocument: {
+        value: function (isFirsttime){
+            var self = this;
+            this.currentView = "EXECUTING";
             this._service.loadEntries().then(function (entries) {
-                self.tasks = entries;
+                console.log(entries);
+                self.entries = entries.sort(function(a,b) {return (b.id > a.id) ? 1 : ((a.id > b.id) ? - 1 : 0);});
             });
         }
     },
 
-    handleClearAction: {
-        value: function () {
-            this.filter = {};
-        }
-    },
-
-    handleApplyAction: {
-        value:function() {
-            var self = this,
-                filter = {};
-            if (this.filter.id) {
-                filter.id = this.filter.id;
-            }
-            if (this.filter.name) {
-                filter.name = this.filter.name;
-            }
-            if (this.filter.state) {
-                filter.state = this.filter.state;
-            }
-            // FIXME: add the right stuff here
-            if (this.filter.started_after) {
-                filter.started_at = this.filter.started_after;
-            }
-            this._service.findTasks(filter).then(function (entries) {
-                self.tasks = entries;
-            })
+    _handleTaskListUpdate: {
+        value: function(state) {
+            this._dataObjectChangeService.handleDataChange(this.entries, state);
         }
     }
 });
