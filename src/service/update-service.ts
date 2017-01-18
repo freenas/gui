32f4be711404d8572e6e975af97b777bd519e696
Hans Van Mil <hans.vanmil72@gmail.com>
2017-01-18 13:51:01 +0100

    fix small issues in System/Update UI

--- src/service/update-service.ts
+++ src/service/update-service.ts
@@ -15,7 +15,7 @@ export class UpdateService {
     }
 
     public getConfig() {
-        return this.updateRepository.getConfig()
+        return this.updateRepository.getConfig();
     }
 
     public saveConfig(config: any) {
@@ -35,14 +35,14 @@ export class UpdateService {
     }
 
     public checkAndDownload() {
-        return this.updateRepository.checkAndDownload()
+        return this.updateRepository.checkAndDownload();
     }
 
     public updateNow(reboot: boolean) {
-        return this.updateRepository.updateNow(reboot)
+        return this.updateRepository.updateNow(reboot);
     }
 
     public apply(reboot: boolean) {
-        return this.updateRepository.apply(reboot)
+        return this.updateRepository.apply(reboot);
     }
 }
