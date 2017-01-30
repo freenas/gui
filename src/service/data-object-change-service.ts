1e6fee919fc7152376a606838ac625cf481bfb92
Pierre Chaussalet <pchaussalet@gmail.com>
2017-01-30 09:40:49 -0800

    Fix list refresh
    Ticket: #20658

--- src/service/data-object-change-service.ts
+++ src/service/data-object-change-service.ts
@@ -7,6 +7,7 @@ export class DataObjectChangeService {
             options = objectType;
             objectType = null;
         }
+        options = options || {};
 
         let newObjects = _.sortBy(
             _.filter(
