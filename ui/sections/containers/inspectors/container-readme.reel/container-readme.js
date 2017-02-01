99eff978712338d94ecade808944e1ad8b5b4652
Xin Tan <mactanxin@gmail.com>
2017-02-01 13:05:28 -0800

    ADDED: markdown support

--- ui/sections/containers/inspectors/container-readme.reel/container-readme.js
+++ ui/sections/containers/inspectors/container-readme.reel/container-readme.js
@@ -1,10 +1,17 @@
 /**
  * @module ui/container-readme.reel
  */
-var Component = require("montage/ui/component").Component;
+var Component = require("montage/ui/component").Component,
+	marked = require('marked');
 
 /**
  * @class ContainerReadme
  * @extends Component
  */
-exports.ContainerReadme = Component.specialize();
+exports.ContainerReadme = Component.specialize({
+    enterDocument: {
+        value: function() {
+            this.parsedHtml = marked(this.object.text);
+        }
+    }	
+});
