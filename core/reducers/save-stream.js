cec5ae24f7206ceadd76238607c4ca2761955f7c
Pierre Chaussalet <pchaussalet@gmail.com>
2017-02-06 16:44:38 -0800

    Fix VmDatastore listing
    Ticket: #20890

--- core/reducers/save-stream.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var immutable = require("immutable");
-function saveStream(previousState, action) {
-    var type = action.meta.type, streams = previousState.get("streams"), stream = immutable.fromJS(action.payload);
-    return previousState.set("streams", streams.set(type, stream));
-}
-exports.saveStream = saveStream;
--- src/model/VmDatastore.ts
+++ src/model/VmDatastore.ts
@@ -1,4 +1,5 @@
 import {AbstractDataObject} from './AbstractDataObject';
 
 export class VmDatastore extends AbstractDataObject {
+    public type: string;
 }
