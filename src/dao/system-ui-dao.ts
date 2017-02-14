0abb6952eee64100b3c8a38881981df3d5f34e14
Xin Tan <mactanxin@gmail.com>
2017-02-14 15:23:08 -0800

    DELETED: redundant save function

--- src/dao/system-ui-dao.ts
+++ src/dao/system-ui-dao.ts
@@ -1,9 +1,5 @@
 import {AbstractDao} from './abstract-dao';
 import {Model} from '../model';
-import { processor as cleaningProcessor } from '../service/data-processor/cleaner';
-import { processor as diffProcessor } from '../service/data-processor/diff';
-import { processor as nullProcessor } from '../service/data-processor/null';
-import { processor as taskProcessor } from '../service/data-processor/methodCleaner';
 import {SystemUi} from '../model/SystemUi';
 
 export class SystemUiDao extends AbstractDao<SystemUi> {
@@ -12,27 +8,4 @@ export class SystemUiDao extends AbstractDao<SystemUi> {
             queryMethod: 'system.ui.get_config'
         });
     }
-
-    public save(object: any, args?: Array<any>) {
-        return Promise.all([
-            this.loadPropertyDescriptors(),
-            this.loadTaskDescriptor(this.updateMethod)
-        ]).spread((propertyDescriptors, methodDescriptor) => {
-            let update = taskProcessor.process(
-                nullProcessor.process(
-                    diffProcessor.process(
-                        cleaningProcessor.process(
-                            object,
-                            propertyDescriptors
-                        ),
-                        Model.SystemUi,
-                        object.id
-                    )
-                ), methodDescriptor);
-            if (update || (args && args.length > 0)) {
-                return this.middlewareClient.submitTask('system.ui.update', [update]);
-            }
-        });
-    }
-
 }
