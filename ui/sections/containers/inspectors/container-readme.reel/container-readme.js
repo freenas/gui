f05b0eb999b16c4c26c64cbe848f9905e69a94f1
Thibault Zanini <thibaultzanini@gmail.com>
2017-02-15 18:30:14 -0500

    Changing docker image update readme section if opened. Ticket: #20979

--- ui/sections/containers/inspectors/container-readme.reel/container-readme.js
+++ ui/sections/containers/inspectors/container-readme.reel/container-readme.js
@@ -1,21 +1,44 @@
 /**
  * @module ui/container-readme.reel
  */
-var Component = require("montage/ui/component").Component,
+var AbstractInspector = require("ui/abstract/abstract-inspector").AbstractInspector;
 	marked = require('marked');
 
 /**
  * @class ContainerReadme
  * @extends Component
  */
-exports.ContainerReadme = Component.specialize({
+exports.ContainerReadme = AbstractInspector.specialize({
     enterDocument: {
-        value: function() {
+        value: function (firstTime) {
+            if (firstTime) {
+                this.addPathChangeListener("context.parentContext.object.image", this, "handleImageChange");
+            }
+
             if (this.object && this.object.text) {
                 this.parsedHtml = marked(this.object.text);
             } else {
                 this.parsedHtml = marked('# Please select a template');
             }
         }
-    }	
+    },
+
+    handleImageChange: {
+        value: function () {
+            if (this._inDocument && this.context && this.context.parentContext && this.object &&
+                this.context.parentContext.object && this.context.parentContext.object.image &&
+                this.context.parentContext.object.image !== this.object.imageName) {
+                    var self = this,
+                        imageName = this.context.parentContext.object.image;
+
+                    this._sectionService.getReadmeforDockerImage(this.context.parentContext.object.image).then(function (object) {
+                        if (imageName === self.context.parentContext.object.image) {
+                            self.object.text = object;
+                            self.object.imageName = object;
+                            self.parsedHtml = marked(self.object.text);
+                        }
+                    });
+            }
+        }
+    }
 });
