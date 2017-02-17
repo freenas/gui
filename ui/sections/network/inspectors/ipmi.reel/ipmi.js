80f99008bc2ebf6c30175ef5315af80a9e4ba0e9
Erin Clark <erin@ixsystems.com>
2017-02-16 18:37:13 -0800

    Reset the IPMI password field after a save
    Ticket: #18780

--- ui/sections/network/inspectors/ipmi.reel/ipmi.js
+++ ui/sections/network/inspectors/ipmi.reel/ipmi.js
@@ -11,10 +11,16 @@ exports.Ipmi = AbstractInspector.specialize(/** @lends Ipmi# */ {
 
     save: {
         value: function() {
+            var self = this;
             if (this.object && !this.object.dhcp && !!this.object.address) {
                 this.object.netmask = this.object.netmask || this._sectionService.IPV4_DEFAULT_NETMASK;
             }
-            return this.inspector.save();
+            return this.inspector.save().then(function(taskSubmission) {
+                return taskSubmission.taskPromise;}).then(function() {
+                    self.object.password = null;
+                    self.passwordComponent.reset();
+                    self.passwordComponent.editEnabled = false;
+                });
         }
     }
 });
