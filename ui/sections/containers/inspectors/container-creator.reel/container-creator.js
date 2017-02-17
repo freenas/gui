8313024b09941ce2512d1810a414be1ab9848245
Thibault Zanini <thibaultzanini@gmail.com>
2017-02-17 18:08:30 -0500

    Migrate code from docker-container component to service.

--- ui/sections/containers/inspectors/container-creator.reel/container-creator.js
+++ ui/sections/containers/inspectors/container-creator.reel/container-creator.js
@@ -113,91 +113,14 @@ exports.ContainerCreator = AbstractInspector.specialize({
 
     save: {
         value: function () {
-            var environmentComponentValues = this._environmentComponent.values,
-                commandString = this._commandComponent.value,
-                portsValues = this._portsComponent.values,
-                volumesValues = this._volumesComponent.values,
-                settingsValues = this._settingsComponent.values,
-                environments = [],
-                self = this;
-
-            if (commandString) {
-                this.object.command = commandString.split(" ");
-            }
-
-            if (this.object.memory_limit) {
-                var memoryLimit = this.application.bytesService.convertStringToSize(this.object.memory_limit, this.application.bytesService.UNITS.M);
-                this.object.memory_limit = memoryLimit || void 0;
-            }
-
-            if (settingsValues && settingsValues.length) {
-                try {
-                    environments = this._getVariablesFromArray(settingsValues);
-                } catch (e) {
-                    //TODO
-                }
-            }
-
-            if (environmentComponentValues && environmentComponentValues.length) {
-                environments = environments.concat(this._getVariablesFromArray(environmentComponentValues));
-            }
-
-            if (environments.length) {
-                this.object.environment = environments;
-            }
-
-            if (portsValues && portsValues.length) {
-                this.object.ports = portsValues.filter(function (entry) {
-                    return entry.host_port && entry.container_port;
-                });
-            }
-
-            if (volumesValues && volumesValues.length) {
-                this.object.volumes = this._extractValidVolumes(volumesValues);
-            }
-
-            if (this.object.primary_network_mode !== this.constructor.PRIMARY_NETWORK_MODE_BRIDGED) {
-                this.object.bridge.address = null;
-                this.object.bridge.macaddress = null;
-                this.object.bridge.dhcp = false;
-            }
-
-            return this._sectionService.saveContainer(this.object).then(function () {
+            return this._sectionService.saveContainer(this.object, {
+                command: this._commandComponent.value,
+                environments: this._environmentComponent.values,
+                settings: this._settingsComponent.values,
+                ports: this._portsComponent.values,
+                volumes: this._volumesComponent.values,
+            }).then(function () {
                 self._reset();
-            });
-        }
-    },
-
-    _extractValidVolumes: {
-        value: function(values) {
-            var volumes = [],
-                entry;
-
-            for (var i = values.length - 1; i >= 0; i--) {
-                entry = values[i];
-
-                if (entry.host_path && entry.container_path) {
-                    delete entry.isLocked;
-                    volumes.push(entry);
-                }
-            }
-
-            return volumes;
-        }
-    },
-
-    _getVariablesFromArray: {
-        value: function (array) {
-            return array.filter(function (entry) {
-                var shouldKeep = !!(entry.variable && entry.value);
-
-                if (!shouldKeep && entry.optional !== void 0 && entry.optional === true) {
-                    throw new Error("missing setting");
-                }
-
-                return shouldKeep;
-            }).map(function (entry) {
-                return entry.variable + "=" + entry.value;
             });
         }
     }
@@ -215,10 +138,6 @@ exports.ContainerCreator = AbstractInspector.specialize({
 
     DEFAULT_PRIMARY_NETWORK: {
         value: 'NAT'
-    },
-
-     PRIMARY_NETWORK_MODE_BRIDGED: {
-        value: 'BRIDGED'
     }
 
 });
