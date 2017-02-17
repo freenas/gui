1bdb5f21d9a0c88e7b8695ccf6b1b55bff63c6d4
Pierre Chaussalet <pchaussalet@gmail.com>
2017-02-16 18:19:36 -0800

    Fix markdown formatting in changelog
    Ticket: #21115

--- ui/sections/system/inspectors/updates.reel/updates.js
+++ ui/sections/system/inspectors/updates.reel/updates.js
@@ -32,7 +32,7 @@ exports.Updates = AbstractInspector.specialize({
                 }).then(function() {
                     self._updateService.getInfo().then(function(info) {
                         self.info = info;
-                        self.parsedHtml = marked(_.join(self.info.changelog, "\n"));
+                        self.parsedHtml = marked(_.replace(_.join(self.info.changelog, "\n"), /## /g, "\n## "));
                     });
                 });
             }
