fe8c94c9ff81c839561b61f54a2d67da800b1c35
Erin Clark <erin@ixsystems.com>
2017-02-22 14:56:03 -0800

    Make alerts and tasks show the correct timezone/date offset (this fix also needs a middleware fix before timezone will auto set without a browser refresh)
    Ticket: #20282

--- ui/dashboard/notifications.reel/notification.reel/alert-notification.reel/alert-notification.js
+++ ui/dashboard/notifications.reel/notification.reel/alert-notification.reel/alert-notification.js
@@ -2,6 +2,8 @@
  * @module ui/notification.reel
  */
 var Component = require("montage/ui/component").Component,
+    SystemService = require("core/service/system-service").SystemService,
+    moment = require('moment-timezone'),
     EventDispatcherService = require("core/service/event-dispatcher-service").EventDispatcherService;
 
 /**
@@ -9,9 +11,26 @@ var Component = require("montage/ui/component").Component,
  * @extends Component
  */
 exports.AlertNotification = Component.specialize(/** @lends AlertNotification# */ {
+    timeFormat: {
+        value: "HH:mm:ss"
+    },
+
+    setDate: {
+        value: function() {
+            var displayedDate = this.object.updated_at - 28800;
+            this.object.date = moment(displayedDate.$date).tz(this.timezone).format(this.timeFormat);
+        }
+    },
+
     enterDocument: {
         value: function() {
-            this.object.date = new Date(this.object.updated_at.$date);
+            var self = this;
+            this.systemService = SystemService.getInstance();
+            this.object.startDate = this.systemService.getGeneral().then(function (general) {
+                self.timezone = general.timezone;
+            }).then(function() {
+                self.setDate();
+            });
         }
     },
 
