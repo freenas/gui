c0cab043c0e79a7164e88fbf332ca78bd086b6a8
Thibault Zanini <thibaultzanini@gmail.com>
2017-02-26 18:41:59 -0500

    Fix #21476

--- ui/inspectors/service.reel/afp-service.reel/afp-service.js
+++ ui/inspectors/service.reel/afp-service.reel/afp-service.js
@@ -1,24 +1,7 @@
-var Component = require("montage/ui/component").Component,
-    Model = require("core/model/model").Model;
+var Component = require("montage/ui/component").Component;
 
 /**
  * @class AfpService
  * @extends Component
  */
-exports.AfpService = Component.specialize({
-
-    users: {
-        value: null
-    },
-
-    templateDidLoad: {
-        value: function () {
-            var self = this;
-
-            this.application.dataService.fetchData(Model.User).then(function(users) {
-                self.users = users;
-            });
-        }
-    }
-
-});
+exports.AfpService = Component.specialize();
