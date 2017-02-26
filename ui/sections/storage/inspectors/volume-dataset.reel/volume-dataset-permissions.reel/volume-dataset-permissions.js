e00d3571d1960831964bab3cdd873b5b214dd285
Thibault Zanini <thibaultzanini@gmail.com>
2017-02-26 18:55:57 -0500

    Fix #21475

--- ui/sections/storage/inspectors/volume-dataset.reel/volume-dataset-permissions.reel/volume-dataset-permissions.js
+++ ui/sections/storage/inspectors/volume-dataset.reel/volume-dataset-permissions.reel/volume-dataset-permissions.js
@@ -1,24 +1,8 @@
-/**
- * @module ui/volume-dataset-permissions.reel
- */
 var AbstractInspector = require("ui/abstract/abstract-inspector").AbstractInspector,
-    Model = require("core/model/model").Model,
     UnixPermissionsConverter = require("core/converter/unix-permissions-converter").UnixPermissionsConverter,
     VolumeDatasetPermissionstype = require('core/model/enumerations/volume-dataset-permissionstype').VolumeDatasetPermissionstype;
 
-/**
- * @class VolumeDatasetPermissions
- * @extends Component
- */
 exports.VolumeDatasetPermissions = AbstractInspector.specialize(/** @lends VolumeDatasetPermissions# */ {
-
-    users: {
-        value: null
-    },
-
-    groups: {
-        value: null
-    },
 
     _permissionsConverter: {
         value: null
@@ -62,43 +46,7 @@ exports.VolumeDatasetPermissions = AbstractInspector.specialize(/** @lends Volum
 
     templateDidLoad: {
         value: function () {
-            //Preload data before entering in the dom, in order to avoid graphic glitches
-            this._loadUsersIfNeeded();
-            this._loadGroupsIfNeeded();
             this._permissionsTypeOptions = VolumeDatasetPermissionstype.members;
-        }
-    },
-
-    enterDocument: {
-        value: function () {
-            this._loadUsersIfNeeded();
-            this._loadGroupsIfNeeded();
-        }
-    },
-
-    _loadUsersIfNeeded: {
-        value: function() {
-            if ((!this._fetchGroupsPromise && !this.users) || (this.users && this.users.length === 0)) {
-                var self = this;
-
-                this._fetchUsersPromise = this.application.dataService.fetchData(Model.User).then(function (users) {
-                    self.users = users;
-                    self._fetchUsersPromise = null;
-                });
-            }
-        }
-    },
-
-    _loadGroupsIfNeeded: {
-        value: function() {
-            if ((!this._fetchGroupsPromise && !this.groups) || (this.groups && this.groups.length === 0)) {
-                var self = this;
-
-                this._fetchGroupsPromise = this.application.dataService.fetchData(Model.Group).then(function (groups) {
-                    self.groups = groups;
-                    self._fetchGroupsPromise = null;
-                });
-            }
         }
     }
 
