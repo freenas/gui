0a90d1e80e90425fbac852e75038b5327bd5e500
Thibault Zanini <thibaultzanini@gmail.com>
2017-02-28 12:12:42 -0500

    Fix #21474

--- ui/sections/storage/inspectors/share.reel/afp-share.reel/afp-share.js
+++ ui/sections/storage/inspectors/share.reel/afp-share.reel/afp-share.js
@@ -1,3 +1,37 @@
-var AbstractShareInspector = require("../abstract-share-inspector").AbstractShareInspector;
+var AbstractShareInspector = require("../abstract-share-inspector").AbstractShareInspector,
+    AccountService = require("core/service/account-service").AccountService,
+    Model = require("core/model").Model;
 
-exports.AfpShare = AbstractShareInspector.specialize();
+ var SearchController = function SearchController(service, model) {
+    this.service = AccountService.getInstance();
+    this.model = model
+ };
+
+ SearchController.prototype.search = function (value) {
+    if (this.model === Model.User) {
+        return this.service.searchUser(value).then(function (results) {
+            return results.map(function (result) {
+                return result.username;
+            });
+        });
+    }
+
+    return this.service.searchGroup(value).then(function (results) {
+        return results.map(function (result) {
+            return result.name;
+        });
+    });
+ };
+
+exports.AfpShare = AbstractShareInspector.specialize({
+
+    enterDocument: {
+        value: function (isFirstTime) {
+            if (isFirstTime) {
+                this.groupsSearchController = new SearchController(Model.Group);
+                this.usersSearchController = new SearchController(Model.User);
+            }
+        }
+    }
+
+});
