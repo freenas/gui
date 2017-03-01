5c912e77dc481997f0f07798a6f147a0e3d180e3
Joshua Rule Dobson <joshua@joshuarule.com>
2017-03-01 10:00:02 -0800

    edits container tables to accept empty add button action and adds proper labeling

--- ui/sections/containers/controls/table-port.reel/table-port.js
+++ ui/sections/containers/controls/table-port.reel/table-port.js
@@ -1,6 +1,18 @@
 var Component = require("montage/ui/component").Component;
 
 exports.TablePort = Component.specialize({
+    prepareForActivationEvents: {
+        value: function () {
+            this.addEventListener("action", this);
+        }
+    },
+
+    handleAddButtonAction: {
+        value: function () {
+            this.table.showNewEntryRow();
+        }
+    },
+
     tableWillUseNewEntry: {
         value: function() {
             return {
