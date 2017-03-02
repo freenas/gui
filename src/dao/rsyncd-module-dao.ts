74afe78d7082bfe3ea3ccd4c0eb726be4d03dae6
Xin Tan <mactanxin@gmail.com>
2017-03-01 18:39:58 -0800

    UPDATED: fix RSYNCD modules does not refresh properly after changes

--- src/dao/rsyncd-module-dao.ts
+++ src/dao/rsyncd-module-dao.ts
@@ -1,8 +1,11 @@
 import {AbstractDao} from './abstract-dao';import {Model} from '../model';
+import {Model} from '../model';
 import {RsyncdModule} from '../model/RsyncdModule';
 
 export class RsyncdModuleDao extends AbstractDao<RsyncdModule> {
     public constructor() {
-        super(Model.RsyncdModule);
+        super(Model.RsyncdModule, {
+        	eventName: 'entity-subscriber.rsyncd.module.changed'
+        });
     }
 }
