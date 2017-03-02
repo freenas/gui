6546f26136779e7ab7a86dc717c20018692fbd1a
Pierre Chaussalet <pchaussalet@gmail.com>
2017-03-02 13:42:02 -0800

    Remove tests of kerberos tabs

--- tests/e2e/tests/index.js
+++ tests/e2e/tests/index.js
@@ -8,7 +8,7 @@ var steps = {
             .url(browser.launchUrl)
             .waitForElementVisible('div[data-montage-id=signIn].SignIn', 10000)
             .setValue('input[data-montage-id=userName]', 'root')
-            .setValue('input[data-montage-id=password]', 'root')
+            .setValue('input[data-montage-id=password]', 'meh')
             .press('button[data-montage-id=submit].SignIn-submit')
             .waitForElementVisible('div.SystemInfo', 5000);
     }
--- tests/e2e/tests/network/04_settings.js
+++ tests/e2e/tests/network/04_settings.js
@@ -4,6 +4,6 @@ module.exports = {
             .press('.CascadingListItem:nth-child(1) [data-montage-id=settings]')
             .waitForElementVisible('.CascadingListItem:nth-child(2) div.SectionSettings');
         browser.expect.element('.CascadingListItem:nth-child(2) div.SectionSettings .Inspector-header').text.to.equal('Network settings');
-        browser.expect.element('.CascadingListItem:nth-child(2) div.SectionSettings div.Settings').to.be.present.before(5000);
+        browser.expect.element('.CascadingListItem:nth-child(2) div.SectionSettings div.Network-settings').to.be.present.before(5000);
     }
 };
