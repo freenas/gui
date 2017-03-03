ceb64169aff70ed9b87d638926a6ce44b2e01ea9
Xin Tan <mactanxin@gmail.com>
2017-03-03 14:29:02 -0800

    UPDATED: switch enum order for efault value

--- core/model/enumerations/vm-device-nic-device.js
+++ core/model/enumerations/vm-device-nic-device.js
@@ -1,3 +1,3 @@
 var Enum = require("montage/core/enum").Enum;
 
-exports.VmDeviceNicDevice = new Enum().initWithMembersAndValues(["E1000","NE2K","VIRTIO"], ["E1000","NE2K","VIRTIO"]);
+exports.VmDeviceNicDevice = new Enum().initWithMembersAndValues(["VIRTIO","NE2K","E1000"], ["VIRTIO","NE2K","E1000"]);
