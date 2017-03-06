b4a8d538f473b23e8f24ba0aef747cf9e2489abc
Erin Clark <erin@ixsystems.com>
2017-03-06 13:48:41 -0800

    Unix permission conversion is no longer needed and causes race conditions
    Ticket: #21858

--- ui/inspectors/service.reel/tftpd-service.reel/tftpd-service.js
+++ ui/inspectors/service.reel/tftpd-service.reel/tftpd-service.js
@@ -19,48 +19,5 @@ exports.TftpdService = Component.specialize({
                 self.users = users;
             });
         }
-    },
-
-    enterDocument: {
-        value: function () {
-            if (this.object.umask) {
-                this.umaskModes = {
-                    user: this.object.umask.user,
-                    group: this.object.umask.group,
-                    others: this.object.umask.others
-                };
-                delete this.object.umask.value;
-            } else {
-                this.umaskModes = {
-                    user: {
-                        read: false,
-                        write: false,
-                        execute: false
-                    },
-                    group: {
-                        read: false,
-                        write: false,
-                        execute: false
-                    },
-                    others: {
-                        read: false,
-                        write: false,
-                        execute: false
-                    }
-                };
-            }
-        }
-    },
-
-    save: {
-        value: function() {
-            this.object.umask = this.umaskModes;
-        }
-    },
-
-    exitDocument: {
-        value: function() {
-            this.umaskModes = null;
-        }
     }
 });
