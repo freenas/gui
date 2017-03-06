672dafa8440ec1278291b62d4c9fb56903f78888
Thibault Zanini <thibaultzanini@gmail.com>
2017-03-06 13:32:15 -0800

    Fix user primary group mixed up Ticket: #21779

---
+++ ui/abstract/abstract-search-account.js
@@ -0,0 +1,129 @@
+var Component = require("montage/ui/component").Component,
+    AccountService = require("core/service/account-service").AccountService,
+    _ = require("lodash");
+
+exports.AbstractSearchAccount = Component.specialize({
+
+    constructor: {
+        value: function () {
+            this.service = AccountService.getInstance();
+        }
+    },
+
+    _labelPath: {
+        value: null
+    },
+
+    labelPath: {
+        set: function (path) {
+            if (this._labelPath !== path) {
+                if (typeof path === "string" && path.length) {
+                    this._labelPath = path;
+                } else {
+                    this._labelPath = null;
+                }
+            }
+        },
+        get: function () {
+            return this._labelPath || this.constructor.labelPath;
+        }
+    },
+
+    _valuePath: {
+        value: null
+    },
+
+    valuePath: {
+        set: function (path) {
+            if (this._valuePath !== path) {
+                if (typeof path === "string" && path.length) {
+                    this._valuePath = path;
+                } else {
+                    this._valuePath = null;
+                }
+            }
+        },
+        get: function () {
+            return this._valuePath || this.constructor.valuePath;
+        }
+    },
+
+    _isLoading: {
+        value: { loadingLabel: false, loadingOptions: false }
+    },
+
+    isLoading: {
+        get: function () {
+            return _.every(this._isLoading);
+        }
+    },
+
+    _value: {
+        value: null
+    },
+
+    value: {
+        set: function (value) {
+            if (this._value !== value) {
+                this._value = value;
+
+                if (value) {
+                    this._findLabelForValue();
+                } else {
+                    this.displayedValue = null;
+                }
+            }
+        },
+        get: function () {
+            return this._value;
+        }
+    },
+
+    _setLoadingStep: {
+        value: function (step, value) {
+            this._isLoading[step] = !!value;
+            this.dispatchOwnPropertyChange('isLoading', this.isLoading);
+        }
+    },
+
+    enterDocument: {
+        value: function () {
+            this._loadInitialOptions();
+        }
+    },
+
+    _findLabelForValue: {
+        value: function () {
+            if (typeof this.loadInitialOptions === 'function') {
+                var criteria = {},
+                    self = this;
+
+                criteria[this.valuePath] = this.value;
+                this._setLoadingStep('loadingLabel', true);
+
+                this.findLabelForValue(criteria).then(function (entries) {
+                    self.displayedValue = entries && entries.length ?
+                        entries[0][self.labelPath] : self.value;
+                }).finally(function () {
+                    self._setLoadingStep('loadingLabel', false);
+                });
+            }
+        }
+    },
+
+    _loadInitialOptions: {
+        value: function () {
+            if (typeof this.loadInitialOptions === 'function') {
+                var self = this;
+                this._setLoadingStep('loadingOptions', true);
+
+                this.loadInitialOptions().then(function (options) {
+                    self.initialOptions = options;
+                }).finally(function () {
+                    self._setLoadingStep('loadingOptions', false);
+                });
+            }
+        }
+    }
+
+});
--- ui/controls/search-users.reel/search-users.js
+++ ui/controls/search-users.reel/search-users.js
@@ -1,86 +1,6 @@
-var Component = require("montage/ui/component").Component,
-    AccountService = require("core/service/account-service").AccountService;
+var AbstractSearchAccount = require("ui/abstract/abstract-search-account").AbstractSearchAccount;
 
-
-exports.SearchUsers = Component.specialize(/** @lends SearchUsers# */ {
-
-    _labelPath: {
-        value: null
-    },
-
-    labelPath: {
-        set: function (path) {
-            if (this._labelPath !== path) {
-                if (typeof path === "string" && path.length) {
-                    this._labelPath = path;
-                } else {
-                    this._labelPath = null;
-                }
-            }
-        },
-        get: function () {
-            return this._labelPath || this.constructor.labelPath;
-        }
-    },
-
-    _valuePath: {
-        value: null
-    },
-
-    valuePath: {
-        set: function (path) {
-            if (this._valuePath !== path) {
-                if (typeof path === "string" && path.length) {
-                    this._valuePath = path;
-                } else {
-                    this._valuePath = null;
-                }
-            }
-        },
-        get: function () {
-            return this._valuePath || this.constructor.valuePath;
-        }
-    },
-
-    enterDocument: {
-        value: function (firstTime) {
-            if (firstTime) {
-                this.service = AccountService.getInstance();
-            }
-
-            if (!this.isLoading && this.value) {
-                var criteria = {},
-                    self = this;
-
-                criteria[this.valuePath] = this.value;
-                this.isLoading= true;
-
-                Promise.all([
-                    this.service.searchUserWithCriteria(criteria),
-                    this.service.listLocalUsers({
-                        labelPath: this.labelPath,
-                        valuePath: this.valuePath
-                    })
-                ]).spread(function (entries, initialOptions) {
-                    if (entries && entries.length) {
-                        self.displayedValue = entries[0][self.labelPath];
-                    } else { // fallback
-                        self.displayedValue = self.value;
-                    }
-
-                    self.initialOptions = initialOptions;
-                }).finally(function () {
-                    self.isLoading = false;
-                });
-            }
-        }
-    },
-
-    exitDocument: {
-        value: function () {
-            this.isLoading = false;
-        }
-    },
+exports.SearchUsers = AbstractSearchAccount.specialize(/** @lends SearchGroups# */ {
 
     search: {
         value: function (value) {
@@ -88,6 +8,21 @@ exports.SearchUsers = Component.specialize(/** @lends SearchUsers# */ {
                  labelPath: this.labelPath,
                  valuePath: this.valuePath
              });
+        }
+    },
+
+    loadInitialOptions: {
+        value: function () {
+            return this.service.listLocalUsers({
+                labelPath: this.labelPath,
+                valuePath: this.valuePath
+            });
+        }
+    },
+
+    findLabelForValue: {
+        value: function (criteria) {
+            return this.service.searchUserWithCriteria(criteria);
         }
     }
 
