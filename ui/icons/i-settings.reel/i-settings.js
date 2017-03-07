ef07c1ea85b9527d9e23a1c02e67e35a379dc33f
Joshua Rule Dobson <joshua@joshuarule.com>
2017-03-06 18:02:11 -0800

    adds settings icon component

---
+++ ui/icons/i-settings.reel/i-settings.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/i-settings.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class ISettings
+ * @extends Component
+ */
+exports.ISettings = Component.specialize(/** @lends ISettings# */ {
+    constructor: {
+        value: function ISettings() {
+            this.super();
+        }
+    }
+});
