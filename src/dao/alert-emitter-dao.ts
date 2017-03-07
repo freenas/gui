d276c99fd040a7b9fa2b2de7e27a613fb664aa0e
Xin Tan <mactanxin@gmail.com>
2017-03-07 14:10:15 -0800

    ADDED: event update for alert emitter

--- src/dao/alert-emitter-dao.ts
+++ src/dao/alert-emitter-dao.ts
@@ -6,6 +6,7 @@ export class AlertEmitterDao extends AbstractDao<AlertEmitter> {
 
     public constructor() {
         super(Model.AlertEmitter, {
+        	eventName: 'entity-subscriber.alert.emitter.changed',
             queryMethod: 'alert.emitter.query',
             createMethod: 'alert.emitter.update',
             updateMethod: 'alert.emitter.update'
