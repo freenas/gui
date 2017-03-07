02fa1612befe9f5feb4c07d899a6e423ccec7ee4
Joshua Rule Dobson <joshua@joshuarule.com>
2017-03-07 13:16:30 -0800

    replace drawer widget images and clean up the component

--- ui/drawer.reel/drawer-item.reel/drawer-item.js
+++ ui/drawer.reel/drawer-item.reel/drawer-item.js
@@ -9,30 +9,6 @@ var AbstractDraggableComponent = require("blue-shark/core/drag-drop/abstract-dra
  */
 exports.DrawerItem = AbstractDraggableComponent.specialize(/** @lends DrawerItem# */ {
 
-    _hasToggled: {
-        value: false
-    },
-
-    hasToggled: {
-        set: function (hasToggled) {
-            hasToggled = !!hasToggled;
-
-            if (this._hasToggled !== hasToggled) {
-
-                this._hasToggled = hasToggled;
-
-                if (hasToggled) {
-                    this.classList.add('has-toggled');
-                } else {
-                    this.classList.remove('has-toggled');
-                }
-            }
-        },
-        get: function () {
-            return this._hasToggled;
-        }
-    },
-
     _placeHolderStrategy: {
         value: "hidden"
     }
