4d7b3f6d1f0fde174e4885b3316e50b22dabb888
Thibault Zanini <thibaultzanini@gmail.com>
2017-03-08 01:28:03 -0800

    Fix users/groups wildcard search. (#619)

--- src/repository/account-repository.ts
+++ src/repository/account-repository.ts
@@ -238,11 +238,11 @@ export class AccountRepository extends AbstractRepository {
     }
 
     public searchUser(value) {
-        return this.searchUserWithCriteria({username: [['~', value]]});
+        return this.searchUserWithCriteria({username: [['~', value + "*"]]});
     }
 
     public searchGroup(value) {
-        return this.searchGroupWithCriteria({name: [['~', value]]});
+        return this.searchGroupWithCriteria({name: [['~', value + "*"]]});
     }
 
     public searchGroupWithCriteria(criteria: any) {
