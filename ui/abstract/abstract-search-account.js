c1a02425d11c0ac2798e9f7e09cb5e4c197905ba
Thibault Zanini <thibaultzanini@gmail.com>
2017-03-10 13:01:10 -0800

    Reset group/user selectors when an object is new. Ticket: #22073

--- ui/abstract/abstract-search-account.js
+++ ui/abstract/abstract-search-account.js
@@ -68,9 +68,12 @@ exports.AbstractSearchAccount = Component.specialize({
             if (this._value !== value) {
                 this._value = value;
 
-                if (value && typeof value === "string" &&
-                    (!this.entry || (this.entry[this.valuePath] !== value))) {
-                    this._findLabelForValue(value);
+                if (value && typeof value === "string") {
+                    if (!this.entry || (this.entry[this.valuePath] !== value)) {
+                        this._findLabelForValue(value);
+                    }
+                } else {
+                    this.entry = null;
                 }
             }
         },
