98ece3ef72c0fb8197c462cffe9ce506d2159e77
Pierre Chaussalet <pchaussalet@gmail.com>
2017-03-13 14:51:01 -0700

    Add RECEIVED to VolumePropertySource enum
    Ticket: #22162

--- src/model/enumerations/VolumePropertySource.ts
+++ src/model/enumerations/VolumePropertySource.ts
@@ -2,7 +2,8 @@ const VolumePropertySource = {
     NONE: 'NONE' as 'NONE',
     DEFAULT: 'DEFAULT' as 'DEFAULT',
     LOCAL: 'LOCAL' as 'LOCAL',
-    INHERITED: 'INHERITED' as 'INHERITED'
+    INHERITED: 'INHERITED' as 'INHERITED',
+    RECEIVED: 'RECEIVED' as 'RECEIVED'
 };
 type VolumePropertySource = (typeof VolumePropertySource)[keyof typeof VolumePropertySource];
 export {VolumePropertySource};
