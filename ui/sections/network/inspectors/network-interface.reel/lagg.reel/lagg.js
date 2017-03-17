378d1e9602fa9e2e00876dd1df73993b2653e472
Pierre Chaussalet <pchaussalet@gmail.com>
2017-03-17 08:42:06 -0700

    Fix lagg creation and add default values
    Ticket: #22341

--- ui/sections/network/inspectors/network-interface.reel/lagg.reel/lagg.js
+++ ui/sections/network/inspectors/network-interface.reel/lagg.reel/lagg.js
@@ -1,12 +1,17 @@
 var Component = require("montage/ui/component").Component,
     NetworkAggregationProtocols = require("core/model/enumerations/network-aggregation-protocols").NetworkAggregationProtocols;
 
-/**
- * @class Lagg
- * @extends Component
- */
 exports.Lagg = Component.specialize({
     laggProtocolOptions: {
         value: NetworkAggregationProtocols.members
+    },
+
+    enterDocument: {
+        value: function() {
+            if (this.object && this.object._isNew) {
+                this.object.lagg.ports = this.object.lagg.ports || [];
+                this.object.lagg.protocol = this.object.lagg.protocol || 'FAILOVER';
+            }
+        }
     }
 });
