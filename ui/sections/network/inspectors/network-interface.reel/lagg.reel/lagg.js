951edbeb198dfed594ba1e14711ef73184280a87
Pierre Chaussalet <pchaussalet@gmail.com>
2017-03-29 10:00:18 -0700

    Avoid conflict between interface types
    Ticket: #22897

--- ui/sections/network/inspectors/network-interface.reel/lagg.reel/lagg.js
+++ ui/sections/network/inspectors/network-interface.reel/lagg.reel/lagg.js
@@ -8,7 +8,7 @@ exports.Lagg = Component.specialize({
 
     enterDocument: {
         value: function() {
-            if (this.object && this.object._isNew) {
+            if (this.object && this.object._isNew && this.object.type === 'LAGG') {
                 this.object.lagg.ports = this.object.lagg.ports || [];
                 this.object.lagg.protocol = this.object.lagg.protocol || 'FAILOVER';
             }
